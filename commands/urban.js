5243ff97b02cbcc0fc420bc0a751c5b526994d47
Monkeyyy11 <gaileshd@gmail.com>
2017-09-04 10:52:47 +0200

    Added new command

---
+++ commands/urban.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const snekfetch = require('snekfetch');
+exports.run = async(client, msg, args) => {
+    const content = args.slice().join(" ");
+    await snekfetch.get(`http://api.urbandictionary.com/v0/define?term=${content}`)
+    .then(r =>{
+        let def = r.body.list[0];
+        const embed = new Discord.RichEmbed()
+            .setTitle("📚 Urban Dictionary")
+            .setThumbnail("https://everythingfat.files.wordpress.com/2013/01/ud-logo.jpg")
+            .setColor('#ABCDEF')
+            .setDescription(`**Definition for ${def.word}**\n` + (def.definition.length > 1500 ? def.definition.substring(0, 1500) + "..." : def.definition))
+            .addField("📃 Example", def.example.length > 1020 ? def.example.substring(0, 1020) + "..." : def.example, false)
+            .addField("👍 Thumbs up", def.thumbs_up, true)
+            .addField("👎 Thumbs down", def.thumbs_down, true);
+            
+        msg.channel.send({ embed });
+    })
+    .catch(err => {
+        msg.channel.send("Could not find anything to your query!").then(m => m.delete(10000));
+    });
+};
+
+exports.conf = {
+	enabled: true,
+	guildOnly: false,
+    aliases: []
+};
+exports.help = {
+	name: 'urban',
+	description: 'Urban dictionary',
+    usage: 'urban {query}',
+    example: 'userinfo Discord',
+	category: 'utility'
+};
