8c163b05161d2ee5c4a34446768d0dbbe312764b
Monkeyyy11 <gaileshd@gmail.com>
2017-09-04 18:07:27 +0200

    Fixed a bug

--- commands/leave.js
+++ commands/leave.js
@@ -2,7 +2,7 @@ exports.run = function(client, msg, args) {
 	const tableload = client.guildconfs.get(msg.guild.id);
 	let addedrole = args.slice().join(' ');
     const foundRole = msg.guild.roles.find(role => role.name.toLowerCase() === args.slice().join(' ').toLowerCase());
-    const author = msg.guild.get(msg.author.id);
+    const author = msg.guild.members.get(msg.author.id);
 
 	if (addedrole.length < 1) return msg.reply('You must specify the name of the role!').then(m => m.delete(10000));
 	if (!foundRole) return msg.reply('Höh ... This role does not exist at all!').then(m => m.delete(10000));
