d38a0b7aaacbba490fe0bbe2ac2e66157bcc64ad
Monkeyyy11 <gaileshd@gmail.com>
2017-09-05 16:28:41 +0200

    test

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 exports.run = client => {
 	console.log(`LENXOBOT: Ready to serve in ${client.channels.size} channels on ${client.guilds.size}, for a total of ${client.users.size} users.`);
 	client.user.setPresence({ game: { name: `?help in ${client.guilds.size} guilds`, type: 0 } });
+	console.log(client.guilds.map(g => g.name));
 };
