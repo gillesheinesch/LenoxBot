811da7b9a5d46fa1622ad9426db0ef9255464528
Monkeyyy11 <gaileshd@gmail.com>
2017-09-05 15:42:17 +0200

    Fix a bug

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -38,8 +38,8 @@ exports.run = (client, guild) => {
 		announcechannel: '',
 		selfassignableroles: []
 	};
-	guild.owner.send(`Hello ${guild.owner.user.username}, \nThanks for choosing LenoxBot! Currently, we are still in the alpha phase, so it can lead to problems. If you find any problems, you can report them on our Discord server: https://discord.gg/5mpwCr8 \n\nYou can use the command ?modules to see all modules of the bot \nTo see all commands of a module, just use ?commands modulename \nTo see more details about a command, just use ?help commandname \n\nWe would be pleased to welcome you to our Discord-Server: https://discord.gg/5mpwCr8`);
 	client.guildconfs.set(guild.id, defaultSettings);
+	guild.owner.send(`Hello ${guild.owner.user.username}, \nThanks for choosing LenoxBot! Currently, we are still in the alpha phase, so it can lead to problems. If you find any problems, you can report them on our Discord server: https://discord.gg/5mpwCr8 \n\nYou can use the command ?modules to see all modules of the bot \nTo see all commands of a module, just use ?commands modulename \nTo see more details about a command, just use ?help commandname \n\nWe would be pleased to welcome you to our Discord-Server: https://discord.gg/5mpwCr8`);
 	const embed = new Discord.RichEmbed()
 	.setTimestamp()
 	.setAuthor(`${guild.name} (${guild.id})`)
