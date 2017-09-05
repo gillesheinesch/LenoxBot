6be90edbbb91a7368bb737a875c8d2ad56fc5b55
Monkeyyy11 <gaileshd@gmail.com>
2017-09-05 22:21:40 +0200

    Test

--- commands/eval.js
+++ commands/eval.js
@@ -3,9 +3,9 @@ exports.run = async (client, msg, args) => {
     try {
       const evaled = eval(code);
       const clean = await client.clean(client, evaled);
-      message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
+      msg.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
     } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
+      msg.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
     }
   };
 
