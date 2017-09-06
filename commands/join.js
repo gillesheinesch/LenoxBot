6fe76b1996ca664afa60ff5c81ff23637e1c0c0c
Monkeyyy11 <gaileshd@gmail.com>
2017-09-06 22:54:48 +0200

    Fixed | Use now async

--- commands/join.js
+++ commands/join.js
@@ -1,8 +1,9 @@
-exports.run = function(client, msg, args) {
+exports.run = async(client, msg, args) => {
 	const tableload = client.guildconfs.get(msg.guild.id);
 	let addedrole = args.slice().join(' ');
     const foundRole = msg.guild.roles.find(role => role.name.toLowerCase() === args.slice().join(' ').toLowerCase());
-    const author = msg.guild.members.get(msg.author.id);
+	const author = msg.guild.members.get(msg.author.id);
+	const channelID = msg.channel.id;
 
 	if (addedrole.length < 1) return msg.reply('You must specify the name of the role!').then(m => m.delete(10000));
     if (!foundRole) return msg.reply('Höh ... This role does not exist at all!').then(m => m.delete(10000));
@@ -10,7 +11,11 @@ exports.run = function(client, msg, args) {
 
 for (var i = 0; i < tableload.selfassignableroles.length; i++) {
 	if (foundRole.id === tableload.selfassignableroles[i]) {
-            author.addRole(foundRole).then(m => m.channel.send('Role successfully assigned!')).catch(msg.channel.send('I don\'t have the necessary rights to give you this role. Please take a look at the rights of your roles and the order of your roles!'));
+		try {
+			await author.addRole(foundRole).then(m => m.guild.channels.get(channelID).send('Role successfully assigned!'))
+		} catch (error) {
+			await msg.channel.send('I don\'t have the necessary rights to give you this role. Please take a look at the rights of your roles and the order of your roles!')
+		}
     }
 }
 };
--- commands/leave.js
+++ commands/leave.js
@@ -1,17 +1,22 @@
-exports.run = function(client, msg, args) {
+exports.run = async(client, msg, args) => {
 	const tableload = client.guildconfs.get(msg.guild.id);
 	let addedrole = args.slice().join(' ');
-    const foundRole = msg.guild.roles.find(role => role.name.toLowerCase() === args.slice().join(' ').toLowerCase());
-    const author = msg.guild.members.get(msg.author.id);
+	const foundRole = msg.guild.roles.find(role => role.name.toLowerCase() === args.slice().join(' ').toLowerCase());
+	const author = msg.guild.members.get(msg.author.id);
+	const channelID = msg.channel.id;
 
 	if (addedrole.length < 1) return msg.reply('You must specify the name of the role!').then(m => m.delete(10000));
-    if (!foundRole) return msg.reply('Höh ... This role does not exist at all!').then(m => m.delete(10000));
-    if (!msg.member.roles.has(foundRole.id)) return msg.reply('You don\'t even have this role!');
+	if (!foundRole) return msg.reply('Höh ... This role does not exist at all!').then(m => m.delete(10000));
+	if (!msg.member.roles.has(foundRole.id)) return msg.reply('You don\'t even have this role!');
 
 for (var i = 0; i < tableload.selfassignableroles.length; i++) {
 	if (foundRole.id === tableload.selfassignableroles[i]) {
-            author.removeRole(foundRole).then(msg.channel.send('Role successfully removed!')).catch(msg.channel.send('I don\'t have the necessary rights to give you this role. Please take a look at the rights of your roles and the order of your roles!'));
-    }
+		try {
+			await author.removeRole(foundRole).then(m => m.guild.channels.get(channelID).send('Role successfully removed!'));
+		} catch (error) {
+			await msg.channel.send('I don\'t have the necessary rights to give you this role. Please take a look at the rights of your roles and the order of your roles!');
+	}        
+	}
 }
 };
 
