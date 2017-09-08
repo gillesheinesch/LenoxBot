00eedcb2aeadf1028920f98b0b352b286515a62f
Monkeyyy11 <gaileshd@gmail.com>
2017-09-08 15:56:12 +0200

    Fix

--- commands/rank.js
+++ commands/rank.js
@@ -3,13 +3,18 @@ const Discord = require('discord.js');
 sql.open("../lenoxbotscore.sqlite");
 exports.run = (client, msg, args) => {
 	const user1 = msg.mentions.users.first() || msg.author;
+	const embed = new Discord.RichEmbed()
+	.setColor('#A4F2DF')
+	.setThumbnail(user1.avatarURL)
+	.setAuthor(user1.tag, user1.avatarURL);
 		sql.get(`SELECT * FROM scores WHERE guildId ="${msg.guild.id}" AND userId = "${user1.id}"`).then(row => {
-			const embed = new Discord.RichEmbed()
-			.setColor('#A4F2DF')
-			.setThumbnail(user1.avatarURL)
-			.setAuthor(user1.tag, user1.avatarURL)
-			.addField('Points', row.points, true)
-			.addField('Level', row.level, true);
+			if (!row) {
+				embed.addField('Points', `0`, true);
+				embed.addField('Level', `0`, true);
+			} else {
+			embed.addField('Points', row.points, true);
+			embed.addField('Level', row.level, true);
+			}
 			return msg.channel.send({ embed });
 		});
 };
