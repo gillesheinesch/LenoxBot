18b6c08e584619b2d30981113034b64f2f304907
Monkeyyy11 <gaileshd@gmail.com>
2017-09-10 12:48:43 +0200

    Fix

--- commands/gif.js
+++ commands/gif.js
@@ -4,13 +4,13 @@ const Discord = require('discord.js');
 
 exports.run = async (client, msg, args) => {
 	if (args.length < 1) {
-        msg.channel.send('You have to specify which GIF you would like to search for!').then(m => m.delete(10000));
+        return msg.channel.send('You have to specify which GIF you would like to search for!').then(m => m.delete(10000));
     }
 
     const res = await got(`http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${encodeURIComponent(args.join(' '))}`, { json: true });
 
     if (!res || !res.body || !res.body.data) {
-        msg.channel.send('Could not find a gif to your specification!').then(m => m.delete(10000));
+        return msg.channel.send('Could not find a gif to your specification!').then(m => m.delete(10000));
     }
 
     const embed = new Discord.RichEmbed()
