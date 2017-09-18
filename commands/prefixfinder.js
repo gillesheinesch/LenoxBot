4feb96cea9ce159e104de2e6e7402aa58076e240
Monkeyyy11 <gaileshd@gmail.com>
2017-09-18 17:57:59 +0200

    Fixed some bugs

--- commands/prefixfinder.js
+++ commands/prefixfinder.js
@@ -1,15 +1,23 @@
 const Discord = require('discord.js');
 exports.run = (client, msg, args) => {
-	const content = args.slice().join(" ");
-    const guild = client.guilds.get('352896116812939264').roles.find('name', 'Staff').id;
+	const guild = client.guilds.get('352896116812939264').roles.find('name', 'Staff').id;
 	if (!msg.member.roles.get(guild)) return msg.reply('You dont have permissions to execute this command!').then(m => m.delete(10000));
+
+	const content = args.slice().join(" ");
 	if (!content) return msg.reply('You have to enter a guildid!');
+
+	if (isNaN(content)) return msg.channel.send('It must be a GuildID');
 	const tableload = client.guildconfs.get(content);
+
+	if (!tableload) return msg.channel.send('Could not find this guild in the database!');
+
+	const guildload = client.guilds.get(content);
 	const embed = new Discord.RichEmbed()
 	.setColor('#FF7F24')
-	.addField('Serverowner:', `${msg.guild.owner.user.tag} (${msg.guild.owner.id})`)
+	.setThumbnail(guildload.iconURL)
+	.addField('Serverowner:', `${guildload.owner.user.tag} (${guildload.owner.id})`)
 	.addField('Prefix:', tableload.prefix)
-	.setAuthor(`${msg.guild.name} (${msg.guild.id})`);
+	.setAuthor(`${guildload.name} (${guildload.id})`);
 	msg.channel.send({ embed });
 };
 
@@ -23,6 +31,6 @@ exports.help = {
 	name: 'prefixfinder',
 	description: 'Command for the LenoxBot Staff to find out a prefix of a guild',
 	usage: 'prefixfinder {guildid}',
-	example: 'randomnumber 352896116812939264',
+	example: 'prefixfinder 352896116812939264',
 	category: 'botowner'
 };
