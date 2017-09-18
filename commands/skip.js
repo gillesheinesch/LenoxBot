ea3b34987852bd581d3190f6a0136eaf18be0eb3
Monkeyyy11 <gaileshd@gmail.com>
2017-09-18 17:58:14 +0200

    Removed a unnecessary msg

--- commands/skip.js
+++ commands/skip.js
@@ -3,8 +3,7 @@ exports.run = (client, msg, args) => {
 	const serverQueue = queue.get(msg.guild.id);
 	if (!msg.member.voiceChannel) return msg.channel.send('You are not in a voice channel, please join a voice channel to play music!');
 	if (!serverQueue) return msg.channel.send('There is nothing playing that I could skip for you.');
-	serverQueue.connection.dispatcher.end();
-	return msg.channel.send('I skipped this song successfully!');
+	return serverQueue.connection.dispatcher.end();
 };
 
 exports.conf = {
