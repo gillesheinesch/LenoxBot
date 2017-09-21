064340c21edb40332b917f43a73a6b9e7e532594
Monkeyyy11 <gaileshd@gmail.com>
2017-09-21 16:11:55 +0200

    The bot can now set a prefix which is longer than 1 character

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,9 +1,9 @@
 exports.run = (client, msg, args) => {
-	const newprefix = args.slice().join(' ');
+	const newprefix = args.slice();
 	const tableconfig = client.guildconfs.get(msg.guild.id);
 
 	if (!newprefix) return msg.channel.send(`The prefix of this server is \`${tableconfig.prefix}\``);
-	if (newprefix.length > 1) return msg.channel.send('Your new prefix can not be longer than 1 character!').then(m => m.delete(10000));
+	if (newprefix.length > 1) return msg.channel.send('Your new prefix cannot have spaces! \nExample: `---`, `=-=` but **not** like `= = ! =`!').then(m => m.delete(10000));
 
 	tableconfig.prefix = newprefix[0];
 	client.guildconfs.set(msg.guild.id, tableconfig);
@@ -19,7 +19,7 @@ exports.help = {
 	name: 'prefix',
 	description: 'Changes the prefix of the server or shows you the current prefix if you just use ?prefix',
 	usage: 'prefix {new prefix}',
-	example: 'randomnumber !',
+	example: 'prefix !=!',
 	category: 'administration'
 };
 
