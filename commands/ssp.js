1021e21a4b7586235731418c8a8dbbdbe187b520
Monkeyyy11 <gaileshd@gmail.com>
2017-09-21 17:56:17 +0200

    beautify

--- commands/ssp.js
+++ commands/ssp.js
@@ -1,105 +1,105 @@
 const Discord = require('discord.js');
 exports.run = (client, msg, args) => {
-	var margs = msg.content.split(" ");
-	let validation = ["scissors", "stone", "paper"];
+	var margs = msg.content.split(' ');
+	let validation = ['scissors', 'stone', 'paper'];
 	let randomofvalidation = validation[Math.floor(Math.random() * validation.length)];
-	
-	if (typeof margs[1] !== "undefined");
-		
-		for (i = 0; i < margs.length; i++) {
+
+	if (typeof margs[1] !== 'undefined');
+
+	for (i = 0; i < margs.length; i++) {
 			if (validation.indexOf(margs[i].toLowerCase()) >= 0) {
-				if (margs[1].toLowerCase() === "stone") {
-					if (randomofvalidation === "stone") {
-			const embed1 = new Discord.RichEmbed()
+				if (margs[1].toLowerCase() === 'stone') {
+					if (randomofvalidation === 'stone') {
+						const embed1 = new Discord.RichEmbed()
 			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
 			.setColor('#0066CC')
 			.setThumbnail('https://cdn.discordapp.com/attachments/339131858283528192/341322660397056003/spieltipp-schere-stein-papier.png')
 			.setFooter(`Scissors, stone and paper minigame`)
 			.setDescription(`**I've chosen stone! ✊** \nDraw!`);
-			msg.channel.send({ embed: embed1 });
-		} else
-			if (randomofvalidation === "scissors") {
-			const embed2 = new Discord.RichEmbed()
+						msg.channel.send({ embed: embed1 });
+					} else
+			if (randomofvalidation === 'scissors') {
+				const embed2 = new Discord.RichEmbed()
 			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
 			.setColor('#0066CC')
 			.setThumbnail('https://cdn.discordapp.com/attachments/339131858283528192/341322660397056003/spieltipp-schere-stein-papier.png')
 			.setFooter(`Scissors, stone and paper minigame`)
 			.setDescription(`**I've chosen scissors! ✌** \nYou won!`);
-			msg.channel.send({ embed: embed2 });
-		} else
-			if (randomofvalidation === "paper") {
+				msg.channel.send({ embed: embed2 });
+			} else
+			if (randomofvalidation === 'paper') {
 				const embed3 = new Discord.RichEmbed()
 			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
 			.setColor('#0066CC')
 			.setThumbnail('https://cdn.discordapp.com/attachments/339131858283528192/341322660397056003/spieltipp-schere-stein-papier.png')
 			.setFooter(`Scissors, stone and paper minigame`)
 			.setDescription(`**I've chosen paper! ✋** \nYou lost!`);
-			msg.channel.send({ embed: embed3 });
+				msg.channel.send({ embed: embed3 });
 			}
-		} else
-		if (margs[1].toLowerCase() === "scissors") {
-			if (randomofvalidation === "paper") {
+				} else
+		if (margs[1].toLowerCase() === 'scissors') {
+			if (randomofvalidation === 'paper') {
 			 const embed4 = new Discord.RichEmbed()
 			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
 			.setColor('#0066CC')
 			.setThumbnail('https://cdn.discordapp.com/attachments/339131858283528192/341322660397056003/spieltipp-schere-stein-papier.png')
 			.setFooter(`Scissors, stone and paper minigame`)
 			.setDescription(`**I've chosen paper! ✋** \nYou won!`);
-			msg.channel.send({ embed: embed4 });
-		} else
-			if (randomofvalidation === "stone") {
+				msg.channel.send({ embed: embed4 });
+			} else
+			if (randomofvalidation === 'stone') {
 				const embed5 = new Discord.RichEmbed()
 			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
 			.setColor('#0066CC')
 			.setThumbnail('https://cdn.discordapp.com/attachments/339131858283528192/341322660397056003/spieltipp-schere-stein-papier.png')
 			.setFooter(`Scissors, stone and paper minigame`)
 			.setDescription(`**I've chosen stone! ✊** \nYou lost!`);
-			msg.channel.send({ embed: embed5 });
-		} else
+				msg.channel.send({ embed: embed5 });
+			} else
 
-			if (randomofvalidation === "scissors") {
-			const embed6 = new Discord.RichEmbed()
+			if (randomofvalidation === 'scissors') {
+				const embed6 = new Discord.RichEmbed()
 			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
 			.setColor('#0066CC')
 			.setThumbnail('https://cdn.discordapp.com/attachments/339131858283528192/341322660397056003/spieltipp-schere-stein-papier.png')
 			.setFooter(`Scissors, stone and paper minigame`)
 			.setDescription(`**I've chosen scissors! ✌** \nDraw!`);
-			msg.channel.send({ embed: embed6 });
-		}
+				msg.channel.send({ embed: embed6 });
+			}
 		} else
-		if (margs[1].toLowerCase() === "paper") {
-			if (randomofvalidation === "scissors") {
-			const embed7 = new Discord.RichEmbed()
+		if (margs[1].toLowerCase() === 'paper') {
+			if (randomofvalidation === 'scissors') {
+				const embed7 = new Discord.RichEmbed()
 			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
 			.setColor('#0066CC')
 			.setThumbnail('https://cdn.discordapp.com/attachments/339131858283528192/341322660397056003/spieltipp-schere-stein-papier.png')
 			.setFooter(`Scissors, stone and paper minigame`)
 			.setDescription(`**I've chosen scissors! ✌** \nYou lost!`);
-			msg.channel.send({ embed: embed7 });
-		} else
+				msg.channel.send({ embed: embed7 });
+			} else
 
-			if (randomofvalidation === "paper") {
-			const embed8 = new Discord.RichEmbed()
+			if (randomofvalidation === 'paper') {
+				const embed8 = new Discord.RichEmbed()
 			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
 			.setColor('#0066CC')
 			.setThumbnail('https://cdn.discordapp.com/attachments/339131858283528192/341322660397056003/spieltipp-schere-stein-papier.png')
 			.setFooter(`scissors, stone and paper minigame`)
 			.setDescription(`**I've chosen paper! ✋** \nDraw!`);
-			msg.channel.send({ embed: embed8 });
-		} else
+				msg.channel.send({ embed: embed8 });
+			} else
 
-			if (randomofvalidation === "stone") {
+			if (randomofvalidation === 'stone') {
 				const embed9 = new Discord.RichEmbed()
 			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
 			.setColor('#0066CC')
 			.setThumbnail('https://cdn.discordapp.com/attachments/339131858283528192/341322660397056003/spieltipp-schere-stein-papier.png')
 			.setFooter(`Scissors, stone and paper minigame`)
 			.setDescription(`**I've chosen stone! ✊** \nYou won!`);
-			msg.channel.send({ embed: embed9 });
-}
-}
-	}
-	}
+				msg.channel.send({ embed: embed9 });
+			}
+		}
+			}
+		}
 };
 
 exports.conf = {
