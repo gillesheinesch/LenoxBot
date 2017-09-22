d82f9ebbf2b098bea87c427e8a471adfd0a73b4c
Monkeyyy11 <gaileshd@gmail.com>
2017-09-22 22:03:00 +0200

    fix

--- events/ready.js
+++ events/ready.js
@@ -51,7 +51,7 @@ exports.run = client => {
 	console.log(`LENXOBOT: Ready to serve in ${client.channels.size} channels on ${client.guilds.size}, for a total of ${client.users.size} users.`);
 	client.user.setPresence({ game: { name: `?help in ${client.guilds.size} guilds`, type: 0 } });
 	client.guilds.filter(g => !client.guildconfs.has(g.id)).forEach(g => client.guildconfs.set(g.id, defaultSettings));
-	client.channels.filter(ch => ch.type === 'text').map(ch => ch.fetchMessages({ limit: 100 }));
+	client.channels.filter(ch => ch.type === 'text' && ch.permissionsFor(client.user).has('READ_MESSAGES')).map(ch => ch.fetchMessages({ limit: 100 }));
 	if (!client.botconfs.has('blackbanlist')) client.botconfs.set('blackbanlist', botconfsdefault);
 	if (!client.botconfs.has('botconfs')) client.botconfs.set('botconfs', botconfs);
 };
