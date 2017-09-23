d9bc5abe6a8ae45eb1de4fb5890319a690fa80e3
Monkeyyy11 <gaileshd@gmail.com>
2017-09-23 13:11:42 +0200

    Added a require permission (Administrator)

--- commands/starboardchannel.js
+++ commands/starboardchannel.js
@@ -1,4 +1,5 @@
 exports.run = (client, msg, args) => {
+	if (!msg.member.hasPermission('ADMINISTRATOR')) return msg.reply('You dont have permissions to execute this command!').then(m => m.delete(10000));	
 	const tableload = client.guildconfs.get(msg.guild.id);
 
 	if (!tableload.starboardchannel) {
