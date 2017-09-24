01f43a7e37d0b885b548694438dd8695420af3c5
Monkeyyy11 <gaileshd@gmail.com>
2017-09-24 09:02:43 +0200

    Fix

--- commands/prefixfinder.js
+++ commands/prefixfinder.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 exports.run = (client, msg, args) => {
 	const guild = client.guilds.get('352896116812939264').roles.find('name', 'Staff').id;
-	// if (!msg.member.roles.get(guild) || msg.author.id !== '238590234135101440') return msg.reply('You dont have permissions to execute this command!').then(m => m.delete(10000));
+	if (!msg.member.roles.get(guild) || msg.author.id !== '238590234135101440') return msg.reply('You dont have permissions to execute this command!').then(m => m.delete(10000));
 
 	const content = args.slice().join(" ");
 	if (!content) return msg.reply('You have to enter a guildid!');
