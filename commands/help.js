9f2d4a290bbeb6e5e41088c47bb435e579ecaa98
Monkeyyy11 <gaileshd@gmail.com>
2017-09-25 17:19:09 +0200

    Fix

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 exports.run = (client, msg, args) => {
 	const prefix = client.guildconfs.get(msg.guild.id).prefix;
 	if (!args[0]) {
-		msg.channel.send('To add this bot to your Discordserver, use this link: https://discordapp.com/oauth2/authorize?client_id=354712333853130752&scope=bot&permissions=8 \n\nYou can use the command `?modules` to see all modules of the bot\nTo see all commands of a module, just use `?commands modulename` \nTo see more details about a command, just use `?help commandname` \n\nYou can join our discord server: **www.discord.gg/5mpwCr8**');
+		msg.channel.send(`To add this bot to your Discordserver, use this link: https://discordapp.com/oauth2/authorize?client_id=354712333853130752&scope=bot&permissions=8 \n\nYou can use the command \`${prefix}modules\` to see all modules of the bot\nTo see all commands of a module, just use \`${prefix}commands modulename\` \nTo see more details about a command, just use \`${prefix}help commandname\` \n\nYou can join our discord server: **www.discord.gg/5mpwCr8**`);
 	} else {
 		let command = args[0];
 		if (client.commands.has(command)) {
