2f5ab46ed5941a09c094fc87fefd4d34e1879e85
Monkeyyy11 <gaileshd@gmail.com>
2017-10-03 19:28:01 +0200

    fix

--- commands/skip.js
+++ commands/skip.js
@@ -31,7 +31,7 @@ exports.run = async(client, msg, args) => {
 	if (mapload.users.length !== number) return undefined;
 
 	msg.channel.send('The song has been skipped by the poll successfully');
-	serverQueue.connection.dispatcher.end();
+	await serverQueue.connection.dispatcher.end();
 	return undefined;
 };
 
