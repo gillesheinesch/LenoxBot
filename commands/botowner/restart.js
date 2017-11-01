493306dc3907fe6211165b266085f003a5439af8
Monkeyyy11 <gaileshd@gmail.com>
2017-11-01 20:57:29 +0100

    Fixed maybe the database bug

--- commands/botowner/restart.js
+++ commands/botowner/restart.js
@@ -1,6 +1,9 @@
 exports.run = async (client, msg, args) => {
     if (msg.author.id !== '238590234135101440') return msg.channel.send('You dont have permissions to execute this command!');
 	await msg.channel.send('The bot now restarts. See you soon! 👋');
+	await client.guildconfs.close();
+	await client.botconfs.close();
+	await client.starboard.close();
 	process.exit(42);
 };
 
