78e2496f283bb64933c9c41463c9372cd5ef2dff
Monkeyyy11 <gaileshd@gmail.com>
2017-11-01 21:00:09 +0100

    beautify

--- commands/botowner/reload.js
+++ commands/botowner/reload.js
@@ -1,5 +1,5 @@
 exports.run = (client, msg, args) => {
-	if (msg.author.id !== '238590234135101440') return msg.channel.send('You dont have permissions to execute this command!');    
+	if (msg.author.id !== '238590234135101440') return msg.channel.send('You dont have permissions to execute this command!');
 	if(!args || args.size < 1) return msg.reply("Must provide a command name to reload.");
 	delete require.cache[require.resolve(`./${args[0]}.js`)];
 	msg.reply(`The command ${args[0]} has been reloaded!`).then(m => m.delete(10000));
