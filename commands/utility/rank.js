a6de0f5d4f0b483c82a7b2968029751d14646289
Monkeyyy11 <gaileshd@gmail.com>
2017-11-18 18:04:05 +0100

    New Update to see now the rank in the rank command

--- commands/utility/rank.js
+++ commands/utility/rank.js
@@ -1,22 +1,38 @@
 const sql = require("sqlite");
 const Discord = require('discord.js');
 sql.open("../lenoxbotscore.sqlite");
-exports.run = (client, msg, args) => {
+exports.run = async(client, msg, args) => {
 	const user1 = msg.mentions.users.first() || msg.author;
 	const embed = new Discord.RichEmbed()
 	.setColor('#A4F2DF')
 	.setThumbnail(user1.avatarURL)
 	.setAuthor(user1.tag, user1.avatarURL);
-		sql.get(`SELECT * FROM scores WHERE guildId ="${msg.guild.id}" AND userId = "${user1.id}"`).then(row => {
-			if (!row) {
-				embed.addField('Points', `0`, true);
-				embed.addField('Level', `0`, true);
-			} else {
+
+	var allmembersArray = [];
+	var i = 0;
+	var rank = 0;
+
+	sql.all(`SELECT * FROM scores WHERE guildId = "${msg.guild.id}" GROUP BY userId ORDER BY points DESC`).then(rows =>
+	rows.forEach(row => {
+		i = i + 1;
+		if (row.userId === user1.id) {
+			rank = i;
+		}
+	}));
+
+	await sql.all(`SELECT (SELECT COUNT (*) FROM scores WHERE guildId = "${msg.guild.id}") AS allmembers`).then(r => allmembersArray.push(r[0].allmembers));
+	await sql.get(`SELECT * FROM scores WHERE guildId ="${msg.guild.id}" AND userId = "${user1.id}"`).then(row => {
+		if (!row) {
+			embed.addField('Points', `0`, true);
+			embed.addField('Level', `0`, true);
+			embed.addField('Rank', `${rank}/${allmembersArray.join('')}`);
+		} else {
 			embed.addField('Points', row.points, true);
 			embed.addField('Level', row.level, true);
-			}
-			return msg.channel.send({ embed });
-		});
+			embed.addField('Rank', `${rank}/${allmembersArray.join('')}`);
+		}
+		return msg.channel.send({ embed });
+	});
 };
 
 exports.conf = {
