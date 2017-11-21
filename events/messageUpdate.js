d3aa004c8a15203fb9b21e103afb1b9185622567
Monkeyyy11 <gaileshd@gmail.com>
2017-11-21 13:49:07 +0100

    Fixed the missing permission check if a user is on the banlist or a guild on the blacklist

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -29,6 +29,27 @@ exports.run = (client, oldMsg, msg) => {
 		cmd = client.commands.get(client.aliases.get(command));
 	}
 	if (cmd) {
+		const banlistembed = new Discord.RichEmbed()
+		.setColor('#FF0000')
+		.setDescription('Unfortunately, this server was set to the bot\'s banlist. All users on this server cannot execute commands of this bot anymore.')
+		.addField('If you have any questions, feel free to join our Discord server', 'https://discord.gg/5mpwCr8')
+		.addField('You can also create a ban appeal:', 'http://bit.ly/2wQ2SYF')
+		.setAuthor(`${msg.guild.name} (${msg.guild.id})`, msg.guild.iconURL);
+
+		const blacklistembed = new Discord.RichEmbed()
+		.setColor('#FF0000')
+		.setDescription('Unfortunately, you were set to the bot\'s blacklist. You cannot execute commands of this bot anymore.')
+		.addField('If you have any questions, feel free to join our Discord server', 'https://discord.gg/5mpwCr8')
+		.addField('You can also create a ban appeal:', 'http://bit.ly/2wQ2SYF')
+		.setAuthor(`${msg.author.tag} (${msg.author.id})`, msg.author.displayAvatarURL);
+
+		const botconfsload = client.botconfs.get('blackbanlist');
+		for (var i = 0; i < botconfsload.banlist.length; i++) {
+			if (msg.guild.id === botconfsload.banlist[i]) return msg.channel.send({ embed: banlistembed });
+	}
+		for (var i = 0; i < botconfsload.blacklist.length; i++) {
+			if (msg.author.id === botconfsload.blacklist[i]) return msg.channel.send({ embed: blacklistembed });
+	}
 		if (cmd.help.botpermissions.every(perm => msg.guild.me.hasPermission(perm)) === false) return msg.channel.send(`It looks like the bot hasn't enough permissions to execute this command! (Required permissions: ${cmd.help.botpermissions.join(', ')})`);
 		if (cmd.conf.userpermissions.every(perm => msg.member.hasPermission(perm)) === false) return msg.channel.send(`It looks like you haven't enough permissions to execute this command! (Required permissions: ${cmd.conf.userpermissions.join(', ')})`);
 			cmd.run(client, msg, args);
