0a40399ee54f157e752468a215979337b4c633c5
Monkeyyy11 <gaileshd@gmail.com>
2017-11-23 09:26:34 +0100

    fix

--- commands/administration/listautomaticrole.js
+++ commands/administration/listautomaticrole.js
@@ -27,7 +27,7 @@ exports.conf = {
 	enabled: true,
 	guildOnly: true,
 	aliases: ['lar'],
-    userpermissions: ['ADMINISTRATION']
+    userpermissions: ['ADMINISTRATOR']
 };
 exports.help = {
 	name: 'listautomaticrole',
