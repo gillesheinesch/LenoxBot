3eb81bdbb9a359fca808a5c7990a2ba40e33d4cf
Monkeyyy11 <gaileshd@gmail.com>
2017-11-28 18:47:36 +0100

    clear command imrpoved. It checks now if the input is a number and deletes 2 messages if your input was to delete 2 messages

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -1,10 +1,12 @@
-exports.run = function(client, msg, args) {
+exports.run = async(client, msg, args) => {
+	if (isNaN(args.slice().join(" "))) return msg.reply('The number of messages, that must be deleted, must be a number!').then(m => m.delete(10000));
+
 	let messagecount = parseInt(args.join(' '));
+
 	if (messagecount > 100) return msg.reply('You can only delete 100 messages at once!').then(m => m.delete(10000));
 	if (messagecount < 2) return msg.reply('You must delete at least 2 messages!').then(m => m.delete(10000));
-	msg.channel.fetchMessages({
-		limit: messagecount
-	}).then(messages => msg.channel.bulkDelete(messages));
+
+	await msg.channel.fetchMessages({ limit: messagecount + 1 }).then(messages => msg.channel.bulkDelete(messages));
 	msg.channel.send(`${messagecount} messages have been successfully deleted! ✅`).then(m => m.delete(5000));
 };
 
