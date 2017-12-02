3137462d1d0482860f258d9427b70f4a37b36188
Monkeyyy11 <gaileshd@gmail.com>
2017-12-02 08:30:06 +0100

    Fixed buggy command [website]

--- commands/help/website.js
+++ commands/help/website.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 exports.run = (client, msg, args) => {
-    const embed = new Discord.RichEmbed
+	const embed = new Discord.RichEmbed()
+	.setColor('0066CC')
     .addField('Here you can find our Trello-Website:', `https://trello.com/b/2IoFBIQ8/lenoxbot`);
     msg.channel.send({ embed });
 };
