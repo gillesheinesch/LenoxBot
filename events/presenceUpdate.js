03e6265258938bb8d13401f7993898d444fdeef3
Monkeyyy11 <gaileshd@gmail.com>
2017-12-05 20:16:23 +0100

    beautify

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -1,17 +1,17 @@
 const Discord = require('discord.js');
 exports.run = (client, oldMember, newMember) => {
-    const tableconfig = client.guildconfs.get(newMember.guild.id);
-    if (tableconfig.presenceupdatelog === 'false') return;
+	const tableconfig = client.guildconfs.get(newMember.guild.id);
+	if (tableconfig.presenceupdatelog === 'false') return;
 
-    const messagechannel = client.channels.get(tableconfig.presenceupdatelogchannel);
-if (oldMember.presence.status !== newMember.presence.status) {
-    const embed = new Discord.RichEmbed()
-    .setColor('#FE2E2E')
-    .setTimestamp()
-    .setAuthor('Presence changed!')
-    .addField(`📎 Member:`, `${oldMember.user.tag} (${oldMember.id})`)
-    .addField(`📤 Old Presence:`, oldMember.presence.status)
-    .addField(`📥 New Presence:`, newMember.presence.status);
-    messagechannel.send({ embed: embed });
+	const messagechannel = client.channels.get(tableconfig.presenceupdatelogchannel);
+	if (oldMember.presence.status !== newMember.presence.status) {
+	const embed = new Discord.RichEmbed()
+	.setColor('#FE2E2E')
+	.setTimestamp()
+	.setAuthor('Presence changed!')
+	.addField(`📎 Member:`, `${oldMember.user.tag} (${oldMember.id})`)
+	.addField(`📤 Old Presence:`, oldMember.presence.status)
+	.addField(`📥 New Presence:`, newMember.presence.status);
+	messagechannel.send({ embed: embed });
 }
 };
