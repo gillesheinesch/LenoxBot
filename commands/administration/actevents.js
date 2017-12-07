481f59276935d5870a9d208949491ec7b3dc7499
Monkeyyy11 <gaileshd@gmail.com>
2017-12-07 18:21:26 +0100

    You see now all disabled/active events with ?events

--- commands/administration/actevents.js
+++ commands/administration/actevents.js
@@ -1,94 +1,121 @@
 const Discord = require('discord.js');
 exports.run = (client, msg, args) => {
     const tableload = client.guildconfs.get(msg.guild.id);
-    if (tableload.modlog === 'false' && tableload.messagedellog === 'false' && tableload.messageupdatelog === 'false' && tableload.channelupdatelog === 'false' && tableload.channelcreatelog === 'false' && tableload.channeldeletelog === 'false' && tableload.guildmemberupdatelog === 'false' && tableload.presenceupdatelog === 'false' && tableload.welcomelog === 'false' && tableload.byelog === 'false' && tableload.rolecreatelog === 'false' && tableload.roledeletelog === 'false' && tableload.roleupdatelog === 'false') return msg.channel.send('There are no active events!');
     const embed = new Discord.RichEmbed()
     .setColor('0066CC')
     .setFooter(`You can get a list of all available events with ${tableload.prefix}listevents`)
-    .setAuthor(`All active events/logs!`);
+    .setAuthor(`All active/disabled events!`);
 
     if (tableload.modlog === 'true') {
         const channelID = tableload.modlogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ modlog active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Modlog active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Modlog disabled', `/`);
     }
 
     if (tableload.messagedellog === 'true') {
         const channelID = tableload.messagedellogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ messagedelete active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Messagedelete active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Messagedelete disabled', `/`);
     }
 
     if (tableload.messageupdatelog === 'true') {
         const channelID = tableload.messageupdatelogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ messageupdate active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Messageupdate active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Messageupdate disabled', `/`);
     }
 
     if (tableload.channelupdatelog === 'true') {
         const channelID = tableload.channelupdatelogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ channelupdate active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Channelupdate active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Channelupdate disabled', `/`);
     }
 
     if (tableload.channelcreatelog === 'true') {
         const channelID = tableload.channelcreatelogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ channelcreate active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Channelcreate active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Channelcreate disabled', `/`);
     }
 
     if (tableload.channeldeletelog === 'true') {
         const channelID = tableload.channeldeletelogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ channeldelete active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Channeldelete active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Channeldelete disabled', `/`);
     }
 
     if (tableload.guildmemberupdatelog === 'true') {
         const channelID = tableload.guildmemberupdatelogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ memberupdate active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Memberupdate active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Memberupdate disabled', `/`);
     }
 
     if (tableload.presenceupdatelog === 'true') {
         const channelID = tableload.presenceupdatelogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ presenceupdate active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Presenceupdate active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Presenceupdate disabled', `/`);
     }
 
     if (tableload.welcomelog === 'true') {
         const channelID = tableload.welcomelogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ userjoin active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Userjoin active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Userjoin disabled', `/`);
     }
 
     if (tableload.byelog === 'true') {
         const channelID = tableload.byelogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ userleft active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Userleft active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Userleft disabled', `/`);
     }
 
     if (tableload.rolecreatelog === 'true') {
         const channelID = tableload.rolecreatelogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ rolecreate active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Rolecreate active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Rolecreate disabled', `/`);
     }
 
     if (tableload.roledeletelog === 'true') {
         const channelID = tableload.roledeletelogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ roledelete active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Roledelete active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Roledelete disabled', `/`);
     }
 
     if (tableload.roleupdatelog === 'true') {
         const channelID = tableload.roleupdatelogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ roleupdate active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Roleupdate active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Roleupdate disabled', `/`);
     }
 
     if (tableload.guildupdatelog === 'true') {
         const channelID = tableload.guildupdatelogchannel;
         const channelName = client.channels.get(channelID).name;
-        embed.addField('✅ guildupdate active', `#${channelName} (${channelID})`);
+        embed.addField('✅ Guildupdate active', `#${channelName} (${channelID})`);
+    } else {
+        embed.addField('❌ Guildupdate disabled', `/`);
     }
     msg.channel.send({ embed });
 };
@@ -101,7 +128,7 @@ exports.conf = {
 };
 exports.help = {
 	name: 'activeevents',
-	description: 'Gives you a list of all active events',
+	description: 'Gives you a list of all active/disabled events',
 	usage: 'activeevents',
     example: ['activeevents'],
     category: 'administration',
