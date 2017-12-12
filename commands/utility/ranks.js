b6949bfb4317767227440b1c84fa08a3a2f0c8d5
Monkeyyy11 <gaileshd@gmail.com>
2017-12-12 20:59:17 +0100

    edited ?ranks to switch pages

--- commands/utility/ranks.js
+++ commands/utility/ranks.js
@@ -2,13 +2,10 @@ const Discord = require('discord.js');
 const sql = require("sqlite");
 sql.open("../lenoxbotscore.sqlite");
 exports.run = async(client, msg, args) => {
-let input = parseInt(args.slice().join(' '));
-if (!input) {
-	sql.all(`SELECT * FROM scores WHERE guildId = "${msg.guild.id}" GROUP BY userId ORDER BY points DESC LIMIT 10`).then(rows => {
-		let embed = new Discord.RichEmbed()
-			.setFooter('Top 10')
-			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
-			.setColor('#A4F2DF');
+	const content = args.slice();
+	let input = parseInt(args.slice().join(' '));
+if (content.length === 0) {
+	const rows = await sql.all(`SELECT * FROM scores WHERE guildId = "${msg.guild.id}" GROUP BY userId ORDER BY points DESC`);
 
 		let userArray = [];
 		let moneyArray = [];
@@ -23,44 +20,68 @@ if (!input) {
 	});
 	for (i = 0; i < userArray.length; i++) {
 		tempArray.push((i+1) + ". " + userArray[i]);
-}
-		embed.addField('Name', tempArray.join('\n'), true);
-		embed.addField('Points', moneyArray.join('\n'), true);
-		embed.addField('Level', levelArray.join('\n'), true);
-				
-		msg.channel.send({ embed });
-	});
-} else {
-if (input > 50) return msg.reply('You can only display TOP 1 to TOP 50!');
-	sql.all(`SELECT * FROM scores WHERE guildId = "${msg.guild.id}" GROUP BY userId ORDER BY points DESC LIMIT ${input}`).then(rows => {
-		let embed = new Discord.RichEmbed()
-			.setFooter(`Top ${input}`)
+	}
+
+	let embed = new Discord.RichEmbed()
+	.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
+	.setColor('#A4F2DF')
+	.addField('Name', tempArray.slice(0, 20).join('\n'), true)
+	.addField('Points', moneyArray.slice(0, 20).join('\n'), true)
+	.addField('Level', levelArray.slice(0, 20).join('\n'), true);
+
+		const message = await msg.channel.send({ embed });
+		
+		await message.react('◀');
+		await message.react('▶');
+		
+		var first = 0;
+		var second = 20;
+		
+		var collector = message.createReactionCollector((reaction, user) => user.id === msg.author.id, { time: 30000 });
+	collector.on('collect', r => {
+		var reactionadd = levelArray.slice(first + 20, second + 20).length;
+		var reactionremove = levelArray.slice(first - 20, second - 20).length;
+		console.log(1);
+		if (r.emoji.name === '▶' && reactionadd !== 0) {
+			const thefirst = moneyArray.slice(first + 20, second + 20);
+			const thesecond = levelArray.slice(first + 20, second + 20);
+			const thethird = tempArray.slice(first + 20, second + 20);
+			console.log(thethird);
+			console.log(2);
+			first = first + 20;
+			second = second + 20;
+
+			const newembed = new Discord.RichEmbed()
+			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
+			.setColor('#A4F2DF')
+			.addField('Name', thethird.join('\n'), true)
+			.addField('Points', thesecond.join('\n'), true)
+			.addField('Level', thefirst.join('\n'), true);
+		
+		message.edit({ embed: newembed });
+	  	} else if (r.emoji.name === '◀' && reactionremove !== 0) {
+			moneyArray = moneyArray.slice(first - 20, second - 20);
+			levelArray = levelArray.slice(first - 20, second - 20);
+			tempArray = tempArray.slice(first - 20, second - 20);
+		
+			first = first - 20;
+			second = second - 20;
+		
+			const newembed = new Discord.RichEmbed()
+			.addField('Name', tempArray.join('\n'), true)
+			.addField('Points', moneyArray.join('\n'), true)
+			.addField('Level', levelArray.join('\n'), true)
 			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
 			.setColor('#A4F2DF');
-
-			let userArray = [];
-			let moneyArray = [];
-			let levelArray = [];
-			let tempArray = [];
-	
-		rows.forEach(row => {
-			const member = msg.guild.member(row.userId);
-			userArray.push(member ? member.displayName : row.userId);
-			moneyArray.push(row.points);
-			levelArray.push(row.level);
-		});
-	  
-		for (i = 0; i < userArray.length; i++) {
-			tempArray.push((i+1) + ". " + userArray[i]);
-	} 
-	embed.addField('Name', tempArray.join('\n'), true);
-	embed.addField('Points', moneyArray.join('\n'), true);
-	embed.addField('Level', levelArray.join('\n'), true);
-
-			msg.channel.send({ embed });
+		
+		message.edit({ newembed });
+		}
+	});
+		collector.on('end',(collected, reason) => {
+		message.react('❌');
 		});
 }
-	};
+};
 
 exports.conf = {
 	enabled: true,
