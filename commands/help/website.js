ac2a615c91b72ac19cd122886e8c65d6a6ef372b
Monkeyyy11 <gaileshd@gmail.com>
2017-12-18 12:17:13 +0100

    Updated the webite command: Added the link to the documentation

--- commands/help/website.js
+++ commands/help/website.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 exports.run = (client, msg, args) => {
 	const embed = new Discord.RichEmbed()
 	.setColor('0066CC')
+	.addField('Here you can find our documentation:', `https://www.monkeyyy11.de/`)
     .addField('Here you can find our Trello-Website:', `https://trello.com/b/2IoFBIQ8/lenoxbot`);
     msg.channel.send({ embed });
 };
