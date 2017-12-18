770b486037014ddf4e4219ed82c3a5d104210e2a
Monkeyyy11 <gaileshd@gmail.com>
2017-12-18 12:15:30 +0100

    beautify

--- commands/botowner/activity.js
+++ commands/botowner/activity.js
@@ -1,23 +1,23 @@
 exports.run = (client, msg, args) => {
-    if (msg.author.id !== '238590234135101440') return msg.channel.send('You dont have permissions to execute this command!');
-    const tableload = client.botconfs.get('botconfs');
-    const channelId = msg.channel.id;
-    if (tableload.activity === false) {
-        tableload.activity = true;
-        tableload.activitychannel = channelId;
-        msg.channel.send('Activity now activated in this channel!');
-    } else {
-        tableload.activity = false;
-        msg.channel.send('Activity now deactivated!');
-    }
-    client.botconfs.set('botconfs', tableload);
+	if (msg.author.id !== '238590234135101440') return msg.channel.send('You dont have permissions to execute this command!');
+	const tableload = client.botconfs.get('botconfs');
+	const channelId = msg.channel.id;
+	if (tableload.activity === false) {
+		tableload.activity = true;
+		tableload.activitychannel = channelId;
+		msg.channel.send('Activity now activated in this channel!');
+	} else {
+		tableload.activity = false;
+		msg.channel.send('Activity now deactivated!');
+	}
+	client.botconfs.set('botconfs', tableload);
 };
 
 exports.conf = {
 	enabled: true,
 	guildOnly: true,
 	aliases: [],
-    userpermissions: []
+	userpermissions: []
 };
 exports.help = {
 	name: 'activity',
@@ -25,5 +25,5 @@ exports.help = {
 	usage: 'activity',
 	example: ['activity'],
 	category: 'botowner',
-    botpermissions: ['SEND_MESSAGES']
+	botpermissions: ['SEND_MESSAGES']
 };
