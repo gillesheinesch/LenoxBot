f7c01b753a050a2d65664c38db7fc610e757efad
Monkeyyy11 <gaileshd@gmail.com>
2017-12-28 21:54:02 +0100

    /

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -74,9 +74,9 @@ exports.run = async(client, guild) => {
 	client.channels.get('353989483517181962').send({ embed: embed });
 
 	const snekfetch = require('snekfetch');
-	/* snekfetch.post(`https://discordbots.org/api/bots/stats`)
+	snekfetch.post(`https://discordbots.org/api/bots/stats`)
 	.set('Authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM1NDcxMjMzMzg1MzEzMDc1MiIsImJvdCI6dHJ1ZSwiaWF0IjoxNTA5NjU3MTkzfQ.dDleV67s0ESxSVUxKxeQ8W_z6n9YwrDrF9ObU2MKgVE')
 	.send({ server_count: client.guilds.size })
 	.then(() => console.log('Updated discordbots.org stats.'))
-	.catch(err => console.error(`Whoops something went wrong: ${err.body}`)); */
+	.catch(err => console.error(`Whoops something went wrong: ${err.body}`));
 };
