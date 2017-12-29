67654ae4bf298ed207cd8b07256dcde1b0eee89c
Monkeyyy11 <gaileshd@gmail.com>
2017-12-29 18:20:32 +0100

    fix

--- commands/botowner/reload.js
+++ commands/botowner/reload.js
@@ -1,7 +1,7 @@
 exports.run = (client, msg, args) => {
 	if (msg.author.id !== '238590234135101440') return msg.channel.send('You dont have permissions to execute this command!');
 	if(!args || args.size < 1) return msg.reply("Must provide a command name to reload.");
-	delete require.cache[require.resolve(`./${args[0]}.js`)];
+	delete require.cache[require.resolve(`./${args[0]}/${args[1]}.js`)];
 	msg.reply(`The command ${args[0]} has been reloaded!`).then(m => m.delete(10000));
 };
 
