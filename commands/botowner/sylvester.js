7c7721e670f36d67cb3430ecba23d8672be37642
Monkeyyy11 <gaileshd@gmail.com>
2017-12-31 19:09:55 +0100

    sylvester

---
+++ commands/botowner/sylvester.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js');
+const moment = require('moment');
+const sql = require('sqlite');
+sql.open("../lenoxbotscore.sqlite");
+require('moment-duration-format');
+exports.run = async(client, msg, args) => {
+	const array = [];
+	
+	const now = new Date().getTime();
+
+	const embed = new Discord.RichEmbed()
+	.setDescription(`The sylvester event has begun! \nTo participate, you only have to react with "🏅". \nWhen you have done that, you will be credited with 500 🏅. \nThen you can call these with the following command ?medals`)
+	.setColor('#ff5050')
+	.setFooter(`Event ends on ${new Date(now + 86400000)}`)
+	.setAuthor('Sylvester Event started!');
+
+	const message = await msg.channel.send({ embed });
+
+	await message.react('🏅');
+
+	var collector = message.createReactionCollector((reaction, user) => reaction.emoji.name === '🏅' && !user.bot, { time: 86400000 });
+	collector.on('collect', r => {
+		if (!array.includes(r.users.last().id)) {
+			array.push(r.users.last().id);
+		
+			sql.get(`SELECT * FROM medals WHERE userId ="${r.users.last().id}"`).then(row => {
+				if (!row) {
+					sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [r.users.last().id, 0]);
+				}
+				sql.run(`UPDATE medals SET medals = ${row.medals + 500} WHERE userId = ${r.users.last().id}`);
+			  }).catch((error) => {
+				console.error(error);
+				sql.run("CREATE TABLE IF NOT EXISTS medals (userId TEXT, medals INTEGER)").then(() => {
+					sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [r.users.last().id, 0]);
+				});
+			});
+		}
+	});
+		collector.on('end',(collected, reason) => {
+		message.delete();
+		});
+};
+
+exports.conf = {
+	enabled: true,
+	guildOnly: false,
+	aliases: [],
+    userpermissions: []
+};
+exports.help = {
+	name: 'sylvester',
+	description: 'Starts an event on the LenoxBot server',
+    usage: 'startevent',
+    example: ['startevent'],
+	category: 'botowner',
+    botpermissions: ['SEND_MESSAGES']
+};
