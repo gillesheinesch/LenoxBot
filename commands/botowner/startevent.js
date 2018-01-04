bc9d38e2e7b92f3d71adb50c4b59fd9bd8f11e58
Monkeyyy11 <gaileshd@gmail.com>
2018-01-04 20:40:57 +0100

    Updates [new guild event]

--- commands/botowner/startevent.js
+++ commands/botowner/startevent.js
@@ -3,55 +3,109 @@ const moment = require('moment');
 const sql = require('sqlite');
 sql.open("../lenoxbotscore.sqlite");
 require('moment-duration-format');
+
 exports.run = async(client, msg, args) => {
-	const array = [];
-	
 	const now = new Date().getTime();
+	const margs = msg.content.split(" ");
 
-	const embed = new Discord.RichEmbed()
-	.setDescription(`The medal collection event has begun! \nTo participate, you only have to react with "🏅". \nWhen you have done that, you will be credited with 100 🏅. \nThen you can call these with the following command ?medals`)
-	.setColor('#ff5050')
-	.setFooter(`Event ends on ${new Date(now + 86400000)}`)
-	.setAuthor('New Event started!');
+	const validation = ['medalevent', 'lenoxbot'];
 
-	const message = await msg.channel.send({ embed });
 
-	await message.react('🏅');
+	for (i = 0; i < margs.length; i++) {
+		if (validation.indexOf(margs[i].toLowerCase()) >= 0) {
+			if (margs[1].toLowerCase() == "medalevent") {
+				var normalevent = [];
 
-	var collector = message.createReactionCollector((reaction, user) => reaction.emoji.name === '🏅' && !user.bot, { time: 86400000 });
-	collector.on('collect', r => {
-		if (!array.includes(r.users.last().id)) {
-			array.push(r.users.last().id);
-		
-			sql.get(`SELECT * FROM medals WHERE userId ="${r.users.last().id}"`).then(row => {
-				if (!row) {
-					sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [r.users.last().id, 0]);
-				}
-				sql.run(`UPDATE medals SET medals = ${row.medals + 100} WHERE userId = ${r.users.last().id}`);
-			  }).catch((error) => {
-				console.error(error);
-				sql.run("CREATE TABLE IF NOT EXISTS medals (userId TEXT, medals INTEGER)").then(() => {
-					sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [r.users.last().id, 0]);
+				const embed = new Discord.RichEmbed()
+					.setDescription(`The medal collection event has begun! \nTo participate, you only have to react with "🏅". \nWhen you have done that, you will be credited with 100 🏅. \nThen you can call these with the following command ?medals`)
+					.setColor('#ff5050')
+					.setFooter(`Event ends on ${new Date(now + 86400000)}`)
+					.setAuthor('New Event started!');
+
+				const message = await msg.channel.send({
+					embed
 				});
-			});
+
+				await message.react('🏅');
+
+				var normaleventcollector = message.createReactionCollector((reaction, user) => reaction.emoji.name === '🏅' && !user.bot, {
+					time: 86400000
+				});
+				normaleventcollector.on('collect', r => {
+					if (!normalevent.includes(r.users.last().id)) {
+						normalevent.push(r.users.last().id);
+
+						sql.get(`SELECT * FROM medals WHERE userId ="${r.users.last().id}"`).then(row => {
+							if (!row) {
+								sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [r.users.last().id, 0]);
+							}
+							sql.run(`UPDATE medals SET medals = ${row.medals + 100} WHERE userId = ${r.users.last().id}`);
+						}).catch((error) => {
+							console.error(error);
+							sql.run("CREATE TABLE IF NOT EXISTS medals (userId TEXT, medals INTEGER)").then(() => {
+								sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [r.users.last().id, 0]);
+							});
+						});
+					}
+				});
+				normaleventcollector.on('end', (collected, reason) => {
+					message.delete();
+				});
+			} else if (margs[1].toLowerCase() == "lenoxbot") {
+				var lenoxbot = [];
+
+				const embed = new Discord.RichEmbed()
+					.setDescription(`The "LenoxBot" SPAM event has begun! \nTo participate, you only have to write "LenoxBot" in the #spam channel. \nWhen you have done that, you will be credited with 100 🏅. \nThen you can call these with the following command ?medals`)
+					.setColor('#ff5050')
+					.setFooter(`Event ends on ${new Date(now + 86400000)}`)
+					.setAuthor('New Event started!');
+
+				const message = await msg.channel.send({
+					embed
+				});
+
+				var spamchannel = msg.guild.channels.find(r => r.name.toLowerCase() === 'spam');
+				var lenoxbotcollector = spamchannel.createMessageCollector(m => m.content.toLowerCase() === 'lenoxbot' && !m.author.bot, {
+					time: 86400000
+				});
+
+				lenoxbotcollector.on('collect', r => {
+					if (!lenoxbot.includes(r.author.id)) {
+						lenoxbot.push(r.author.id);
+
+						sql.get(`SELECT * FROM medals WHERE userId ="${r.author.id}"`).then(row => {
+							if (!row) {
+								sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [r.author.id, 0]);
+							}
+							sql.run(`UPDATE medals SET medals = ${row.medals + 100} WHERE userId = ${r.author.id}`);
+						}).catch((error) => {
+							console.error(error);
+							sql.run("CREATE TABLE IF NOT EXISTS medals (userId TEXT, medals INTEGER)").then(() => {
+								sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [r.author.id, 0]);
+							});
+						});
+					}
+					msg.channel.send(`${msg.author}, You have successfully received 100 medals!`);
+				});
+				normaleventcollector.on('end', (collected, reason) => {
+					message.delete();
+				});
+			}
 		}
-	});
-		collector.on('end',(collected, reason) => {
-		message.delete();
-		});
+	}
 };
 
 exports.conf = {
 	enabled: true,
 	guildOnly: false,
 	aliases: [],
-    userpermissions: []
+	userpermissions: []
 };
 exports.help = {
 	name: 'startevent',
 	description: 'Starts an event on the LenoxBot server',
-    usage: 'startevent',
-    example: ['startevent'],
+	usage: 'startevent',
+	example: ['startevent lenoxbot', 'startevent medalevent'],
 	category: 'botowner',
-    botpermissions: ['SEND_MESSAGES']
+	botpermissions: ['SEND_MESSAGES']
 };
