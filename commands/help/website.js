717d7cb9e1ffd0064ae48378c49acba5c43148c8
Monkeyyy11 <gaileshd@gmail.com>
2018-01-14 15:25:55 +0100

    New Domain

--- commands/help/website.js
+++ commands/help/website.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 exports.run = (client, msg, args, lang) => {
 	const embed = new Discord.RichEmbed()
 	.setColor('0066CC')
-	.addField(lang.website_documentation, `https://www.monkeyyy11.de/`)
+	.addField(lang.website_documentation, `https://www.documentation.lenoxbot.com/`)
 	.addField(lang.website_trello, `https://trello.com/b/2IoFBIQ8/lenoxbot`);
 
     msg.channel.send({ embed });
