d2a1e9930f098035171c12eec2d96b72ceef720e
Monkeyyy11 <gaileshd@gmail.com>
2018-01-20 13:00:41 +0100

    Removed the feature to add a specific color when you create a role

--- commands/administration/createrole.js
+++ commands/administration/createrole.js
@@ -2,12 +2,10 @@ const Discord = require('discord.js');
 exports.run = async(client, msg, args, lang) => {
 	const input = args.slice();
 
-	if (input.length === 0) return msg.channel.send(lang.createrole_nocolor);
-	if (input.length === 1) return msg.channel.send(lang.createrole_norolename);
+	if (input.length === 0) return msg.channel.send(lang.createrole_norolename);
 
 	msg.guild.createRole({
-		name: args.slice(1).join(" "),
-		color: input[0]
+		name: args.slice().join(" ")
 	}).then(msg.channel.send(lang.createrole_rolecreated));
 };
 
@@ -19,9 +17,9 @@ exports.conf = {
 };
 exports.help = {
 	name: 'createrole',
-	description: 'Creates a role with a color and a name',
-	usage: 'createrole {Color (Can be a Hex Literal, Hex String, Number)} {rolename}',
-	example: ['createrole #336600 Test', 'createrole 255,255,255 Test', 'createrole 11,43%,55% Test'],
+	description: 'Creates a role with a name',
+	usage: 'createrole {rolename}',
+	example: ['createrole Test'],
 	category: 'administration',
 	botpermissions: ['SEND_MESSAGES', 'MANAGE_ROLES']
 };
