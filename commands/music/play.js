8bb0c515c3b48785873dbbcfc0cdd8a4aa9349ad
Monkeyyy11 <gaileshd@gmail.com>
2018-01-28 18:45:16 +0100

    Music should now start faster

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,8 +1,11 @@
 const Discord = require('discord.js');
-exports.run = async(client, msg, args, lang) => {
+exports.run = async (client, msg, args, lang) => {
 	const config = require('../../settings.json');
-	const tableload = client.guildconfs.get(msg.guild.id)
-	const { Client, Util } = require('discord.js');
+	const tableload = client.guildconfs.get(msg.guild.id);
+	const {
+		Client,
+		Util
+	} = require('discord.js');
 	const YouTube = require('simple-youtube-api');
 	const youtube = new YouTube(config.googlekey);
 	const queue = client.queue;
@@ -41,15 +44,19 @@ exports.run = async(client, msg, args, lang) => {
 
 				let index = 0;
 				const embed = new Discord.RichEmbed()
-				.setColor('#7BB3FF')
-				.setDescription(`${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}`)
-				.setAuthor(lang.play_songselection, 'https://cdn.discordapp.com/attachments/355972323590930432/357097120580501504/unnamed.jpg');
+					.setColor('#7BB3FF')
+					.setDescription(`${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}`)
+					.setAuthor(lang.play_songselection, 'https://cdn.discordapp.com/attachments/355972323590930432/357097120580501504/unnamed.jpg');
 
 				const embed2 = new Discord.RichEmbed()
-				.setColor('#0066CC')
-				.setDescription(lang.play_value);
-				msg.channel.send({ embed });
-				msg.channel.send({ embed: embed2 });
+					.setColor('#0066CC')
+					.setDescription(lang.play_value);
+				msg.channel.send({
+					embed
+				});
+				msg.channel.send({
+					embed: embed2
+				});
 				try {
 					var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11 && msg.author.id === msg2.author.id, {
 						maxMatches: 1,
@@ -68,83 +75,84 @@ exports.run = async(client, msg, args, lang) => {
 		return handleVideo(video, msg, voiceChannel);
 	}
 	async function handleVideo(video, msg, voiceChannel, playlist = false) {
-			const serverQueue = queue.get(msg.guild.id);
-			const song = {
-				id: video.id,
-				title: Util.escapeMarkdown(video.title),
-				url: `https://www.youtube.com/watch?v=${video.id}`
+		const serverQueue = queue.get(msg.guild.id);
+		const song = {
+			id: video.id,
+			title: Util.escapeMarkdown(video.title),
+			url: `https://www.youtube.com/watch?v=${video.id}`
+		};
+		if (!serverQueue) {
+			const queueConstruct = {
+				textChannel: msg.channel,
+				voiceChannel: voiceChannel,
+				connection: null,
+				songs: [],
+				volume: 5,
+				playing: true
 			};
-			if (!serverQueue) {
-				const queueConstruct = {
-					textChannel: msg.channel,
-					voiceChannel: voiceChannel,
-					connection: null,
-					songs: [],
-					volume: 5,
-					playing: true
-				};
-				await queue.set(msg.guild.id, queueConstruct);
+			await queue.set(msg.guild.id, queueConstruct);
 
-				await queueConstruct.songs.push(song);
-
-				const vote = {
-					users: []
-				};
-
-				skipvote.set(msg.guild.id, vote);
-
-				try {
-					var connection = await voiceChannel.join();
-					queueConstruct.connection = connection;
-					await play(msg.guild, queueConstruct.songs[0]);
-				} catch (error) {
-					await queue.delete(msg.guild.id);
-					await skipvote.delete(msg.guild.id);
-					return msg.channel.send(lang.play_errorjoin);
-				}
-			} else {
-				await serverQueue.songs.push(song);
-				if (playlist) return undefined;
-				else {
-					var songadded = lang.play_songadded.replace('%songtitle', `**${song.title}**`);
-					return msg.channel.send(songadded);
-				}
-			}
-			return undefined;
-		}
-
-	async function play(guild, song) {
-			const serverQueue = await queue.get(guild.id);
-		
-			if (!song) {
-				await serverQueue.voiceChannel.leave();
-				await queue.delete(guild.id);
-				return undefined;
-			}
-			const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
-				.on('end', async reason => {
-					if (reason === 'Stream is not generating quickly enough.');
-					await serverQueue.songs.shift('Stream is not generating quickly enough');
-					await play(guild, serverQueue.songs[0]);
-				})
-				.on('error', error => console.error(error));
-			dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+			await queueConstruct.songs.push(song);
 
 			const vote = {
 				users: []
 			};
+
 			skipvote.set(msg.guild.id, vote);
 
-			var startplaying = lang.play_startplaying.replace('%songtitle', `**${song.title}**`);
-			serverQueue.textChannel.send(startplaying);
+			try {
+				var connection = await voiceChannel.join();
+				queueConstruct.connection = connection;
+				await play(msg.guild, queueConstruct.songs[0]);
+			} catch (error) {
+				await queue.delete(msg.guild.id);
+				await skipvote.delete(msg.guild.id);
+				return msg.channel.send(lang.play_errorjoin);
+			}
+		} else {
+			await serverQueue.songs.push(song);
+			if (playlist) return undefined;
+			else {
+				var songadded = lang.play_songadded.replace('%songtitle', `**${song.title}**`);
+				return msg.channel.send(songadded);
+			}
 		}
+		return undefined;
+	}
+
+	async function play(guild, song) {
+		const serverQueue = await queue.get(guild.id);
+
+		if (!song) {
+			await serverQueue.voiceChannel.leave();
+			await queue.delete(guild.id);
+			return undefined;
+		}
+		const dispatcher = await serverQueue.connection.playStream(ytdl(song.url), { filter: 'audioonly' })
+			.on('end', async reason => {
+				console.log(reason);
+				if (reason === 'Stream is not generating quickly enough.');
+					serverQueue.songs.shift('Stream is not generating quickly enough');
+					await play(guild, serverQueue.songs[0]);
+			})
+			.on('error', error => console.error(error));
+		dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+
+		const vote = {
+			users: []
+		};
+		skipvote.set(msg.guild.id, vote);
+
+		var startplaying = lang.play_startplaying.replace('%songtitle', `**${song.title}**`);
+		serverQueue.textChannel.send(startplaying);
+	}
 };
 
 exports.conf = {
 	enabled: true,
 	guildOnly: false,
 	aliases: [],
-    userpermissions: []
+	userpermissions: []
 };
 
 exports.help = {
@@ -153,5 +161,5 @@ exports.help = {
 	usage: 'play {query}',
 	example: ['play Gangnam Style'],
 	category: 'music',
-    botpermissions: ['SEND_MESSAGES', 'CONNECT', 'SPEAK']
+	botpermissions: ['SEND_MESSAGES', 'CONNECT', 'SPEAK']
 };
