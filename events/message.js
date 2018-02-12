49c6972e93f4269f2f624c110e5d6c928e230513
Monkeyyy11 <gaileshd@gmail.com>
2018-02-12 09:58:05 +0100

    fixed

--- events/message.js
+++ events/message.js
@@ -6,6 +6,11 @@ exports.run = async(client, msg) => {
 
 	const tableload = await client.guildconfs.get(msg.guild.id);
 
+	if (!tableload.language === 'de') {
+		tableload.language = 'ge';
+		await client.guildconfs.set(msg.guild.id, tableload);
+	}
+
 	if (!tableload.application.denyrole) {
 		tableload.application.denyrole = '';
 		await client.guildconfs.set(msg.guild.id, tableload);
