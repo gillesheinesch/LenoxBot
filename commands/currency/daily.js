cef07fc717d0e24d23c0e4c1e8fa6c99b040f611
Monkeyyy11 <gaileshd@gmail.com>
2018-02-13 17:24:07 +0100

    New Command [?daily] to get the daily reward

---
+++ commands/currency/daily.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+const sql = require('sqlite');
+sql.open("../lenoxbotscore.sqlite");
+exports.run = async(client, msg, args, lang) => {
+	const mentioncheck = msg.mentions.users.first();
+	const userdb = client.userdb.get(msg.author.id);
+
+	if (!mention) {
+		sql.get(`SELECT * FROM medals WHERE userId ="${msg.author.id}"`).then(row => {
+			if (!row) {
+				sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [msg.author.id, 0]);
+			}
+			sql.run(`UPDATE medals SET medals = ${row.medals + 200 + userdb.dailystreak} WHERE userId = ${msg.author.id}`);
+		  });
+
+		  userdb.dailystreak = userdb.dailystreak + 1;
+		  await client.userdb.set(msg.author.id, userdb);
+
+		  const author = lang.daily_author.replace('%amount', `$${200 + userdb.dailystreak}`);
+		  return msg.reply(author);
+	} else {
+		sql.get(`SELECT * FROM medals WHERE userId ="${mentioncheck.id}"`).then(row => {
+			if (!row) {
+				sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [mentioncheck.id, 0]);
+			}
+			sql.run(`UPDATE medals SET medals = ${row.medals + 200 + userdb.dailystreak} WHERE userId = ${mentioncheck.id}`);
+		  });
+
+		userdb.dailystreak = userdb.dailystreak + 1;
+		await client.userdb.set(msg.author.id, userdb);
+
+		const mention = lang.daily_mention.replace('%mentiontag', mentioncheck.tag).replace('%amount', `$${200 + userdb.dailystreak}`);
+		msg.reply(mention);
+	}
+};
+
+exports.conf = {
+	enabled: true,
+	guildOnly: true,
+	aliases: ['d'],
+	userpermissions: [],
+	cooldown: 86400000
+};
+exports.help = {
+	name: 'daily',
+	description: 'Get your daily reward or give it away to another discord user',
+	usage: 'daily [@User]',
+	example: ['daily', 'daily @Tester#3873'],
+	category: 'currency',
+    botpermissions: ['SEND_MESSAGES']
+};
+
