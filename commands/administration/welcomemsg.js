718f982222b905d65ab853ece93fec587e2b8ee7
Monkeyyy11 <gaileshd@gmail.com>
2018-02-18 17:13:36 +0100

    fix

--- commands/administration/welcomemsg.js
+++ commands/administration/welcomemsg.js
@@ -21,7 +21,7 @@ exports.help = {
 	name: 'welcomemsg',
     description: 'Sets a welcome message to greet your users',
     usage: 'welcomemsg {welcome msg}',
-    example: ['welcomemsg Hello $user$, welcome on the $servername$ discord-server!'],
+    example: ['welcomemsg Hello $username$, welcome on the $servername$ discord-server!'],
 	category: 'administration',
     botpermissions: ['SEND_MESSAGES']
 };
