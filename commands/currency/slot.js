b69a4f69cee7581ba82d14043d1152947d0ea496
Monkeyyy11 <gaileshd@gmail.com>
2018-03-06 17:06:22 +0100

    slot moved into currency module because you play now with credits

--- commands/currency/slot.js
+++ commands/currency/slot.js
@@ -1,7 +1,11 @@
 const slotThing = [':grapes:', ':tangerine:', ':pear:', ':cherries:'];
 const Discord = require('discord.js');
+const sql = require('sqlite');
+sql.open("../lenoxbotscore.sqlite");
+exports.run = async(client, msg, args, lang) => {
+	var msgauthortable = await sql.get(`SELECT * FROM medals WHERE userId ="${msg.author.id}"`);
+	if (msgauthortable.medals < 50) return msg.channel.send(lang.slot_error);
 
-exports.run = (client, msg, args, lang) => {
 	const slotOne = slotThing[Math.floor(Math.random() * slotThing.length)];
 	const slotTwo = slotThing[Math.floor(Math.random() * slotThing.length)];
 	const slotThree = slotThing[Math.floor(Math.random() * slotThing.length)];
@@ -11,6 +15,13 @@ exports.run = (client, msg, args, lang) => {
 		.setColor('#3ADF00')
 		.addField(`${slotOne}|${slotTwo}|${slotThree}`, lang.slot_triple);
 		msg.channel.send({ embed: embed1 });
+
+		await sql.get(`SELECT * FROM medals WHERE userId ="${msg.author.id}"`).then(row => {
+			if (!row) {
+				sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [msg.author.id, 0]);
+			}
+			sql.run(`UPDATE medals SET medals = ${row.medals + 100} WHERE userId = ${msg.author.id}`);
+		});
 	} else 
 	if (slotOne === slotTwo || slotTwo === slotThree) {
 		const embed3 = new Discord.RichEmbed()
@@ -18,12 +29,26 @@ exports.run = (client, msg, args, lang) => {
 		.setColor('#3ADF00')
 		.addField(`${slotOne}|${slotTwo}|${slotThree}`, lang.slot_double);
 		msg.channel.send({ embed: embed3 });
+
+		await sql.get(`SELECT * FROM medals WHERE userId ="${msg.author.id}"`).then(row => {
+			if (!row) {
+				sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [msg.author.id, 0]);
+			}
+			sql.run(`UPDATE medals SET medals = ${row.medals + 25} WHERE userId = ${msg.author.id}`);
+		});
 	} else {
 	const embed2 = new Discord.RichEmbed()
 	.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
 	.setColor('#3ADF00')
 	.addField(`${slotOne}|${slotTwo}|${slotThree}`, lang.slot_nothing);
 		msg.channel.send({ embed: embed2 });
+
+		await sql.get(`SELECT * FROM medals WHERE userId ="${msg.author.id}"`).then(row => {
+			if (!row) {
+				sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [msg.author.id, 0]);
+			}
+			sql.run(`UPDATE medals SET medals = ${row.medals - 50} WHERE userId = ${msg.author.id}`);
+		});
 	}
 };
 
@@ -35,9 +60,9 @@ exports.conf = {
 };
 exports.help = {
 	name: 'slot',
-	description: 'Play with the slot machine',
+	description: 'Play a round with the slot machine',
 	usage: 'slot',
 	example: ['slot'],
-	category: 'fun',
+	category: 'currency',
     botpermissions: ['SEND_MESSAGES']
 };
