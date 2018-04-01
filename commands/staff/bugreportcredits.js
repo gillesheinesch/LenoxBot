51780902e0e4b169da84ee3bbdf717db8e0ea65f
Monkeyyy11 <gaileshd@gmail.com>
2018-04-01 22:12:55 +0200

    the lenoxbot staff has now to enter the ticket ID when they give credits to somebody

--- commands/staff/bugreportcredits.js
+++ commands/staff/bugreportcredits.js
@@ -5,35 +5,36 @@ exports.run = async (client, msg, args) => {
 	const guild = client.guilds.get('352896116812939264').roles.find('name', 'Moderator').id;
 	if (!msg.member.roles.get(guild)) return msg.reply('You dont have permissions to execute this command!');
 
-	const content = args.slice().join(" ");
-	if (!content) return msg.reply('You have to enter a UserID');
+	const content = args.slice();
 
-	if (isNaN(content)) return msg.channel.send('It must be a UserID');
+	if (!content || content.length < 2) return msg.reply('You used the command incorretly! You have to enter the UserID and after that, the Ticket ID!');
+
+	if (isNaN(content[0]) || isNaN(content[1])) return msg.channel.send('It must be a UserID and a TicketID');
 
 	try {
-		await client.users.get(content);
+		await client.users.get(content[0]);
 	} catch (error) {
 		return msg.reply(`Couldn't find this user!`);
 	}
 
-	await sql.get(`SELECT * FROM medals WHERE userId ="${content}"`).then(row => {
+	await sql.get(`SELECT * FROM medals WHERE userId ="${content[0]}"`).then(row => {
 		if (!row) {
-			sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [content, 1000]);
+			sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [content[0], 1000]);
 		} else {
-		sql.run(`UPDATE medals SET medals = ${row.medals + 1000} WHERE userId = ${content}`);
+		sql.run(`UPDATE medals SET medals = ${row.medals + 1000} WHERE userId = ${content[0]}`);
 		}
 	}).catch((error) => {
 		console.error(error);
 		sql.run("CREATE TABLE IF NOT EXISTS medals (userId TEXT, medals INTEGER)").then(() => {
-			sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [content, 1000]);
+			sql.run("INSERT INTO medals (userId, medals) VALUES (?, ?)", [content[0], 1000]);
 		});
 	});
 
-	const searcheduser = client.users.get(content);
+	const searcheduser = client.users.get(content[0]);
 
 	const embed = new Discord.RichEmbed()
 		.setColor('#FF7F24')
-		.setTitle('1000 Credits given because of an accepted bugreport!')
+		.setTitle(`1000 Credits given because of an accepted bugreport! ID: ${content[1]}`)
 		.setAuthor(`${searcheduser.tag} (${searcheduser.id})`, searcheduser.displayAvatarURL)
 		.setFooter(`Requested by ${msg.author.tag}`);
 	client.channels.get('425752252180070401').send({
