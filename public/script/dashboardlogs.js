c0d5bd5ba9bf674891841ca900853ba248e7c61f
Monkeyyy11 <gaileshd@gmail.com>
2018-04-28 17:33:39 +0200

    set alert timer to slide up to 5000ms (for saved settings)

--- public/script/dashboardlogs.js
+++ public/script/dashboardlogs.js
@@ -1,7 +1,7 @@
 // JavaScript source code
 // Eventregistration
 $("document").ready(function () {
-    $("#success-alert").fadeTo(2000, 500).slideUp(1000, function () {
+    $("#success-alert").fadeTo(5000, 500).slideUp(1000, function () {
         $("#success-alert").slideUp(500);
     });
 });
