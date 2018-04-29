d2104e69cef31fd2109089cf218d667f32d5a191
Monkeyyy11 <gaileshd@gmail.com>
2018-04-29 08:57:52 +0200

    increased the duration of the alert to slide up from 2500 to 5000

--- public/script/dashboardlogs.js
+++ public/script/dashboardlogs.js
@@ -2,6 +2,6 @@
 // Eventregistration
 $("document").ready(function () {
     $("#success-alert").fadeTo(5000, 500).slideUp(1000, function () {
-        $("#success-alert").slideUp(500);
+        $("#success-alert").slideUp(1000);
     });
 });
