e1ac0ef47a13c867024cffa237fdbc14e1c39747
Monkeyyy11 <gaileshd@gmail.com>
2018-05-14 17:42:28 +0200

    improvments

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -1,24 +1,24 @@
 const Discord = require('discord.js');
 exports.run = (client, channel) => {
-    if (channel.type !== 'text') return;
+	if (channel.type !== 'text') return;
 
-    const tableload = client.guildconfs.get(channel.guild.id);
-    if (tableload.channeldeletelog === 'false') return;
+	const tableload = client.guildconfs.get(channel.guild.id);
+	if (tableload.channeldeletelog === 'false') return;
 
-    if (tableload.language === '') {
-        tableload.language = 'en';
-        client.guildconfs.set(channel.guild.id, tableload);
+	if (tableload.language === '') {
+		tableload.language = 'en';
+		client.guildconfs.set(channel.guild.id, tableload);
 	}
 
-    var lang = require(`../languages/${tableload.language}.json`);
+	var lang = require(`../languages/${tableload.language}.json`);
 
-    const messagechannel = client.channels.get(tableload.channeldeletelogchannel);
+	const messagechannel = client.channels.get(tableload.channeldeletelogchannel);
 
-    const embed = new Discord.RichEmbed()
-    .setColor('#FE2E2E')
-    .setTimestamp()
-    .setAuthor(lang.channeldeleteevent_channeldeleted)
-    .addField(`📎 ${lang.channelcreateevent_channelid}:`, channel.id)
-    .addField(`📝 ${lang.channelcreateevent_name}:`, channel.name);
-    messagechannel.send({ embed: embed });
+	const embed = new Discord.RichEmbed()
+	.setColor('#FE2E2E')
+	.setTimestamp()
+	.setAuthor(lang.channeldeleteevent_channeldeleted)
+	.addField(`📎 ${lang.channelcreateevent_channelid}:`, channel.id)
+	.addField(`📝 ${lang.channelcreateevent_name}:`, channel.name);
+	messagechannel.send({ embed: embed });
 };
--- events/channelUpdate.js
+++ events/channelUpdate.js
@@ -1,47 +1,47 @@
 const Discord = require('discord.js');
 exports.run = (client, oldChannel, newChannel) => {
-    if (newChannel.type !== 'text' || oldChannel.type !== 'text') return;
+	if (newChannel.type !== 'text' || oldChannel.type !== 'text') return;
 
-    const tableload = client.guildconfs.get(newChannel.guild.id);
-    if (tableload.channelupdatelog === 'false') return;
+	const tableload = client.guildconfs.get(newChannel.guild.id);
+	if (tableload.channelupdatelog === 'false') return;
 
-    if (tableload.language === '') {
-        tableload.language = 'en';
-        client.guildconfs.set(newChannel.guild.id, tableload);
+	if (tableload.language === '') {
+		tableload.language = 'en';
+		client.guildconfs.set(newChannel.guild.id, tableload);
 	}
 
-    var lang = require(`../languages/${tableload.language}.json`);
+	var lang = require(`../languages/${tableload.language}.json`);
 
-    const messagechannel = client.channels.get(tableload.channelupdatelogchannel);
+	const messagechannel = client.channels.get(tableload.channelupdatelogchannel);
 
-    if (oldChannel.name !== newChannel.name) {
-    const embed = new Discord.RichEmbed()
-    .setColor('#FE2E2E')
-    .setTimestamp()
-    .setAuthor(lang.channelupdateevent_nameupdated)
-    .addField(`📎 ${lang.channelcreateevent_channelid}:`, oldChannel.id)
-    .addField(`📤 ${lang.channelupdateevent_oldname}`, oldChannel.name)
-    .addField(`📥 ${lang.channelupdateevent_newname}`, newChannel.name);
-    return messagechannel.send({ embed: embed });
+	if (oldChannel.name !== newChannel.name) {
+	const embed = new Discord.RichEmbed()
+	.setColor('#FE2E2E')
+	.setTimestamp()
+	.setAuthor(lang.channelupdateevent_nameupdated)
+	.addField(`📎 ${lang.channelcreateevent_channelid}:`, oldChannel.id)
+	.addField(`📤 ${lang.channelupdateevent_oldname}`, oldChannel.name)
+	.addField(`📥 ${lang.channelupdateevent_newname}`, newChannel.name);
+	return messagechannel.send({ embed: embed });
 }
 if (oldChannel.topic !== newChannel.topic) {
-    const embed = new Discord.RichEmbed()
-    .setColor('#FE2E2E')
-    .setTimestamp()
-    .setAuthor(lang.channelupdateevent_topicupdated)
-    .addField(`📎 ${lang.channelcreateevent_channelid}`, oldChannel.id)
-    .addField(`📤 ${lang.channelupdateevent_oldtopic}`, oldChannel.topic)
-    .addField(`📥 ${lang.channelupdateevent_newtopic}`, newChannel.topic);
-    return messagechannel.send({ embed: embed });
+	const embed = new Discord.RichEmbed()
+	.setColor('#FE2E2E')
+	.setTimestamp()
+	.setAuthor(lang.channelupdateevent_topicupdated)
+	.addField(`📎 ${lang.channelcreateevent_channelid}`, oldChannel.id)
+	.addField(`📤 ${lang.channelupdateevent_oldtopic}`, oldChannel.topic)
+	.addField(`📥 ${lang.channelupdateevent_newtopic}`, newChannel.topic);
+	return messagechannel.send({ embed: embed });
 } 
 if (oldChannel.position !== newChannel.position) {
-    const embed = new Discord.RichEmbed()
-    .setColor('#FE2E2E')
-    .setTimestamp()
-    .setAuthor(lang.channelupdateevent_positionupdated)
-    .addField(`📎 ${lang.channelcreateevent_channelid}`, oldChannel.id)
-    .addField(`📤 ${lang.channelupdateevent_oldposition}`, oldChannel.position)
-    .addField(`📥 ${lang.channelupdateevent_newposition}`, newChannel.position);
-    return messagechannel.send({ embed: embed });
+	const embed = new Discord.RichEmbed()
+	.setColor('#FE2E2E')
+	.setTimestamp()
+	.setAuthor(lang.channelupdateevent_positionupdated)
+	.addField(`📎 ${lang.channelcreateevent_channelid}`, oldChannel.id)
+	.addField(`📤 ${lang.channelupdateevent_oldposition}`, oldChannel.position)
+	.addField(`📥 ${lang.channelupdateevent_newposition}`, newChannel.position);
+	return messagechannel.send({ embed: embed });
 }
 };
