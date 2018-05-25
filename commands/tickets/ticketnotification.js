fb11f66d5afa01c72f0fe13dd73faa37276a11dd
Monkeyyy11 <gaileshd@gmail.com>
2018-05-25 19:21:31 +0200

    new command ?ticketnotification

---
+++ commands/tickets/ticketnotification.js
@@ -0,0 +1,32 @@
+exports.run = async(client, msg, args, lang) => {
+	const tableload = client.guildconfs.get(msg.guild.id);
+	const channelid = msg.channel.id;
+	if (tableload.tickets.status === false) {
+		tableload.tickets.status = true;
+		tableload.tickets.notificationchannel = channelid;
+		await client.guildconfs.set(msg.guild.id, tableload);
+
+		var channelset = lang.ticketnotification_channelset.replace('%channelname', `**#${msg.channel.name}**`);
+		return msg.channel.send(channelset);
+	} else {
+		tableload.tickets.status = false;
+		await client.guildconfs.set(msg.guild.id, tableload);
+		
+		return msg.channel.send(lang.ticketnotification_channeldeleted);
+	}
+};
+
+exports.conf = {
+	enabled: true,
+	guildOnly: true,
+	aliases: [],
+	userpermissions: ['ADMINISTRATOR']
+};
+exports.help = {
+	name: 'ticketnotification',
+	description: 'Defines a channel in which ticket-notifications are sent',
+	usage: 'ticketnotification',
+	example: ['ticketnotification'],
+	category: 'tickets',
+	botpermissions: ['SEND_MESSAGES']
+};
