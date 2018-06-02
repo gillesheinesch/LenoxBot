f618cd396a7ae3ea50632a8e0f2d9a7722ce585e
Monkeyyy11 <gaileshd@gmail.com>
2018-06-02 22:23:06 +0200

    some changes

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,11 +1,12 @@
-exports.run = (client, oldMember, newMember) => {
+exports.run = async (client, oldMember, newMember) => {
 	const queue = client.queue;
 	const serverQueue = queue.get(newMember.guild.id);
+	console.log(serverQueue);
 	if (!serverQueue) return;
 	if (!oldMember.voiceChannel) return;
 
 	if (oldMember.voiceChannel.members.size === 1) {
 		serverQueue.songs = [];
-		serverQueue.connection.dispatcher.destroy();
+		await serverQueue.connection.dispatcher.destroy();
 	}
 };
