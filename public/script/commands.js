5fe534c2aec526a13c0268bca98bdfaeabf2d121
Monkeyyy11 <gaileshd@gmail.com>
2018-06-06 17:54:38 +0200

    added commands search box

--- public/script/commands.js
+++ public/script/commands.js
@@ -255,6 +255,12 @@ $("document").ready(function () {
 		}
 		document.getElementById("allcommands-button").classList.add('active');
 	});
+	$("#myInput").on("keyup", function () {
+		var value = $(this).val().toLowerCase();
+		$("#myTable tr").filter(function () {
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+		});
+	});
 });
 
 function scrollTo(id) {
