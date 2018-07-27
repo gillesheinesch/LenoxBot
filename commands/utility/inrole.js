9861ac25b5d213a27c3fd75bca1360bee4cd1292
Monkeyyy11 <gaileshd@gmail.com>
2018-07-27 18:37:45 +0200

    some changes for the ?inrole command

--- commands/utility/inrole.js
+++ commands/utility/inrole.js
@@ -1,33 +1,32 @@
 const Discord = require('discord.js');
 exports.run = (client, msg, args, lang) => {
-    if (!args.slice().length >= 1) return msg.channel.send(lang.inrole_noinput);
-    try {
-        const role = msg.guild.roles.find(r => r.name.toLowerCase() === args.slice().join(' ').toLowerCase());
-        var inRole = role.members.array();
-        var array = [];
-        var inRoleArray = inRole.forEach(function(element){
-            array.push(element.user.tag);
-        })
+	if (!args.slice().length >= 1) return msg.channel.send(lang.inrole_noinput);
+	try {
+		const role = msg.guild.roles.find(r => r.name.toLowerCase() === args.slice().join(' ').toLowerCase());
+		var inRole = role.members.array();
+		var array = [];
+		inRole.forEach(function (element) {
+			array.push(element.user.tag);
+		})
 
-        const embed = new Discord.RichEmbed()
-        .setDescription(array.join(', '))
-        .setColor('#ABCDEF')
-        .setAuthor(`${role.name} (${array.length})`);
-        msg.channel.send({ embed });
-    } catch (error) {
-        msg.channel.send(lang.inrole_rolenotexist);
-    }
+		const embed = new Discord.RichEmbed()
+			.setDescription(array.join(', '))
+			.setColor('#ABCDEF')
+			.setAuthor(`${role.name} (${array.length} ${lang.inrole_members})`);
+		msg.channel.send({
+			embed
+		});
+	} catch (error) {
+		msg.channel.send(lang.inrole_rolenotexist);
+	}
 };
 
 exports.conf = {
 	enabled: true,
 	guildOnly: true,
 	aliases: [],
-
-    
-
-    userpermissions: [], dashboardsettings: true
-
+	userpermissions: [],
+	dashboardsettings: true
 };
 exports.help = {
 	name: 'inrole',
@@ -35,5 +34,5 @@ exports.help = {
 	usage: 'inrole {rolename}',
 	example: ['inrole Admin'],
 	category: 'utility',
-    botpermissions: ['SEND_MESSAGES']
+	botpermissions: ['SEND_MESSAGES']
 };
