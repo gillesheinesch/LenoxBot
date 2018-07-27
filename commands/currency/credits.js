b164e6d40a41c271a52a4fc33f22470c9914cc4b
Monkeyyy11 <gaileshd@gmail.com>
2018-07-27 17:02:51 +0200

    new alias for ?credits = ?c

--- commands/currency/credits.js
+++ commands/currency/credits.js
@@ -32,10 +32,7 @@ exports.run = async(client, msg, args, lang) => {
 exports.conf = {
 	enabled: true,
 	guildOnly: false,
-	aliases: ['balance'],
-
-	
-
+	aliases: ['balance', 'c'],
 	userpermissions: [], dashboardsettings: false
 
 };
