685d55cb3cbef54d92336ce3eaf9d62ee3494539
Monkeyyy11 <gaileshd@gmail.com>
2018-07-27 18:13:48 +0200

    ?creditsranklist allows you to see the top 20 and anymore all places

--- commands/currency/creditranklist.js
+++ commands/currency/creditranklist.js
@@ -23,65 +23,15 @@ exports.run = async(client, msg, args, lang) => {
 	.addField(lang.creditranklist_name, tempArray.slice(0, 20).join('\n'), true)
 	.addField(lang.creditranklist_credits, moneyArray.slice(0, 20).join('\n'), true);
 
-		const message = await msg.channel.send({ embed });
-		
-		await message.react('◀');
-		await message.react('▶');
-		
-		var first = 0;
-		var second = 20;
-		
-		var collector = message.createReactionCollector((reaction, user) => user.id === msg.author.id, { time: 30000 });
-	collector.on('collect', r => {
-		var reactionadd = moneyArray.slice(first + 20, second + 20).length;
-		var reactionremove = moneyArray.slice(first - 20, second - 20).length;
-
-		if (r.emoji.name === '▶' && reactionadd !== 0) {
-			r.remove(msg.author.id);
-			const thefirst = moneyArray.slice(first + 20, second + 20);
-			const thesecond = tempArray.slice(first + 20, second + 20);
-
-			first = first + 20;
-			second = second + 20;
-
-			const newembed = new Discord.RichEmbed()
-			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
-			.setColor('#009933')
-			.addField(lang.creditranklist_name, thesecond.join('\n'), true)
-			.addField(lang.creditranklist_credits, thefirst.join('\n'), true);
-		
-		message.edit({ embed: newembed });
-	  	} else if (r.emoji.name === '◀' && reactionremove !== 0) {
-			r.remove(msg.author.id);
-			const thefirst = moneyArray.slice(first - 20, second - 20);
-			const thesecond = tempArray.slice(first - 20, second - 20);
-		
-			first = first - 20;
-			second = second - 20;
-		
-			const newembed = new Discord.RichEmbed()
-			.addField(lang.creditranklist_name, thesecond.join('\n'), true)
-			.addField(lang.creditranklist_credits, thefirst.join('\n'), true)
-			.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.displayAvatarURL)
-			.setColor('#009933');
-		
-		message.edit({ embed: newembed });
-		}
-	});
-		collector.on('end',(collected, reason) => {
-		message.react('❌');
-		});
+	await msg.channel.send({ embed });
 };
 
 exports.conf = {
 	enabled: true,
 	guildOnly: true,
 	aliases: ['richest'],
-
-    
-
-    userpermissions: [], dashboardsettings: true
-
+	userpermissions: [],
+	dashboardsettings: true
 };
 exports.help = {
 	name: 'creditranklist',
