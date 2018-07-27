3d1d05ecdbe91044f4466e1df743ce695968f5e1
Monkeyyy11 <gaileshd@gmail.com>
2018-07-27 18:27:04 +0200

    Possibility to see the global rank using the ?credits command

--- commands/currency/credits.js
+++ commands/currency/credits.js
@@ -1,31 +1,56 @@
 const Discord = require('discord.js');
 const sql = require('sqlite');
 sql.open("../lenoxbotscore.sqlite");
-
-exports.run = async(client, msg, args, lang) => {
+exports.run = async (client, msg, args, lang) => {
 	const user1 = msg.mentions.users.first() || msg.author;
 	const userdb = client.userdb.get(msg.author.id);
 	const lenoxbotcoin = client.emojis.get('412952854354067456');
 
 	if (userdb.creditsmessage === false) {
 		const embed = new Discord.RichEmbed()
-		.setColor('#3399ff')
-		.setDescription(lang.credits_hintembed)
-		.setAuthor(lang.credits_hint);
+			.setColor('#3399ff')
+			.setDescription(lang.credits_hintembed)
+			.setAuthor(lang.credits_hint);
 
 		userdb.creditsmessage = true;
 		await client.userdb.set(msg.author.id, userdb);
 
-		msg.author.send({ embed });
+		msg.author.send({
+			embed
+		});
+	}
+
+	const rows = await sql.all(`SELECT * FROM medals GROUP BY userId ORDER BY medals DESC`);
+	let useridsArray = [];
+	let userArray = [];
+	let moneyArray = [];
+	let tempArray = [];
+	var globalrank = [];
+	rows.forEach(row => {
+		useridsArray.push(row.userId);
+		const member = client.users.get(row.userId);
+		userArray.push(member ? member.tag : row.userId);
+		moneyArray.push(row.medals);
+	});
+	for (i = 0; i < userArray.length; i++) {
+		tempArray.push((i + 1));
+	}
+
+	for (var index = 0; index < userArray.length; index++) {
+		if (useridsArray[index] === user1.id) {
+			globalrank.push(tempArray[index]);
+		}
 	}
 
 	sql.get(`SELECT * FROM medals WHERE userId = "${user1.id}"`).then(row => {
 		var embed = new Discord.RichEmbed()
-		.setAuthor(user1.tag, user1.avatarURL)
-		.setDescription(`**${lang.credits_credits}** ${row.medals} ${lenoxbotcoin} `)
-		.setColor('#009933');
+			.setAuthor(`${user1.tag} (${lang.credits_globalrank} #${globalrank})`, user1.avatarURL)
+			.setDescription(`**${lang.credits_credits}** ${row.medals} ${lenoxbotcoin} `)
+			.setColor('#009933');
 
-		msg.channel.send({ embed });
+		msg.channel.send({
+			embed
+		});
 	});
 };
 
@@ -33,8 +58,8 @@ exports.conf = {
 	enabled: true,
 	guildOnly: false,
 	aliases: ['balance', 'c'],
-	userpermissions: [], dashboardsettings: false
-
+	userpermissions: [],
+	dashboardsettings: false
 };
 exports.help = {
 	name: 'credits',
