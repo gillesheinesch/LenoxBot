15dd708107df8d9ea754452f9e6b1c945d6ff407
Monkeyyy11 <gaileshd@gmail.com>
2018-07-27 21:56:06 +0200

    Changed some guild rules

--- commands/botowner/embedpost.js
+++ commands/botowner/embedpost.js
@@ -12,11 +12,10 @@ exports.run = async (client, msg, args, lang) => {
 					'**2.** This server only deal with LenoxBot and nothing else. We do not give support for anything that has nothing to do with LenoxBot',
 					'**3.** If you need help and no one is currently online, you can use @help. Please do not use @help without justification. Example of how to use @help correctly: "@help why is the bot offline?"',
 					'**4.** If you have questions, please try to provide as much information as possible so that we can help you quickly',
-					'**5.** Please do not mark team members to get a quicker answer to your question. Direct messages are also included. It\'s much better to ask your question (s) in Public Channel. Most of the time you get a faster answer!',
+					'**5.** Please do not mention team members to get a quicker answer to your question. Direct messages are also included. It\'s much better to ask your question (s) in Public Channel. Most of the time you get a faster answer!',
 					'**6.** If your discord name is not easy to mention, please change your nickname to a name that is easier to mark!',
-					'**7.** Respect the channel category language. For each language in which the bot is available, there is a separate channel category in which only this language may be spoken!',
-					'**8.** Writing in caps is prohibited',
-					'**9.** Disrespectful or offensive messages to a Discord User are prohibited!'
+					'**7.** Writing in caps is prohibited',
+					'**8.** Disrespectful or offensive messages to a Discord User are prohibited!'
 				];
 				const embed = new Discord.RichEmbed()
 					.setDescription(rules.join("\n\n"))
