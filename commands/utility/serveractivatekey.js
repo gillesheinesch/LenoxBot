b3ab13d5edd072388e7eefe81cb74a1a121f2030
Monkeyyy11 <gaileshd@gmail.com>
2018-07-28 09:48:55 +0200

    /

--- commands/utility/serveractivatekey.js
+++ commands/utility/serveractivatekey.js
@@ -1,4 +1,4 @@
-exports.run = async(client, msg, args, lang) => {
+exports.run = async (client, msg, args, lang) => {
 	const input = args.slice();
 	const botconfspremiumload = client.botconfs.get('premium');
 	const tableload = client.guildconfs.get(msg.guild.id);
@@ -17,50 +17,54 @@ exports.run = async(client, msg, args, lang) => {
 		tableload.premium.status = true;
 		tableload.premium.bought.push(new Date().getTime);
 
-	const now = new Date().getTime();
-	tableload.premium.end = new Date(now + 7776000000);
+		const now = new Date().getTime();
+		tableload.premium.end = new Date(now + 7776000000);
 
-	botconfspremiumload.keys.redeemedguildkeys.push(input.join(" "));
+		botconfspremiumload.keys.redeemedguildkeys.push(input.join(" "));
 
-	await client.guildconfs.set(msg.author.id, tableload);
-	await client.botconfs.set('premium', botconfspremiumload);
+		await client.guildconfs.set(msg.author.id, tableload);
+		await client.botconfs.set('premium', botconfspremiumload);
 
-	const timestamps = client.cooldowns.get('serveractivatekey');
-	timestamps.delete(msg.author.id);
+		const timestamps = client.cooldowns.get('serveractivatekey');
+		timestamps.delete(msg.author.id);
 
-	const embed = new Discord.RichEmbed()
-	.setDescription(`This discord server used a premium serverkey (Code: ${input.join(" ")})! \n\nThis discord server has premium until ${tableload.premium.end.toUTCString()}`)
-	.setAuthor(`Serverkey used by ${msg.author.tag} for ${msg.guild.name} (${msg.guild.id})`, msg.guild.iconURL)
-	.setTimestamp()
-	.setColor('#ff0000')
-	.setTitle('New Serverkey used!');
-	await client.channels.get('419877966265319424').send({ embed });
+		const embed = new Discord.RichEmbed()
+			.setDescription(`This discord server used a premium serverkey (Code: ${input.join(" ")})! \n\nThis discord server has premium until ${tableload.premium.end.toUTCString()}`)
+			.setAuthor(`Serverkey used by ${msg.author.tag} for ${msg.guild.name} (${msg.guild.id})`, msg.guild.iconURL)
+			.setTimestamp()
+			.setColor('#ff0000')
+			.setTitle('New Serverkey used!');
+		await client.channels.get('419877966265319424').send({
+			embed
+		});
 
-	const redeemed = lang.serveractivatekey_redeemed.replace('%date', `\`${tableload.premium.end.toUTCString()}\``);
-	return msg.reply(redeemed);
+		const redeemed = lang.serveractivatekey_redeemed.replace('%date', `\`${tableload.premium.end.toUTCString()}\``);
+		return msg.reply(redeemed);
 	} else {
 		tableload.premium.bought.push(new Date().getTime);
 
 		tableload.premium.end = new Date(Date.parse(tableload.premium.end) + 7776000000);
 
-	botconfspremiumload.keys.redeemedguildkeys.push(input.join(" "));
+		botconfspremiumload.keys.redeemedguildkeys.push(input.join(" "));
 
-	await client.guildconfs.set(msg.author.id, tableload);
-	await client.botconfs.set('premium', botconfspremiumload);
+		await client.guildconfs.set(msg.author.id, tableload);
+		await client.botconfs.set('premium', botconfspremiumload);
 
-	const timestamps = client.cooldowns.get('serveractivatekey');
-	timestamps.delete(msg.author.id);
+		const timestamps = client.cooldowns.get('serveractivatekey');
+		timestamps.delete(msg.author.id);
 
-	const embed = new Discord.RichEmbed()
-	.setDescription(`This discord server used a premium serverkey (Code: ${input.join(" ")})! \n\nThis discord server has premium until ${new Date(Date.parse(tableload.premium.end) + 7776000000).toUTCString()}`)
-	.setAuthor(`Serverkey used by ${msg.author.tag} for ${msg.guild.name} (${msg.guild.id})`, msg.guild.iconURL)
-	.setTimestamp()
-	.setColor('#ff0000')
-	.setTitle('New Serverkey used!');
-	client.channels.get('419877966265319424').send({ embed });
+		const embed = new Discord.RichEmbed()
+			.setDescription(`This discord server used a premium serverkey (Code: ${input.join(" ")})! \n\nThis discord server has premium until ${new Date(Date.parse(tableload.premium.end) + 7776000000).toUTCString()}`)
+			.setAuthor(`Serverkey used by ${msg.author.tag} for ${msg.guild.name} (${msg.guild.id})`, msg.guild.iconURL)
+			.setTimestamp()
+			.setColor('#ff0000')
+			.setTitle('New Serverkey used!');
+		client.channels.get('419877966265319424').send({
+			embed
+		});
 
-	const extended = lang.serveractivatekey_extended.replace('%date', `\`${new Date(Date.parse(tableload.premium.end) + 7776000000).toUTCString()}\``);
-	return msg.reply(extended);
+		const extended = lang.serveractivatekey_extended.replace('%date', `\`${new Date(Date.parse(tableload.premium.end) + 7776000000).toUTCString()}\``);
+		return msg.reply(extended);
 	}
 };
 
@@ -69,7 +73,8 @@ exports.conf = {
 	guildOnly: true,
 	aliases: [],
 
-	userpermissions: [], dashboardsettings: false,
+	userpermissions: [],
+	dashboardsettings: false,
 
 	cooldown: 43200000
 };
