74b503bd17a3b3426c471a6c4f4fc953f50ae50c
Monkeyyy11 <gaileshd@gmail.com>
2018-08-20 17:33:37 +0200

    added reactions to ?listchatfilter so you can now see all entries

--- commands/administration/listchatfilter.js
+++ commands/administration/listchatfilter.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
-exports.run = async(client, msg, args, lang) => {
+exports.run = async (client, msg, args, lang) => {
 	const tableload = client.guildconfs.get(msg.guild.id);
-    const array = [];
-	
+	const array = [];
+
 	if (!tableload.chatfilter) {
 		tableload.chatfilter = {
 			chatfilter: 'false',
@@ -14,14 +14,69 @@ exports.run = async(client, msg, args, lang) => {
 	if (tableload.chatfilter.array.length === 0) return msg.channel.send(lang.listchatfilter_error);
 
 	const embed = new Discord.RichEmbed()
-    .setColor('#ABCDEF');
+		.setColor('#ABCDEF');
 
-		for (var i = 0; i < tableload.chatfilter.array.length; i++) {
-			array.push(tableload.chatfilter.array[i]);
-		}
-        embed.addField(lang.listchatfilter_embed, array.join("\n"), true);
-        
-		return msg.channel.send({ embed: embed });
+	for (var i = 0; i < tableload.chatfilter.array.length; i++) {
+		array.push(tableload.chatfilter.array[i]);
+	}
+
+	embed.addField(lang.listchatfilter_embed, array.slice(0, 15).join("\n"), true);
+
+	const message = await msg.channel.send({
+		embed: embed
+	});
+
+	if (array.length > 15) {
+		var reaction1 = await message.react('◀');
+		var reaction2 = await message.react('▶');
+
+		var first = 0;
+		var second = 15;
+
+		var collector = message.createReactionCollector((reaction, user) => user.id === msg.author.id, {
+			time: 120000
+		});
+		collector.on('collect', r => {
+			var reactionadd = array.slice(first + 15, second + 15).length;
+			var reactionremove = array.slice(first - 15, second - 15).length;
+
+			if (r.emoji.name === '▶' && reactionadd !== 0) {
+				r.remove(msg.author.id);
+
+				first = first + 15;
+				second = second + 15;
+
+				const newembed = new Discord.RichEmbed()
+					.setColor('#ABCDEF');
+
+				newembed.addField(lang.listchatfilter_embed, array.slice(first, second).join("\n"), true);
+
+				message.edit({
+					embed: newembed
+				});
+			} else if (r.emoji.name === '◀' && reactionremove !== 0) {
+				r.remove(msg.author.id)
+
+				first = first - 15;
+				second = second - 15;
+
+				const newembed = new Discord.RichEmbed()
+					.setColor('#ABCDEF');
+
+				newembed.addField(lang.listchatfilter_embed, array.slice(first, second).join("\n"), true);
+
+				message.edit({
+					embed: newembed
+				});
+			}
+		});
+		collector.on('end', (collected, reason) => {
+			reaction1.remove();
+			reaction2.remove();
+		});
+	} else {
+		return undefined;
+	}
 };
 
 exports.conf = {
@@ -37,5 +92,5 @@ exports.help = {
 	usage: 'listchatfilter',
 	example: ['listchatfilter'],
 	category: 'administration',
-    botpermissions: ['SEND_MESSAGES']
+	botpermissions: ['SEND_MESSAGES']
 };
