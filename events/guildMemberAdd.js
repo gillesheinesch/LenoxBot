8e3035756c4aa892ca5e99baa09e7e63497dbdca
Monkeyyy11 <gaileshd@gmail.com>
2018-08-22 09:37:35 +0200

    the muted user gets now the role if he leaves and joins the guild again

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,35 +1,51 @@
 const Discord = require('discord.js');
-exports.run = (client, member) => {
-	const tableload = client.guildconfs.get(member.guild.id);
+exports.run = async (client, member) => {
+	const tableload = await client.guildconfs.get(member.guild.id);
+	const botconfs = await client.botconfs.get('botconfs');
 	if (!tableload) return;
 
 	if (tableload.language === '') {
-        tableload.language = 'en';
-        client.guildconfs.set(member.guild.id, tableload);
+		tableload.language = 'en';
+		client.guildconfs.set(member.guild.id, tableload);
 	}
 
 	var lang = require(`../languages/${tableload.language}.json`);
 
-	if (tableload.welcomelog === 'true') {
-	const messagechannel = client.channels.get(tableload.welcomelogchannel);
-	const embed = new Discord.RichEmbed()
-	.setFooter(lang.guildmemberaddevent_userjoined)
-	.setTimestamp()
-	.setColor(0x00AE86)
-	.setAuthor(`${member.user.tag} (${member.user.id})`, member.user.avatarURL);
-	messagechannel.send({ embed: embed });
+	for (var i in botconfs.mutes) {
+		if (botconfs.mutes[i].discordserverid === member.guild.id && botconfs.mutes[i].memberid === member.id) {
+			var muteOfThisUser = botconfs.mutes[i];
+		}
 	}
-	
+
+	if (muteOfThisUser) {
+		if (member.guild.roles.get(muteOfThisUser.roleid)) {
+			const mutedRole = member.guild.roles.get(muteOfThisUser.roleid);
+			member.addRole(mutedRole);
+		}
+	}
+
+	if (tableload.welcomelog === 'true') {
+		const messagechannel = client.channels.get(tableload.welcomelogchannel);
+		const embed = new Discord.RichEmbed()
+			.setFooter(lang.guildmemberaddevent_userjoined)
+			.setTimestamp()
+			.setColor(0x00AE86)
+			.setAuthor(`${member.user.tag} (${member.user.id})`, member.user.avatarURL);
+		messagechannel.send({
+			embed: embed
+		});
+	}
+
 	if (tableload.welcome === 'true') {
 		if (tableload.welcomemsg.length < 1) return;
 		const message = tableload.welcomemsg;
 		const messagechannel = client.channels.get(tableload.welcomechannel);
 		const newMessage = message.replace('$username$', member.user.username)
-		.replace('$usermention$', member.user)
-		.replace('$usertag$', member.user.tag)
-		.replace('$userid$', member.user.id)
-		.replace('$guildname$', member.guild.name)
-		.replace('$guildid$', member.guild.id);
+			.replace('$usermention$', member.user)
+			.replace('$usertag$', member.user.tag)
+			.replace('$userid$', member.user.id)
+			.replace('$guildname$', member.guild.name)
+			.replace('$guildid$', member.guild.id);
 		messagechannel.send(newMessage);
 	}
 };
