712b3169cf1fa314f7a120b6ed2187e1cbe5333d
Monkeyyy11 <gaileshd@gmail.com>
2018-08-31 12:12:36 +0200

    added policy site and updated cookies alert

--- commands/utility/setprofiledescription.js
+++ commands/utility/setprofiledescription.js
@@ -18,11 +18,8 @@ exports.conf = {
 	enabled: true,
 	guildOnly: true,
 	aliases: [],
-
-	
-
-	userpermissions: [], dashboardsettings: true
-
+	userpermissions: [],
+	dashboardsettings: true
 };
 exports.help = {
 	name: 'setprofiledescription',
