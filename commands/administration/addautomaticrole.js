8c7975d708b286c8be32ec34439914d185a08c82
Monkeyyy11 <gaileshd@gmail.com>
2018-09-09 13:01:55 +0200

    eslint addautomaticrole.js

--- commands/administration/addautomaticrole.js
+++ commands/administration/addautomaticrole.js
@@ -1,36 +1,36 @@
-exports.run = async(client, msg, args, lang) => {
+exports.run = async (client, msg, args, lang) => {
 	const tableload = client.guildconfs.get(msg.guild.id);
-	
+
 	if (!tableload.ara) {
 		tableload.ara = [];
 		await client.guildconfs.set(msg.guild.id, tableload);
 	}
 
-	let input = args.slice();
+	const input = args.slice();
 
 	if (!input[0]) return msg.reply(lang.addautomaticrole_noinput).then(m => m.delete(10000));
-	if (parseInt(input[0]) < 0) return msg.reply(lang.addautomaticrole_inputcannotbe0);
-	if (!args.slice(1).join(" ")) return msg.reply(lang.addautomaticrole_norolename).then(m => m.delete(10000));
+	if (parseInt(input[0], 10) < 0) return msg.reply(lang.addautomaticrole_inputcannotbe0);
+	if (!args.slice(1).join(' ')) return msg.reply(lang.addautomaticrole_norolename).then(m => m.delete(10000));
 
-	let roleinput = args.slice(1).join(" ");
+	const roleinput = args.slice(1).join(' ');
 	const foundRole = msg.guild.roles.find(role => role.name.toLowerCase() === roleinput.toLowerCase());
 	if (!foundRole) return msg.reply(lang.addautomaticrole_rolenotexist).then(m => m.delete(10000));
 
-		for (var i = 0; i < tableload.ara.length;  i += 2) {
-			if (foundRole.id === tableload.ara[i]) return msg.channel.send(lang.addautomaticrole_alreadyadded);
-		}
-		const roleId = foundRole.id;
-		tableload.ara.push(roleId);
-		tableload.ara.push(input[0]);
-		await client.guildconfs.set(msg.guild.id, tableload);
-		
-		return msg.channel.send(lang.addautomaticrole_added);
+	for (let i = 0; i < tableload.ara.length; i += 2) {
+		if (foundRole.id === tableload.ara[i]) return msg.channel.send(lang.addautomaticrole_alreadyadded);
+	}
+	const roleId = foundRole.id;
+	tableload.ara.push(roleId);
+	tableload.ara.push(input[0]);
+	await client.guildconfs.set(msg.guild.id, tableload);
+
+	return msg.channel.send(lang.addautomaticrole_added);
 };
 
 exports.conf = {
 	enabled: true,
 	guildOnly: true,
-	shortDescription: "Automaticroles",
+	shortDescription: 'Automaticroles',
 	aliases: ['aar'],
 	userpermissions: ['ADMINISTRATOR'], dashboardsettings: true
 };
