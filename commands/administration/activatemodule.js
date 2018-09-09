e3ff43315688def6d0ae015e2e6aefd473938ea4
Monkeyyy11 <gaileshd@gmail.com>
2018-09-09 12:52:57 +0200

    eslint activatemodule.js

--- commands/administration/activatemodule.js
+++ commands/administration/activatemodule.js
@@ -1,7 +1,6 @@
-const Discord = require('discord.js');
-exports.run = async(client, msg, args, lang) => {
+exports.run = async (client, msg, args, lang) => {
 	const tableload = client.guildconfs.get(msg.guild.id);
-	var moduleactivated = lang.activatemodule_moduleactivated.replace('%modulename', args.slice());
+	const moduleactivated = lang.activatemodule_moduleactivated.replace('%modulename', args.slice());
 
 	if (!tableload.modules) {
 		tableload.modules = {};
@@ -19,87 +18,88 @@ exports.run = async(client, msg, args, lang) => {
 
 	if (args.slice().length === 0) return msg.channel.send(lang.activatemodule_noinput);
 
-	const margs = msg.content.split(" ");
+	const margs = msg.content.split(' ');
 	const validation = ['administration', 'help', 'music', 'fun', 'searches', 'nsfw', 'utility', 'moderation', 'application', 'currency', 'partner', 'tickets', 'customcommands'];
 
-	for (i = 0; i < margs.length; i++) {
+	for (let i = 0; i < margs.length; i++) {
 		if (validation.indexOf(margs[i].toLowerCase()) >= 0) {
-			if (margs[1].toLowerCase() == "administration") {
+			if (margs[1].toLowerCase() === 'administration') {
 				return msg.channel.send(lang.activatemodule_administration);
-			} else if (margs[1].toLowerCase() == "partner") {
+			} else if (margs[1].toLowerCase() === 'partner') {
 				return msg.channel.send(lang.activatemodule_partner);
-			} else if (margs[1].toLowerCase() == "utility") {
-				if (tableload.modules.utility = 'true') return msg.channel.send(lang.activatemodule_alreadyactivated);
-				
+			} else if (margs[1].toLowerCase() === 'utility') {
+				if (tableload.modules.utility === 'true') return msg.channel.send(lang.activatemodule_alreadyactivated);
+
 				tableload.modules.utility = 'true';
 			    await client.guildconfs.set(msg.guild.id, tableload);
 				return msg.channel.send(moduleactivated);
-			} else if (margs[1].toLowerCase() == "music") {
+			} else if (margs[1].toLowerCase() === 'music') {
 				if (tableload.modules.music === 'true') return msg.channel.send(lang.activatemodule_alreadyactivated);
-				
+
 				tableload.modules.music = 'true';
 			    await client.guildconfs.set(msg.guild.id, tableload);
 				return msg.channel.send(moduleactivated);
-			} else if (margs[1].toLowerCase() == "fun") {
+			} else if (margs[1].toLowerCase() === 'fun') {
 				if (tableload.modules.fun === 'true') return msg.channel.send(lang.activatemodule_alreadyactivated);
-				
+
 				tableload.modules.fun = 'true';
 			    await client.guildconfs.set(msg.guild.id, tableload);
 				return msg.channel.send(moduleactivated);
-			} else if (margs[1].toLowerCase() == "help") {
+			} else if (margs[1].toLowerCase() === 'help') {
 				if (tableload.modules.help === 'true') return msg.channel.send(lang.activatemodule_alreadyactivated);
-				
+
 				tableload.modules.help = 'true';
 			    await client.guildconfs.set(msg.guild.id, tableload);
 				return msg.channel.send(moduleactivated);
-			} else if (margs[1].toLowerCase() == "searches") {
+			} else if (margs[1].toLowerCase() === 'searches') {
 				if (tableload.modules.searches === 'true') return msg.channel.send(lang.activatemodule_alreadyactivated);
-				
+
 				tableload.modules.searches = 'true';
 			    await client.guildconfs.set(msg.guild.id, tableload);
 				return msg.channel.send(moduleactivated);
-			} else if (margs[1].toLowerCase() == "nsfw") {
+			} else if (margs[1].toLowerCase() === 'nsfw') {
 				if (tableload.modules.nsfw === 'true') return msg.channel.send(lang.activatemodule_alreadyactivated);
-				
+
 				tableload.modules.nsfw = 'true';
 			    await client.guildconfs.set(msg.guild.id, tableload);
 				return msg.channel.send(moduleactivated);
-			} else if (margs[1].toLowerCase() == "moderation") {
+			} else if (margs[1].toLowerCase() === 'moderation') {
 				if (tableload.modules.moderation === 'true') return msg.channel.send(lang.activatemodule_alreadyactivated);
-				
+
 				tableload.modules.moderation = 'true';
 			    await client.guildconfs.set(msg.guild.id, tableload);
 				return msg.channel.send(moduleactivated);
-			} else if (margs[1].toLowerCase() == "application") {
+			} else if (margs[1].toLowerCase() === 'application') {
 				if (tableload.modules.application === 'true') return msg.channel.send(lang.activatemodule_alreadyactivated);
-				
+
 				tableload.modules.application = 'true';
 			    await client.guildconfs.set(msg.guild.id, tableload);
 				return msg.channel.send(moduleactivated);
-			} else if (margs[1].toLowerCase() == "currency") {
+			} else if (margs[1].toLowerCase() === 'currency') {
 				if (tableload.modules.currency === 'true') return msg.channel.send(lang.activatemodule_alreadyactivated);
-				
+
 				tableload.modules.currency = 'true';
 			    await client.guildconfs.set(msg.guild.id, tableload);
 				return msg.channel.send(moduleactivated);
-			} else if (margs[1].toLowerCase() == "customcommands") {
+			} else if (margs[1].toLowerCase() === 'customcommands') {
 				if (tableload.modules.customcommands === 'true') return msg.channel.send(lang.activatemodule_alreadyactivated);
-				
+
 				tableload.modules.customcommands = 'true';
 			    await client.guildconfs.set(msg.guild.id, tableload);
 				return msg.channel.send(moduleactivated);
 			}
 		}
 	}
-	msg.channel.send(lang.activatemodule_error);
+	return msg.channel.send(lang.activatemodule_error);
 };
 
 exports.conf = {
 	enabled: true,
 	guildOnly: true,
-	shortDescription: "Modules",
+	shortDescription: 'Modules',
 	aliases: ['am'],
-	userpermissions: ['ADMINISTRATOR'], dashboardsettings: true
+	userpermissions: ['ADMINISTRATOR'],
+	dashboardsettings: true
 };
 exports.help = {
 	name: 'activatemodule',
