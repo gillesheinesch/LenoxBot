e87562d196f0a0c80fdfb0a69f2f3d7381712074
Monkeyyy11 <gaileshd@gmail.com>
2018-09-12 18:46:41 +0200

    You can now use ?announce {text -embed} to send an announcement in an RichEmbed #50

--- commands/administration/announce.js
+++ commands/administration/announce.js
@@ -1,6 +1,8 @@
-exports.run = (client, msg, args, lang) => {
-	const tableload = client.guildconfs.get(msg.guild.id);
+const Discord = require('discord.js');
+exports.run = async (client, msg, args, lang) => {
+	const tableload = await client.guildconfs.get(msg.guild.id);
 	const text = args.slice().join(' ');
+	const embedCheck = text.toLowerCase().includes('-embed');
 
 	const announceactivated = lang.announce_announcedeactivated.replace('%prefix', tableload.prefix);
 	if (tableload.announce === 'false') return msg.channel.send(announceactivated);
@@ -9,8 +11,20 @@ exports.run = (client, msg, args, lang) => {
 
 	const announcechannel = tableload.announcechannel;
 	const announcement = lang.announce_announcement.replace('%authortag', msg.author.tag);
-	client.channels.get(announcechannel).send(`${announcement} ${text}`);
-	msg.channel.send(lang.announce_annoucementsent);
+	if (embedCheck) {
+		const newText = text.replace('-embed', '');
+		const embed = new Discord.RichEmbed()
+			.setColor('#33cc33')
+			.setDescription(newText)
+			.setAuthor(announcement);
+		await client.channels.get(announcechannel).send({
+			embed: embed
+		});
+	} else {
+		await client.channels.get(announcechannel).send(`${announcement} ${text}`);
+	}
+
+	return msg.reply(lang.announce_annoucementsent);
 };
 
 exports.conf = {
@@ -24,8 +38,8 @@ exports.conf = {
 exports.help = {
 	name: 'announce',
 	description: 'Write a new server announcement',
-	usage: 'announce {announcement text}',
-	example: ['announce Today we reached 5000 members. Thank you for that!'],
+	usage: 'announce {announcement text (-embed)}',
+	example: ['announce Today we reached 5000 members. Thank you for that!', 'announce Today we reached 5000 members. Thank you for that! -embed'],
 	category: 'administration',
 	botpermissions: ['SEND_MESSAGES']
 };
