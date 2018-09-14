7b67ea66d6cb8c2aa244bccff79fb828c5e17ac6
Monkeyyy11 <gaileshd@gmail.com>
2018-09-14 11:47:24 +0200

    ?listentry has now all ids of all entries in front of each entry and supports now pages (reactions to change the page)

--- commands/application/listentry.js
+++ commands/application/listentry.js
@@ -15,19 +15,69 @@ exports.run = async (client, msg, args, lang) => {
 		await client.guildconfs.set(msg.guild.id, tableload);
 	}
 
+	if (tableload.application.template.length === 0) return msg.reply(lang.listentry_error);
+
 	const templates = [];
+
+	for (let i = 0; i < tableload.application.template.length; i++) {
+		templates.push(`\`${i + 1}.\` ${tableload.application.template[i]}`);
+	}
+
 	const embed = new Discord.RichEmbed()
 		.setColor('#ABCDEF');
+	embed.addField(lang.listentry_current, templates.slice(0, 10).join('\n'), true);
+	const message = await msg.channel.send({
+		embed: embed
+	});
 
-	try {
-		for (let i = 0; i < tableload.application.template.length; i++) {
-			templates.push(tableload.application.template[i]);
+	if (templates.length <= 10) return;
+	const reaction1 = await message.react('◀');
+	const reaction2 = await message.react('▶');
+
+	let first = 0;
+	let second = 10;
+
+	const collector = message.createReactionCollector((reaction, user) => user.id === msg.author.id, {
+		time: 120000
+	});
+	collector.on('collect', r => {
+		const reactionadd = templates.slice(first + 10, second + 10).length;
+		const reactionremove = templates.slice(first - 10, second - 10).length;
+
+		if (r.emoji.name === '▶' && reactionadd !== 0) {
+			r.remove(msg.author.id);
+
+			first += 10;
+			second += 10;
+
+			const newembed = new Discord.RichEmbed()
+				.setColor('#ABCDEF');
+
+			newembed.addField(lang.listentry_current, templates.slice(first, second).join('\n'), true);
+
+			message.edit({
+				embed: newembed
+			});
+		} else if (r.emoji.name === '◀' && reactionremove !== 0) {
+			r.remove(msg.author.id);
+
+			first -= 10;
+			second -= 10;
+
+			const newembed = new Discord.RichEmbed()
+				.setColor('#ABCDEF');
+
+			newembed.addField(lang.listentry_current, templates.slice(first, second).join('\n'), true);
+
+			message.edit({
+				embed: newembed
+			});
 		}
-		embed.addField(lang.listentry_current, templates.join('\n'), true);
-		return msg.channel.send({ embed: embed });
-	} catch (error) {
-		return msg.channel.send(lang.listentry_error);
-	}
+	});
+	collector.on('end', () => {
+		reaction1.remove();
+		reaction2.remove();
+	});
 };
 
 exports.conf = {
