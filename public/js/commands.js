3cb0c93c9fb241e9b3b63aa3632531a514efbe87
jecsham <jecsham27@gmail.com>
2018-09-26 15:50:54 -0500

    preparing for responsive design

--- public/js/commands.js
+++ public/js/commands.js
@@ -3,15 +3,16 @@
 // Filter by buttons
 $("button").click(function () {
     var value = this.id;
-    $("#commands-table tbody tr").filter(function () {
+    $("#table tbody tr").filter(function () {
         $(this).toggle($(this).attr('class').indexOf(value) > - 1);
+        
     });
 });
 
 // Filter by text
 $("#search-input").on("keyup", function () {
     var value = $(this).val().toLowerCase();
-    $("#commands-table tr").filter(function () {
+    $("#table tr").filter(function () {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
     });
 });
--- public/script/alerts.js
+++
@@ -1,7 +0,0 @@
-// JavaScript source code
-// Eventregistration
-$("document").ready(function () {
-    $("#success-alert").fadeTo(5000, 500).slideUp(1000, function () {
-        $("#success-alert").slideUp(1000);
-    });
-});
