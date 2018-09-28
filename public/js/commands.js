2dba4430f40050eccb3890dc3da400abd97b119e
Jecsham <jecsham27@gmail.com>
2018-09-28 16:30:38 -0500

    fix - commands btn list

--- public/js/commands.js
+++ public/js/commands.js
@@ -16,3 +16,12 @@ $("#search-input").on("keyup", function () {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
     });
 });
+
+// Activate current nav commands button
+$('.lb-btn-list').click(function(){
+    $('.lb-btn-list').each(function(){
+        $(this).removeClass('lb-btn-list-focus');
+        console.log(this);
+    });
+    $(this).addClass('lb-btn-list-focus');
+});
