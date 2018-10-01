492f370b78bda967b54aadd9e25e2d64b6e40757
Jecsham <jecsham27@gmail.com>
2018-10-01 17:44:40 -0500

    ajax - customcommands  commands & fixes

--- lenoxbotapp.js
+++ lenoxbotapp.js
@@ -7642,8 +7642,76 @@ app.get('/error', (req, res) => {
 	});
 });
 
-// catch error and forward to error handler
+// Global post for commands status change
 
+app.post('/dashboard/:id/global/:command/submitcommandstatuschange', async (req, res) => {
+	// console.log('Work');
+	try {
+		const dashboardid = res.req.originalUrl.substr(11, 18);
+		if (req.user) {
+			let index = -1;
+			for (let i = 0; i < req.user.guilds.length; i++) {
+				if (req.user.guilds[i].id === dashboardid) {
+					index = i;
+				}
+			}
+
+			if (index === -1) return res.redirect('/servers');
+
+			if (!client.guildconfs.get(dashboardid).dashboardpermissionroles) {
+				client.guildconfs.get(dashboardid).dashboardpermissionroles = [];
+			}
+
+			if (client.guildconfs.get(dashboardid).dashboardpermissionroles.length !== 0 && client.guilds.get(dashboardid).ownerID !== req.user.id) {
+				let allwhitelistedrolesoftheuser = 0;
+
+				for (let index2 = 0; index2 < client.guildconfs.get(dashboardid).dashboardpermissionroles.length; index2++) {
+					if (!client.guilds.get(dashboardid).members.get(req.user.id)) return res.redirect('/servers');
+					if (!client.guilds.get(dashboardid).members.get(req.user.id).roles.has(client.guildconfs.get(dashboardid).dashboardpermissionroles[index2])) {
+						allwhitelistedrolesoftheuser += 1;
+					}
+				}
+				if (allwhitelistedrolesoftheuser === client.guildconfs.get(dashboardid).dashboardpermissionroles.length) {
+					return res.redirect('/servers');
+				}
+			} else if (((req.user.guilds[index].permissions) & 8) !== 8) {
+				return res.redirect('/servers');
+			}
+
+			if (!client.guilds.get(req.user.guilds[index].id)) return res.redirect('/servers');
+
+			const tableload = client.guildconfs.get(dashboardid);
+
+			tableload.commands[req.params.command].status = req.body.statuschange;
+
+			tableload.globallogs.push({
+				action: `Activated/Deactivated the "${req.params.command}" command!`,
+				username: req.user.username,
+				date: Date.now(),
+				showeddate: new Date().toUTCString()
+			});
+
+			await client.guildconfs.set(dashboardid, tableload);
+			return res.redirect(url.format({
+				pathname: `/dashboard/${dashboardid}/global`,
+				query: {
+					submit: true
+				}
+			}));
+		}
+		return res.redirect('/nologin');
+	} catch (error) {
+		return res.redirect(url.format({
+			pathname: `/error`,
+			query: {
+				statuscode: 500,
+				message: error.message
+			}
+		}));
+	}
+});
+
+// catch error and forward to error handler
 
 app.use((req, res) => {
 	const err = new Error('Not Found');
