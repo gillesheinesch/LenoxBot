663e17e215c5a41189ab935f551a622483bef1e1
Jecsham <jecsham27@gmail.com>
2018-10-04 00:42:18 -0500

    fix - Commandslist search function searches also for the table header

--- public/js/commands.js
+++ public/js/commands.js
@@ -5,21 +5,26 @@ $(".lb-btn-list").click(function () {
     var value = this.id;
     $("#table tbody tr").filter(function () {
         $(this).toggle($(this).attr('class').indexOf(value) > - 1);
-        
+
     });
 });
 
 // Filter by text
 $("#search-input").on("keyup", function () {
     var value = $(this).val().toLowerCase();
-    $("#table tr").filter(function () {
+    $("#table tbody tr").filter(function () {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
     });
+    if ($('#table tbody tr:visible').length === 0) {
+        $('#notfound').show();
+    }else{
+        $('#notfound').hide();
+    }
 });
 
 // Activate current nav commands button
-$('.lb-btn-list').click(function(){
-    $('.lb-btn-list').each(function(){
+$('.lb-btn-list').click(function () {
+    $('.lb-btn-list').each(function () {
         $(this).removeClass('lb-btn-list-focus');
         console.log(this);
     });
