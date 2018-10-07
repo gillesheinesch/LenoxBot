eec89e0ac3df3fe86e7ebbc40ca8cc8b944e5e15
Jecsham <jecsham27@gmail.com>
2018-10-07 03:44:31 -0500

    indent fix

--- public/js/global.js
+++ public/js/global.js
@@ -19,8 +19,8 @@ if (typeof currentNavDashId !== 'undefined') {
 
 
 // Cool scrollbar style
-(function($) {
-    $(window).on("load", function() {
+(function ($) {
+    $(window).on("load", function () {
         $(".lb-mcsp").mCustomScrollbar({
             theme: "lb-theme",
             scrollInertia: 300, //in ms
@@ -32,30 +32,30 @@ if (typeof currentNavDashId !== 'undefined') {
 })(jQuery);
 
 // Enable tooltips
-$(function() {
+$(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
 
 // Animation stats
-$('.lb-count').each(function() {
+$('.lb-count').each(function () {
     $(this).prop('Counter', 0).animate({
         Counter: $(this).text()
     }, {
             duration: 2500,
             easing: 'swing',
-            step: function(now) {
+            step: function (now) {
                 $(this).text(Math.ceil(now));
             }
         });
 });
 
 // Toggle menus (sidebars)
-$('#toggle-menu').click(function() {
+$('#toggle-menu').click(function () {
     $('.menu').slideToggle();
 });
 
 // Show menus if window is >= 992px
-$(window).resize(function() {
+$(window).resize(function () {
     if (parseInt($(window).width()) >= 992) {
         if (!$('.menu').is(':visible')) {
             $('.menu').slideDown();
