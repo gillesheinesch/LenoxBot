5d1be937128396fb2440e8df6046a59144507662
Monkeyyy11 <gaileshd@gmail.com>
2018-10-20 17:48:56 +0200

    added strings to the lenoxbot.json

--- commands/botowner/givexp.js
+++ commands/botowner/givexp.js
@@ -6,9 +6,9 @@ exports.run = async (client, msg, args, lang) => {
 	const userId = msg.mentions.users.first() ? msg.mentions.users.first().id : msg.mentions.users.first() || args.slice(0, 1).join(' ');
 	const xpAmount = parseInt(args.slice(1, 2).join(' '), 10);
 
-	if (args.slice(1, 2).length === 0) return msg.reply('Amount missing!');
-	if (isNaN(xpAmount)) return msg.reply('Not a number!');
-	if (xpAmount <= 0) return msg.reply('At least 1 xp');
+	if (args.slice(1, 2).length === 0) return msg.reply(lang.givexp_noamount);
+	if (isNaN(xpAmount)) return msg.reply(lang.givexp_amountnan);
+	if (xpAmount <= 0) return msg.reply(lang.givexp_atleast1);
 
 	await sql.get(`SELECT * FROM scores WHERE guildId ="${msg.guild.id}" AND userId ="${userId}"`).then(row => {
 		if (row) {
@@ -31,20 +31,20 @@ exports.run = async (client, msg, args, lang) => {
 		});
 	});
 
-	return msg.reply('done!');
+	return msg.reply(lang.givexp_done);
 };
 
 exports.conf = {
 	enabled: true,
 	guildOnly: false,
-	shortDescription: 'Credits',
+	shortDescription: 'xp',
 	aliases: [],
 	userpermissions: [],
 	dashboardsettings: true
 };
 exports.help = {
 	name: 'givexp',
-	description: '',
+	description: 'Gives an user a specific amount of XP on this discord server',
 	usage: 'givexp {userid} {amount}',
 	example: ['givexp 327533963923161090 1000'],
 	category: 'botowner',
--- commands/botowner/removexp.js
+++ commands/botowner/removexp.js
@@ -6,9 +6,9 @@ exports.run = async (client, msg, args, lang) => {
 	const userId = msg.mentions.users.first() ? msg.mentions.users.first().id : msg.mentions.users.first() || args.slice(0, 1).join(' ');
 	const xpAmount = parseInt(args.slice(1, 2).join(' '), 10);
 
-	if (args.slice(1, 2).length === 0) return msg.reply('Amount missing!');
-	if (isNaN(xpAmount)) return msg.reply('Not a number!');
-	if (xpAmount <= 0) return msg.reply('At least 1 xp');
+	if (args.slice(1, 2).length === 0) return msg.reply(lang.removexp_noamount);
+	if (isNaN(xpAmount)) return msg.reply(lang.removexp_amountnan);
+	if (xpAmount <= 0) return msg.reply(lang.removexp_atleast1);
 
 	await sql.get(`SELECT * FROM scores WHERE guildId ="${msg.guild.id}" AND userId ="${userId}"`).then(row => {
 		if (row) {
@@ -31,13 +31,13 @@ exports.run = async (client, msg, args, lang) => {
 		});
 	});
 
-	return msg.reply('done!');
+	return msg.reply(lang.removexp_done);
 };
 
 exports.conf = {
 	enabled: true,
 	guildOnly: false,
-	shortDescription: 'Credits',
+	shortDescription: 'xp',
 	aliases: [],
 	userpermissions: [],
 	dashboardsettings: true
