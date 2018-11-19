848258e59aaa5940a8228e67e93a98fc506c3502
Jannik Müller <jannik@andreasmueller.de>
2018-11-19 20:44:30 +0100

    Finalize SettingsProvider(needs testing)

--- utils/SettingsProvider.js
+++ utils/SettingsProvider.js
@@ -11,6 +11,7 @@ module.exports = class LenoxBotSettingsProvider extends Commando.SettingProvider
 
         this.dbClient = new mongodb.MongoClient(url);
         this.guildSettings = new Map();
+        this.listeners = new Map();
     }
     
     async init(client) {
@@ -19,16 +20,115 @@ module.exports = class LenoxBotSettingsProvider extends Commando.SettingProvider
             console.log("Connected to mongodb");
 
             this.db = this.dbClient.db("lenoxbot");
+            const settingsCollection = db.collection('guildSettings');
+
+            settingsCollection.createIndex("guildId", {unique: true});
 
             client.guilds.every(function(guild) {
-                const settingsCollection = db.collection('guildSettings');
-                
-                //Load
+                settingsCollection.findOne({'guildId': guild.id}).then((err, result) => {
+                    if(err) {
+                        //Can't find DB make new one.
+
+                    }
+
+                    this.guildSettings.set(guild.id, result.settings);
+                })
             });
         });
+
+        this.listeners
+        .set('commandPrefixChange', (guild, prefix) => this.set(guild, 'prefix', prefix))
+        .set('commandStatusChange', (guild, command, enabled) => this.set(guild, `cmd-${command.name}`, enabled))
+        .set('groupStatusChange', (guild, group, enabled) => this.set(guild, `grp-${group.id}`, enabled))
+        .set('guildCreate', guild => {
+            const settings = this.guildSettings.get(guild.id);
+            if(!settings) return;
+            this.setupGuild(guild.id, settings);
+        })
+        .set('commandRegister', command => {
+            for(const [guild, settings] of this.guildSettings) {
+                if(guild !== 'global' && !client.guilds.has(guild)) continue;
+                this.setupGuildCommand(client.guilds.get(guild), command, settings);
+            }
+        })
+        .set('groupRegister', group => {
+            for(const [guild, settings] of this.guildSettings) {
+                if(guild !== 'global' && !client.guilds.has(guild)) continue;
+                this.setupGuildGroup(client.guilds.get(guild), group, settings);
+            }
+        });
+        for(const [event, listener] of this.listeners) client.on(event, listener);
+    }
+
+    async destroy() {
+        // Remove all listeners from the client
+		for(const [event, listener] of this.listeners) this.client.removeListener(event, listener);
+        this.listeners.clear();
+    }
+
+    async set(guild, key, val) {
+        guild = this.constructor.getGuildID(guild);
+        let settings = this.guildSettings.get(guild);
+        if(!settings) {
+			settings = {};
+			this.guildSettings.set(guild, settings);
+        }
+
+        settings[key] = val;
+        const settingsCollection = this.db.collection('guildSettings');
+
+        await settingsCollection.save({'guildId': guild, 'settings': settings});
+        return val;
+    }
+
+    async remove(guild, key, val) {
+        guild = this.constructor.getGuildID(guild);
+        let settings = this.guildSettings.get(guild);
+        if(!settings) {
+			settings = {};
+			this.guildSettings.set(guild, settings);
+        }
+
+        const val = settings[key];
+        settings[key] = undefined;
+        const settingsCollection = this.db.collection('guildSettings');
+
+        await settingsCollection.save({'guildId': guild, 'settings': settings});
+        return val;
+    }
+
+    
+	async clear(guild) {
+		guild = this.constructor.getGuildID(guild);
+		if(!this.settings.has(guild)) return;
+        this.settings.delete(guild);
+        const settingsCollection = this.db.collection('guildSettings');
+		await this.settingsCollection.deleteOne({'guildId': guild});
+    }
+
+    get(guild, key, defVal) {
+        const settings = this.guildSettings.get(this.constructor.getGuildID(guild));
+        return settings ? typeof settings[key] !== 'undefined' ? settings[key] : defVal : defVal;
     }
 
     getDatabase() {
         return this.db;
+    }
+
+    /**
+     * Sets the guild up in the db for usage.
+     * @param {snowflake} guildId 
+     * @param {object containing properties} settings 
+     */
+    setupGuild(guildId, settings) {
+
+    }
+
+    setupGuildCommand(guildId, command, setting) {
+
+    }
+
+    setupGuildGroup(guildId, command, setting) {
+
     }
 }
