e45909f9235d60068f48b748f2b0050dc89cbb92
Monkeyyy11 <gaileshd@gmail.com>
2018-12-15 22:39:36 +0100

    added template of ?physics

---
+++ commands/currency/physics.js
@@ -0,0 +1,20 @@
+exports.run = async (client, msg, args, lang) => {
+	// Physics command
+};
+
+exports.conf = {
+	enabled: true,
+	guildOnly: true,
+	shortDescription: 'Games',
+	aliases: [],
+	userpermissions: [],
+	dashboardsettings: true
+};
+exports.help = {
+	name: 'physics',
+	description: '',
+	usage: '',
+	example: [''],
+	category: 'currency',
+	botpermissions: ['SEND_MESSAGES']
+};
