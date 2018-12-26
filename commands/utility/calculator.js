f6d9a0c0e51230a1e0c1607d797cb76cfe8dd7bb
Monkeyyy11 <gaileshd@gmail.com>
2018-12-26 19:03:41 +0100

    New command ?join of the module utility

--- commands/utility/calculator.js
+++ commands/utility/calculator.js
@@ -20,9 +20,7 @@ module.exports = class calculatorCommand extends LenoxCommand {
 	}
 
 	run(msg) {
-		const provider = msg.client.provider;
-
-		const langSet = provider.get(msg.message.guild.id, 'language', 'en-US');
+		const langSet = msg.client.provider.get(msg.message.guild.id, 'language', 'en-US');
 		const lang = require(`../../languages/${langSet}.json`);
 
 		const args = msg.content.split(' ').slice(1);
---
+++ commands/utility/join.js
@@ -0,0 +1,47 @@
+const LenoxCommand = require('../LenoxCommand.js');
+
+module.exports = class joinCommand extends LenoxCommand {
+	constructor(client) {
+		super(client, {
+			name: 'join',
+			group: 'utility',
+			memberName: 'join',
+			description: 'Join a self-assignable role',
+			format: 'join {rolename}',
+			aliases: [],
+			examples: ['join Member'],
+			clientPermissions: ['SEND_MESSAGES', 'MANAGE_ROLES'],
+			userPermissions: ['ADMINISTRATOR'],
+			shortDescription: 'Selfassignableroles',
+			dashboardsettings: true
+		});
+	}
+
+	run(msg) {
+		const langSet = msg.client.provider.get(msg.message.guild.id, 'language', 'en-US');
+		const lang = require(`../../languages/${langSet}.json`);
+
+		const args = msg.content.split(' ').slice(1);
+
+		const addedrole = args.slice().join(' ');
+		const foundRole = msg.message.guild.roles.find(role => role.name.toLowerCase() === args.slice().join(' ').toLowerCase());
+		const author = msg.message.guild.members.get(msg.author.id);
+		const channelID = msg.message.channel.id;
+
+		if (addedrole.length < 1) return msg.reply(lang.join_noinput);
+		if (!foundRole) return msg.reply(lang.join_rolenotexist);
+		if (msg.member.roles.has(foundRole.id)) return msg.reply(lang.join_alreadyhave);
+
+		const selfAssignableRoles = msg.client.provider.get(msg.message.guild.id, 'selfassignableroles', '[]');
+		for (let i = 0; i < selfAssignableRoles.length; i++) {
+			if (foundRole.id === selfAssignableRoles[i]) {
+				try {
+					return author.addRole(foundRole).then(m => m.guild.channels.get(channelID).send(lang.join_roleassigned));
+				} catch (error) {
+					return msg.channel.send(lang.join_nopermission);
+				}
+			}
+		}
+		return msg.reply(lang.join_notwhitelisted);
+	}
+};
