012c706a670b14b0d3a6604fc33a9130578e8b95
Monkeyyy11 <gaileshd@gmail.com>
2018-12-27 16:06:53 +0100

    new command ?translate

---
+++ commands/help/translate.js
@@ -0,0 +1,37 @@
+const LenoxCommand = require('../LenoxCommand.js');
+
+module.exports = class translateCommand extends LenoxCommand {
+	constructor(client) {
+		super(client, {
+			name: 'translate',
+			group: 'help',
+			memberName: 'translate',
+			description: 'Gives you informations about our translation project',
+			format: 'translate',
+			aliases: [],
+			examples: ['translate'],
+			clientPermissions: ['SEND_MESSAGES'],
+			userPermissions: [],
+			shortDescription: 'Help',
+			dashboardsettings: false
+		});
+	}
+
+	run(msg) {
+		const Discord = require('discord.js');
+		const langSet = msg.client.provider.getGuild(msg.message.guild.id, 'language');
+		const lang = require(`../../languages/${langSet}.json`);
+
+
+		const translateEmbed = new Discord.RichEmbed()
+			.setTitle(lang.translate_embedtitle)
+			.setDescription(lang.translate_embeddescription)
+			.addField(lang.translate_embedfieldtitle, 'https://crowdin.com/project/lenoxbot')
+			.setURL('https://crowdin.com/project/lenoxbot')
+			.setColor('BLUE');
+
+		msg.channel.send({
+			embed: translateEmbed
+		});
+	}
+};
