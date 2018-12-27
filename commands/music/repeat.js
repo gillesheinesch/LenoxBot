ebc048985526ee5a28497c5f09cf389bfc3ab91c
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2018-12-27 03:20:00 -0500

    Update repeat.js

--- commands/music/repeat.js
+++ commands/music/repeat.js
@@ -1,1 +1,27 @@
+exports.run = async (client, msg, args, lang) => {
+	const queue = client.queue;
+	const serverQueue = queue.get(msg.guild.id);
 
+  if (!msg.member.voiceChannel) return msg.channel.send(`You are not in a voice channel, please join a voice channel to repeat the song!`);
+	if (!serverQueue) return msg.channel.send(`I can't repeat a song because the queue is empty!`);
+  
+	serverQueue.songs[0].repeat = !serverQueue.songs[0].repeat;
+  msg.channel.send(`Song looping has been ${serverQueue.songs[0].repeat ? '`enabled`' : '`disabled`'}.`);
+};
+
+exports.conf = {
+	enabled: true,
+	guildOnly: false,
+	shortDescription: 'repeat song',
+	aliases: [],
+	userpermissions: [],
+	dashboardsettings: true
+};
+exports.help = {
+	name: 'repeat',
+	description: 'Allows the users to repeat the current playing song.',
+	usage: 'repeat',
+	example: ['repeat'],
+	category: 'music',
+	botpermissions: ['SEND_MESSAGES', 'SPEAK']
+};
