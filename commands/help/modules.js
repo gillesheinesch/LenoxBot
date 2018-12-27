233241bbfcefbce8eb7aaea46aa8a4b0df034420
Monkeyyy11 <gaileshd@gmail.com>
2018-12-27 16:06:48 +0100

    new command ?modules

---
+++ commands/help/modules.js
@@ -0,0 +1,46 @@
+const LenoxCommand = require('../LenoxCommand.js');
+
+module.exports = class modulesCommand extends LenoxCommand {
+	constructor(client) {
+		super(client, {
+			name: 'modules',
+			group: 'help',
+			memberName: 'modules',
+			description: 'Gives you a list of all modules and their meaning',
+			format: 'modules',
+			aliases: ['m'],
+			examples: ['modules'],
+			clientPermissions: ['SEND_MESSAGES'],
+			userPermissions: [],
+			shortDescription: 'Help',
+			dashboardsettings: false
+		});
+	}
+
+	run(msg) {
+		const Discord = require('discord.js');
+		const langSet = msg.client.provider.getGuild(msg.message.guild.id, 'language');
+		const lang = require(`../../languages/${langSet}.json`);
+		const prefix = msg.client.provider.getGuild(msg.message.guild.id, 'prefix');
+
+		const commandscommand = lang.modules_commandscommand.replace('%prefix', prefix);
+		const embed = new Discord.RichEmbed()
+			.setFooter(commandscommand)
+			.setColor('0066CC')
+			.addField('Administration', lang.modules_administration)
+			.addField('Moderation', lang.modules_administration)
+			.addField('Help', lang.modules_help)
+			.addField('Music', lang.modules_music)
+			.addField('Fun', lang.modules_fun)
+			.addField('Searches', lang.modules_searches)
+			.addField('NSFW', lang.modules_nsfw)
+			.addField('Utility', lang.modules_utility)
+			.addField('Application', lang.modules_application)
+			.addField('Currency', lang.modules_currency)
+			.addField('Tickets', lang.modules_tickets)
+			.addField('Customcommands', lang.modules_customcommands)
+			.setTitle(lang.modules_embed);
+
+		msg.channel.send({ embed });
+	}
+};
