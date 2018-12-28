35937bb39f502eac8a9a82a414d3210a6e38356c
Monkeyyy11 <gaileshd@gmail.com>
2018-12-28 16:04:55 +0100

    new command ?ranks

---
+++ commands/utility/ranks.js
@@ -0,0 +1,27 @@
+const LenoxCommand = require('../LenoxCommand.js');
+
+module.exports = class ranksCommand extends LenoxCommand {
+	constructor(client) {
+		super(client, {
+			name: 'ranks',
+			group: 'utility',
+			memberName: 'ranks',
+			description: 'Ranking list, sorted by points',
+			format: 'ranks',
+			aliases: [],
+			examples: ['ranks'],
+			clientPermissions: ['SEND_MESSAGES'],
+			userPermissions: [],
+			shortDescription: 'XP',
+			dashboardsettings: true
+		});
+	}
+
+	run(msg) {
+		const langSet = msg.client.provider.getGuild(msg.message.guild.id, 'language');
+		const lang = require(`../../languages/${langSet}.json`);
+
+		const message = lang.ranks_message.replace('%id', msg.guild.id).replace('%guildname', msg.guild.name);
+		msg.reply(message);
+	}
+};
