4a6dc67b2d9adffa89318676542f257d2ab18d54
Monkeyyy11 <gaileshd@gmail.com>
2018-12-30 12:54:38 +0100

    added dailyreminder check to the ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 exports.run = async client => {
 	const chalk = require('chalk');
 	client.user.setPresence({
@@ -23,6 +24,47 @@ exports.run = async client => {
 					client.guilds.array()[i]._commandPrefix = client.provider.getGuild(client.guilds.array()[i].id, 'prefix');
 				}
 			}
+
+			await client.provider.setBotsettings('botconfs', 'botstats', {
+				botguildscount: client.guilds.size,
+				botmemberscount: client.users.size,
+				botcommands: client.provider.getBotsettings('botconfs', 'commandsexecuted'),
+				botcommandsincrement: Math.floor(client.provider.getBotsettings('botconfs', 'commandsexecuted') / 170) + 1,
+				botmemberscountincrement: Math.floor(client.users.size / 170) + 1,
+				botguildscountincrement: Math.floor(client.guilds.size / 170) + 1
+			});
+
+			function timeoutForDaily(dailyreminder, timeoutTime) {
+				setTimeout(async () => {
+					client.users.get(dailyreminder.userId).send('Don\'t forget to pick up your daily reward');
+					const currentDailyreminder = client.provider.getBotsettings('botconfs', 'dailyreminder');
+					delete currentDailyreminder[dailyreminder.userId];
+					await client.provider.setBotsettings('botconfs', 'dailyreminder', currentDailyreminder);
+				}, timeoutTime);
+			}
+
+			if (typeof client.provider.getBotsettings('botconfs', 'dailyreminder') !== 'undefined') {
+				if (Object.keys(client.provider.getBotsettings('botconfs', 'dailyreminder')).length !== 0) {
+					/* eslint guard-for-in: 0 */
+					for (const index in client.provider.getBotsettings('botconfs', 'dailyreminder')) {
+						const timeoutTime = client.provider.getBotsettings('botconfs', 'dailyreminder')[index].remind - Date.now();
+						console.log(client.provider.getBotsettings('botconfs', 'dailyreminder')[index]);
+						timeoutForDaily(client.provider.getBotsettings('botconfs', 'dailyreminder')[index], timeoutTime);
+					}
+				}
+			}
+		});
+	}
+
+	const embed = new Discord.RichEmbed()
+		.setTitle('Botrestart')
+		.setDescription('LenoxBot had a restart and is back again!\nEveryone can now execute commands!')
+		.setColor('#99ff66')
+		.setAuthor(client.user.tag, client.user.displayAvatarURL);
+
+	if (client.user.id === '354712333853130752') {
+		client.channels.get('497400107109580801').send({
+			embed
 		});
 	}
 };
