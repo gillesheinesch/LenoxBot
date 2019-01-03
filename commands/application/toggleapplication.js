805bda7ecf2519b0fbcdde5ac3ab95faac3ca170
Monkeyyy11 <gaileshd@gmail.com>
2019-01-03 17:21:11 +0100

    fix

--- commands/application/toggleapplication.js
+++ commands/application/toggleapplication.js
@@ -21,6 +21,17 @@ module.exports = class toggleapplicationCommand extends LenoxCommand {
 		const langSet = msg.client.provider.getGuild(msg.message.guild.id, 'language');
 		const lang = require(`../../languages/${langSet}.json`);
 
-		
+		if (msg.client.provider.getGuild(msg.message.guild.id, 'application').status === 'false') {
+			const currentApplication = msg.client.provider.getGuild(msg.message.guild.id, 'application');
+			currentApplication.status = 'true';
+			await msg.client.provider.setGuild(msg.message.guild.id, 'application', currentApplication);
+
+			return msg.channel.send(lang.toggleapplication_activated);
+		}
+		const currentApplication = msg.client.provider.getGuild(msg.message.guild.id, 'application');
+		currentApplication.status = 'false';
+		await msg.client.provider.setGuild(msg.message.guild.id, 'application', currentApplication);
+
+		return msg.channel.send(lang.toggleapplication_disabled);
 	}
 };
