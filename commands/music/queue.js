303f398d2dbf2a0340bebd19ae697002301dba85
Monkeyyy11 <gaileshd@gmail.com>
2019-02-10 18:46:13 +0100

    fixes #332

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -18,7 +18,7 @@ module.exports = class queueCommand extends LenoxCommand {
 		});
 	}
 
-	run(msg) {
+	async run(msg) {
 		const langSet = msg.client.provider.getGuild(msg.message.guild.id, 'language');
 		const lang = require(`../../languages/${langSet}.json`);
 
@@ -28,11 +28,68 @@ module.exports = class queueCommand extends LenoxCommand {
 
 		const nowplaying = lang.queue_nowplaying.replace('%songtitle', serverQueue.songs[0].title);
 		const songqueue = lang.queue_songqueue.replace('%songtitle', serverQueue.songs[0].title);
-		const embed = new Discord.RichEmbed()
+		const queueEmbed = new Discord.RichEmbed()
 			.setColor('#009696')
-			.setDescription(`${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
-		\n${nowplaying}`)
+			.setDescription(`${serverQueue.songs.slice(0, 15).map(song => `**-** ${song.title}`).join('\n')}
+		\n**${nowplaying}**`)
 			.setAuthor(songqueue, 'https://cdn.discordapp.com/attachments/355972323590930432/357097120580501504/unnamed.jpg');
-		return msg.channel.send({ embed });
+
+		const message = await msg.channel.send({
+			embed: queueEmbed
+		});
+
+		if (serverQueue.songs.length > 15) {
+			const reaction1 = await message.react('◀');
+			const reaction2 = await message.react('▶');
+
+			let first = 0;
+			let second = 15;
+
+			const collector = message.createReactionCollector((reaction, user) => user.id === msg.author.id, {
+				time: 120000
+			});
+			collector.on('collect', r => {
+				const reactionadd = serverQueue.songs.slice(first + 15, second + 15).length;
+				const reactionremove = serverQueue.songs.slice(first - 15, second - 15).length;
+
+				if (r.emoji.name === '▶' && reactionadd !== 0) {
+					r.remove(msg.author.id);
+
+					first += 15;
+					second += 15;
+
+					const newEmbed = new Discord.RichEmbed()
+						.setColor('#009696')
+						.setAuthor(songqueue, 'https://cdn.discordapp.com/attachments/355972323590930432/357097120580501504/unnamed.jpg');
+
+					newEmbed.setDescription(`${serverQueue.songs.slice(first, second).map(song => `**-** ${song.title}`).join('\n')}
+					\n**${nowplaying}**`);
+
+					message.edit({
+						embed: newEmbed
+					});
+				} else if (r.emoji.name === '◀' && reactionremove !== 0) {
+					r.remove(msg.author.id);
+
+					first -= 15;
+					second -= 15;
+
+					const newEmbed = new Discord.RichEmbed()
+						.setColor('#009696')
+						.setAuthor(songqueue, 'https://cdn.discordapp.com/attachments/355972323590930432/357097120580501504/unnamed.jpg');
+
+					newEmbed.setDescription(`${serverQueue.songs.slice(first, second).map(song => `**-** ${song.title}`).join('\n')}
+					\n**${nowplaying}**`);
+
+					message.edit({
+						embed: newEmbed
+					});
+				}
+			});
+			collector.on('end', () => {
+				reaction1.remove();
+				reaction2.remove();
+			});
+		}
 	}
 };
