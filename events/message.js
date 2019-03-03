64f12a5b01eb0a33ea4af596c42037323928a466
Monkeyyy11 <gaileshd@gmail.com>
2019-03-03 17:49:41 +0100

    fixes

--- events/message.js
+++ events/message.js
@@ -9,48 +9,6 @@ exports.run = async (client, msg) => {
 	const langSet = msg.client.provider.getGuild(msg.guild.id, 'language');
 	const lang = require(`../languages/${langSet}.json`);
 
-	if (msg.client.provider.getGuild(msg.guild.id, 'modules').utility === 'true') {
-		if (!msg.client.provider.getGuild(msg.guild.id, 'togglexp').channelids.includes(msg.channel.id)) {
-			const currentScores = client.provider.getGuild(msg.guild.id, 'scores');
-			if (currentScores[msg.author.id]) {
-				currentScores[msg.author.id].points += 1;
-			} else {
-				currentScores[msg.author.id] = {
-					points: 0,
-					level: 0
-				};
-			}
-
-			const curLevel = Math.floor(0.3 * Math.sqrt(currentScores[msg.author.id].points + 1));
-			if (curLevel > currentScores[msg.author.id].level) {
-				currentScores[msg.author.id].level = curLevel;
-
-				if (client.provider.getGuild(msg.guild.id, 'xpmessages') === 'true') {
-					const levelup = lang.messageevent_levelup.replace('%author', msg.author).replace('%level', currentScores[msg.author.id].level);
-					msg.channel.send(levelup);
-				}
-			}
-			if (curLevel < currentScores[msg.author.id].level) {
-				currentScores[msg.author.id].level = curLevel;
-
-				if (client.provider.getGuild(msg.guild.id, 'xpmessages') === 'true') {
-					const levelup = lang.messageevent_levelup.replace('%author', msg.author).replace('%level', currentScores[msg.author.id].level);
-					msg.channel.send(levelup);
-				}
-			}
-
-			for (let i = 1; i < client.provider.getGuild(msg.guild.id, 'ara').length; i += 2) {
-				if (client.provider.getGuild(msg.guild.id, 'ara').ara[i] < currentScores[msg.author.id].points && !msg.member.roles.get(client.provider.getGuild(msg.guild.id, 'ara')[i - 1])) {
-					const role = msg.message.guild.roles.get(client.provider.getGuild(msg.guild.id, 'ara')[i - 1]);
-					msg.member.addRole(role);
-
-					const automaticrolegotten = lang.messageevent_automaticrolegotten.replace('%rolename', role.name);
-					msg.channel.send(automaticrolegotten);
-				}
-			}
-			await client.provider.setGuild(msg.guild.id, 'scores', currentScores);
-		}
-	}
 	if (client.provider.getGuild(msg.guild.id, 'language')) { // Everything can be requested here
 		for (const key in guildsettingskeys) {
 			if (!client.provider.getGuild(msg.guild.id, key)) {
@@ -125,6 +83,49 @@ exports.run = async (client, msg) => {
 		}
 	} else {
 		await msg.client.provider.setUserComplete(msg.author.id, usersettingskeys);
+	}
+
+	if (msg.client.provider.getGuild(msg.guild.id, 'modules').utility === 'true') {
+		if (!msg.client.provider.getGuild(msg.guild.id, 'togglexp').channelids.includes(msg.channel.id)) {
+			const currentScores = client.provider.getGuild(msg.guild.id, 'scores');
+			if (currentScores[msg.author.id]) {
+				currentScores[msg.author.id].points += 1;
+			} else {
+				currentScores[msg.author.id] = {
+					points: 0,
+					level: 0
+				};
+			}
+
+			const curLevel = Math.floor(0.3 * Math.sqrt(currentScores[msg.author.id].points + 1));
+			if (curLevel > currentScores[msg.author.id].level) {
+				currentScores[msg.author.id].level = curLevel;
+
+				if (client.provider.getGuild(msg.guild.id, 'xpmessages') === 'true') {
+					const levelup = lang.messageevent_levelup.replace('%author', msg.author).replace('%level', currentScores[msg.author.id].level);
+					msg.channel.send(levelup);
+				}
+			}
+			if (curLevel < currentScores[msg.author.id].level) {
+				currentScores[msg.author.id].level = curLevel;
+
+				if (client.provider.getGuild(msg.guild.id, 'xpmessages') === 'true') {
+					const levelup = lang.messageevent_levelup.replace('%author', msg.author).replace('%level', currentScores[msg.author.id].level);
+					msg.channel.send(levelup);
+				}
+			}
+
+			for (let i = 1; i < client.provider.getGuild(msg.guild.id, 'ara').length; i += 2) {
+				if (client.provider.getGuild(msg.guild.id, 'ara').ara[i] < currentScores[msg.author.id].points && !msg.member.roles.get(client.provider.getGuild(msg.guild.id, 'ara')[i - 1])) {
+					const role = msg.message.guild.roles.get(client.provider.getGuild(msg.guild.id, 'ara')[i - 1]);
+					msg.member.addRole(role);
+
+					const automaticrolegotten = lang.messageevent_automaticrolegotten.replace('%rolename', role.name);
+					msg.channel.send(automaticrolegotten);
+				}
+			}
+			await client.provider.setGuild(msg.guild.id, 'scores', currentScores);
+		}
 	}
 
 	if (!client.provider.getUser(msg.author.id, 'userID')) {
