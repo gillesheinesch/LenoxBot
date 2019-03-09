998d15254d996d2fd18f0855e6968ada04e876e6
maltesermailo <jannik@andreasmueller.de>
2019-03-09 21:23:41 +0100

    Update migrate.js

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -97,52 +97,11 @@ function migrate() {
 					process.stdout.write('3/3 Loading sqlite db for credits and xp');
 
 					const db = await sql.open(`${settingsFile.sqlitefilename}.sqlite`);
-					db.all('SELECT * FROM medals').then(async rows => {
-						for (var row of rows) {
-							const result = await userSettingsCollection.findOne({ userId: row.userId });
-							let settings = undefined;
-
-							if (result && result.settings) {
-								settings = result.settings;
-							} else {
-								settings = usersettingskeys;
-
-								userSettingsCollection.insertOne({ userId: row.userId, settings: settings });
-							}
-
-							settings["credits"] = row.medals;
-							process.stdout.clearLine();
-							process.stdout.cursorTo(0);
-							process.stdout.write(`3/3 Converting credits of user ${row.userId}\n`);
-							userSettingsCollection.updateOne({ userId: row.userId }, { $set: { settings: settings } });
-						}
+					/*.then(async rows => {
+						
 
 						db.all('SELECT * FROM scores').then(async rowsScores => {
-							for (var rowScores of rowsScores) {
-								const result = await guildSettingsCollection.findOne({ guildId: rowScores.guildid });
-								let settings = undefined;
-
-								if (!result || !result.settings) {
-									settings = guildsettingskeys;
-
-									guildSettingsCollection.insertOne({ guildId: rowScores.guildid, settings: settings });
-								} else {
-									settings = result.settings;
-								}
-
-								// This doesn't exist in the normal layout of the old db, so we need to create it.
-								if(!settings["scores"]) {
-									settings["scores"] = {};
-								}
-								let currentScores = settings["scores"];
-								currentScores[rowScores.userId] = {};
-								currentScores[rowScores.userId].points = rowScores.points;
-								currentScores[rowScores.userId].level = rowScores.level;
-								process.stdout.clearLine();
-								process.stdout.cursorTo(0);
-								process.stdout.write(`3/3 Converting score of user ${row.userId}\n`);
-								guildSettingsCollection.updateOne({ guildId: rowScores.guildid }, { $set: { settings: settings } });
-							}
+							
 
 							process.stdout.clearLine();
 							process.stdout.cursorTo(0);
@@ -159,7 +118,54 @@ function migrate() {
 							this.dbClient.close();
 							console.log('Migration done');
 						});
-					});
+					});*/
+					const rows = await db.all('SELECT * FROM medals');
+					for (var row of rows) {
+						const result = await userSettingsCollection.findOne({ userId: row.userId });
+						let settings = undefined;
+
+						if (result && result.settings) {
+							settings = result.settings;
+						} else {
+							settings = usersettingskeys;
+
+							userSettingsCollection.insertOne({ userId: row.userId, settings: settings });
+						}
+
+						settings["credits"] = row.medals;
+						process.stdout.clearLine();
+						process.stdout.cursorTo(0);
+						process.stdout.write(`3/3 Converting credits of user ${row.userId}\n`);
+						await userSettingsCollection.updateOne({ userId: row.userId }, { $set: { settings: settings } });
+					}
+
+
+					const rowsScores = await db.all('SELECT * FROM scores');
+					for (var rowScores of rowsScores) {
+						const result = await guildSettingsCollection.findOne({ guildId: rowScores.guildid });
+						let settings = undefined;
+
+						if (!result || !result.settings) {
+							settings = guildsettingskeys;
+
+							guildSettingsCollection.insertOne({ guildId: rowScores.guildid, settings: settings });
+						} else {
+							settings = result.settings;
+						}
+
+						// This doesn't exist in the normal layout of the old db, so we need to create it.
+						if(!settings["scores"]) {
+							settings["scores"] = {};
+						}
+						let currentScores = settings["scores"];
+						currentScores[rowScores.userId] = {};
+						currentScores[rowScores.userId].points = rowScores.points;
+						currentScores[rowScores.userId].level = rowScores.level;
+						process.stdout.clearLine();
+						process.stdout.cursorTo(0);
+						process.stdout.write(`3/3 Converting score of user ${row.userId}\n`);
+						await guildSettingsCollection.updateOne({ guildId: rowScores.guildid }, { $set: { settings: settings } });
+					}
 				});
 			});
 		});
