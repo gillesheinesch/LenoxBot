3c76a244263253159463749a8df923bba1618f7d
Monkeyyy11 <gaileshd@gmail.com>
2019-03-17 21:11:29 +0100

    removed one line

--- lenoxbot.js
+++ lenoxbot.js
@@ -68,7 +68,6 @@ if (process.env.SHARD_COUNT) {
 
 
 	client.dispatcher.addInhibitor(msg => {
-		if (msg.author.bot) return undefined;
 		if (msg.channel.type !== 'text') return msg.reply(englishlang.messageevent_error);
 		if (!client.provider.isReady) return undefined;
 		console.log(`isReady> ${client.provider.isReady}`)
