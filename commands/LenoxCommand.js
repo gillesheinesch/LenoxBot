d26d6fbc512af920fd00e43d93827fe04bd448ed
Monkeyyy11 <gaileshd@gmail.com>
2019-03-18 16:07:52 +0100

    fix

--- commands/LenoxCommand.js
+++ commands/LenoxCommand.js
@@ -9,30 +9,4 @@ module.exports = class LenoxCommand extends DiscordCommando.Command {
 		this.clientpermissions = info.clientpermissions || [];
 		this.userpermissions = info.userpermissions || [];
 	}
-
-	hasPermission(message, ownerOverride = true) {
-		if (!this.ownerOnly && !this.userpermissions) return true;
-		if (ownerOverride && this.client.isOwner(message.author)) return true;
-
-		const provider = message.client.provider;
-		const langSet = provider.getGuild(message.guild.id, 'language', 'en-US');
-		const lang = require(`../languages/${langSet}.json`);
-
-		if (this.ownerOnly && (ownerOverride || !this.client.isOwner(message.author))) {
-			return `${lang.botownercommands_error}`;
-		}
-
-		if (message.channel.type === 'text' && this.userpermissions) {
-			const missing = message.channel.permissionsFor(message.author).missing(this.userpermissions);
-			if (missing.length > 0) {
-				const botnopermission = lang.messageevent_botnopermission.replace('%missingpermissions', missing.map(perm => missing[perm]).join(', '));
-				if (missing.length === 1) {
-					return `${botnopermission}`;
-				}
-				return `${botnopermission}`;
-			}
-		}
-
-		return true;
-	}
 };
