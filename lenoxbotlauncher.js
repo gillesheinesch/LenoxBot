dda4f9b3981f6bddb2fa0e6a765391d9c58a4129
Monkeyyy11 <gaileshd@gmail.com>
2019-04-03 16:01:21 +0200

    undo

--- lenoxbotlauncher.js
+++ lenoxbotlauncher.js
@@ -66,17 +66,10 @@ if (cluster.isMaster) {
 		rl.prompt();
 	});
 
-	async function fetchShards() {
-		const x = await Discord.fetchRecommendedShards(settings.token, 500);
-		return x;
-	}
-
-	fetchShards().then(r => {
-		shardingManager.spawn(r, 15000).then(() => {
-			console.log(chalk.green(`[ShardManager] Started ${shardingManager.totalShards} shards`));
-		}).catch(error => {
-			console.log(error);
-		});
+	shardingManager.spawn(shardingManager.totalShards, 15000).then(() => {
+		console.log(chalk.green(`[ShardManager] Started ${shardingManager.totalShards} shards`));
+	}).catch(error => {
+		console.log(error);
 	});
 
 	for (let i = 0; i < numCPUs; i++); {
