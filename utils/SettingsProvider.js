7c55d18d1463cc7086e3cd3eca5a052bff638520
Monkeyyy11 <gaileshd@gmail.com>
2019-04-05 17:07:19 +0200

    test2

--- utils/SettingsProvider.js
+++ utils/SettingsProvider.js
@@ -314,9 +314,9 @@ class LenoxBotSettingsProvider extends Commando.SettingProvider {
 	}
 
 	getGuild(guild, key, defVal) {
-		console.log('getGuild 1');
+		if (guild === '352896116812939264') console.log('getGuild 1');
 		const settings = this.guildSettings.get(this.constructor.getGuildID(guild));
-		console.log('getGuild 2');
+		if (guild === '352896116812939264') console.log('getGuild 2');
 		return settings ? typeof settings[key] === 'undefined' ? defVal : settings[key] : defVal;
 	}
 
