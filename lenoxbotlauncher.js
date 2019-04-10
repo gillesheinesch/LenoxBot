20e0229030c6ee14bd5e306079fbe4c53b4b1e1d
Monkeyyy11 <gaileshd@gmail.com>
2019-04-10 11:36:36 +0200

    fixes

--- lenoxbotlauncher.js
+++ lenoxbotlauncher.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const settings = require('./settings.json');
 const chalk = require('chalk');
 
-const userdb = new Map();
 const shardingManager = new Discord.ShardingManager('./lenoxbot.js',
 	{
 		token: settings.token
