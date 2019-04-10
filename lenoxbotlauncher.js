e4fb12f503b34e17d845b88d6d92abb68fb25d59
Monkeyyy11 <gaileshd@gmail.com>
2019-04-10 12:11:51 +0200

    test api

--- lenoxbotlauncher.js
+++ lenoxbotlauncher.js
@@ -12,3 +12,45 @@ shardingManager.spawn(shardingManager.totalShards, 15000).then(() => {
 }).catch(error => {
 	console.log(error);
 });
+
+const express = require('express');
+const app = express();
+
+function authorizationCodeCheck(authorizationcode, res) {
+	if (settings.apitoken !== authorizationcode) {
+		return false;
+	}
+	return true;
+}
+
+// get all todos
+app.get('/api/v1/:authorizationcode/islenoxbotondiscordserver/:guildid', async (req, res) => {
+	const check = authorizationCodeCheck(req.params.authorizationcode, res);
+	if (!check) {
+		return res.status(401).send({
+			success: 'false',
+			message: 'Unauthorized: Token invalid'
+		});
+	}
+
+	const result = await shardingManager.broadcastEval(`this.guilds.get('${req.params.guildid}')`);
+
+	if (result && typeof result[0] !== 'undefined') {
+		res.status(200).send({
+			success: 'true',
+			message: 'Guild found',
+			content: result
+		});
+	} else {
+		res.status(404).send({
+			success: 'false',
+			message: 'Guild not found'
+		});
+	}
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+	console.log(`API running on port ${settings.websiteport}`);
+});
