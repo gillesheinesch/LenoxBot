b93939055fc780195b593a6095b8c1e6cc77f33c
Monkeyyy11 <gaileshd@gmail.com>
2019-05-10 18:42:25 +0200

    fixed presenceupdate event which wasn't working

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -1,23 +1,23 @@
 const Discord = require('discord.js');
-exports.run = (client, oldMember, newMember) => {
+exports.run = (client, oldPresence, newPresence) => {
 	if (!client.provider.isReady) return;
-	if (!client.provider.getGuild(newMember.guild.id, 'prefix')) return;
+	if (!client.provider.getGuild(oldPresence.guild.id, 'prefix')) return;
 
-	if (client.provider.getGuild(newMember.guild.id, 'presenceupdatelog') === 'false') return;
+	if (client.provider.getGuild(oldPresence.guild.id, 'presenceupdatelog') === 'false') return;
 
-	const lang = require(`../languages/${client.provider.getGuild(newMember.guild.id, 'language')}.json`);
+	const lang = require(`../languages/${client.provider.getGuild(oldPresence.guild.id, 'language')}.json`);
 
-	const messagechannel = client.channels.get(client.provider.getGuild(newMember.guild.id, 'presenceupdatelogchannel'));
+	const messagechannel = client.channels.get(client.provider.getGuild(oldPresence.guild.id, 'presenceupdatelogchannel'));
 	if (!messagechannel) return;
 
-	/* if (oldMember.presence.status !== newMember.presence.status) {
+	if (oldPresence.status !== newPresence.status) {
 		const embed = new Discord.MessageEmbed()
 			.setColor('ORANGE')
 			.setTimestamp()
 			.setAuthor(lang.presenceupdateevent_changed)
-			.addField(`📎 ${lang.presenceupdateevent_member}:`, `${oldMember.user.tag} (${oldMember.id})`)
-			.addField(`📤 ${lang.presenceupdateevent_old}:`, oldMember.presence.status)
-			.addField(`📥 ${lang.presenceupdateevent_new}:`, newMember.presence.status);
+			.addField(`📎 ${lang.presenceupdateevent_member}:`, `${oldPresence.user.tag} (${oldPresence.userID})`)
+			.addField(`📤 ${lang.presenceupdateevent_old}:`, oldPresence.status)
+			.addField(`📥 ${lang.presenceupdateevent_new}:`, newPresence.status);
 		messagechannel.send({ embed: embed });
-	}*/
+	}
 };
