f949bc1e7eb0406bea9970028b3a6ec9b1443628
Monkeyyy11 <gaileshd@gmail.com>
2019-06-08 10:57:01 +0200

    closes #474

--- commands/staff/givecredits.js
+++ commands/staff/givecredits.js
@@ -1,5 +1,6 @@
 const LenoxCommand = require('../LenoxCommand.js');
 const Discord = require('discord.js');
+const settings = require('../../settings.json');
 
 module.exports = class givecreditsCommand extends LenoxCommand {
 	constructor(client) {
@@ -23,6 +24,12 @@ module.exports = class givecreditsCommand extends LenoxCommand {
 		const langSet = msg.client.provider.getGuild(msg.guild.id, 'language');
 		const lang = require(`../../languages/${langSet}.json`);
 		const args = msg.content.split(' ').slice(1);
+
+		if (settings.owners.includes(msg.author.id) || settings.administrators.includes(msg.author.id)) {
+			const timestamps = msg.client.provider.getBotsettings('botconfs', 'givecredits');
+			delete timestamps.ticket[msg.author.id];
+			await msg.client.provider.setBotsettings('botconfs', 'givecredits', timestamps);
+		}
 
 		const guild = msg.client.guilds.get('352896116812939264').roles.find(r => r.name.toLowerCase() === 'moderator').id;
 		if (!msg.member.roles.get(guild)) return msg.reply(lang.botownercommands_error);
