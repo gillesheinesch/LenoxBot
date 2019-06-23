694e900b87b70476fea24982ea1b2ef2c85cff6d
BannerBomb <BannerBomb55@gmail.com>
2019-06-23 16:02:22 -0400

    updated to match changes
    updated to match the new changes

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -1,25 +1,27 @@
 const Discord = require('discord.js');
-exports.run = (client, channel) => {
-	if (!channel) return;
-	if (channel.type !== 'text') return;
-	if (!client.provider.isReady) return;
+module.exports = {
+	run: (client, channel) => {
+		if (!channel) return;
+		if (channel.type !== 'text') return;
+		if (!client.provider.isReady) return;
 
-	if (!client.provider.getGuild(channel.guild.id, 'prefix')) return;
-	if (client.provider.getGuild(channel.guild.id, 'channelcreatelog') === 'false') return;
+		if (!client.provider.getGuild(channel.guild.id, 'prefix')) return;
+		if (client.provider.getGuild(channel.guild.id, 'channelcreatelog') === 'false') return;
 
-	const langSet = client.provider.getGuild(channel.guild.id, 'language');
-	const lang = require(`../languages/${langSet}.json`);
+		const langSet = client.provider.getGuild(channel.guild.id, 'language');
+		const lang = require(`../languages/${langSet}.json`);
 
-	const messagechannel = client.channels.get(client.provider.getGuild(channel.guild.id, 'channelcreatelogchannel'));
-	if (!messagechannel) return;
+		const messagechannel = client.channels.get(client.provider.getGuild(channel.guild.id, 'channelcreatelogchannel'));
+		if (!messagechannel) return;
 
-	const embed = new Discord.MessageEmbed()
-		.setColor('GREEN')
-		.setTimestamp()
-		.setAuthor(lang.channelcreateevent_channelcreated)
-		.addField(`📎 ${lang.channelcreateevent_channelid}:`, channel.id)
-		.addField(`📝 ${lang.channelcreateevent_name}`, channel.name);
-	messagechannel.send({
-		embed: embed
-	});
+		const embed = new Discord.MessageEmbed()
+			.setColor('GREEN')
+			.setTimestamp()
+			.setAuthor(lang.channelcreateevent_channelcreated)
+			.addField(`📎 ${lang.channelcreateevent_channelid}:`, channel.id)
+			.addField(`📝 ${lang.channelcreateevent_name}`, channel.name);
+		messagechannel.send({
+			embed: embed
+		});
+	}
 };
