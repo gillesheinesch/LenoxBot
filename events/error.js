306202b3516cfe9b27702611f9b09b4228a84117
BannerBomb <BannerBomb55@gmail.com>
2019-06-23 16:56:14 -0400

    removed the client argument
    removed the client argument

--- events/error.js
+++ events/error.js
@@ -1,5 +1,17 @@
 module.exports = {
-	run: (client, error) => {
-		console.log(error);
+	run: (error) => {
+		if (!client.ready || !client.settings || !client.settings.log_channels || !client.settings.log_channels.errors || !error) return;
+		try {
+			if (client.channels.has(client.settings.log_channels.errors)) client.channels.get(client.settings.log_channels.errors).send(null, {
+				embed: {
+					color: 15684432,
+					timestamp: new Date(),
+					title: 'Error',
+					description: error.stack ? `\`\`\`x86asm\n${error.stack}\n\`\`\`` : `\`${error.toString()}\``
+				}
+			});
+		} catch (error) {
+			return console.error(error.stack ? error.stack : error.toString());
+		}
 	}
-};
+}
