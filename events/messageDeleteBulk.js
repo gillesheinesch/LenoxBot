d9dfb5b4b17c6227401242f7a41691b90ae2c191
BannerBomb <BannerBomb55@gmail.com>
2019-06-23 16:13:49 -0400

    updated to match new changes
    updated to match new changes

--- events/messageDeleteBulk.js
+++ events/messageDeleteBulk.js
@@ -1,33 +1,35 @@
 const Discord = require('discord.js');
-exports.run = (client, messages) => {
-	if (!client.provider.isReady) return;
-	if (messages.size === 0) return;
+module.exports = {
+	run: (client, messages) => {
+		if (!client.provider.isReady) return;
+		if (messages.size === 0) return;
 
-	/* eslint guard-for-in: 0 */
-	messages.forEach(msg => {
-		if (msg.author.bot) return;
-		if (msg.channel.type !== 'text') return;
+		/* eslint guard-for-in: 0 */
+		messages.forEach(msg => {
+			if (msg.author.bot) return;
+			if (msg.channel.type !== 'text') return;
 
-		if (!client.provider.getGuild(msg.guild.id, 'prefix')) return;
+			if (!client.provider.getGuild(msg.guild.id, 'prefix')) return;
 
-		if (client.provider.getGuild(msg.guild.id, 'messagedellog') === 'false') return;
-		const messagechannel = client.channels.get(client.provider.getGuild(msg.guild.id, 'messagedellogchannel'));
+			if (client.provider.getGuild(msg.guild.id, 'messagedellog') === 'false') return;
+			const messagechannel = client.channels.get(client.provider.getGuild(msg.guild.id, 'messagedellogchannel'));
 
-		if (!messagechannel) return;
+			if (!messagechannel) return;
 
-		const lang = require(`../languages/${client.provider.getGuild(msg.guild.id, 'language')}.json`);
+			const lang = require(`../languages/${client.provider.getGuild(msg.guild.id, 'language')}.json`);
 
-		const embed = new Discord.MessageEmbed()
-			.setColor('RED')
-			.setTimestamp()
-			.setAuthor(lang.messagedeleteevent_deleted)
-			.addField(`🗣 ${lang.messagedeleteevent_author}`, msg.author.tag)
-			.addField(`📲 ${lang.messagedeleteevent_channel}`, `${msg.channel.name} (${msg.channel.id})`)
-			.addField(`📎 ${lang.messagedeleteevent_mid}`, msg.id)
-			.addField(`📜 ${lang.messagedeleteevent_message}`, msg.cleanContent.length >= 1 ? msg.cleanContent.substring(0, 960) : '-');
+			const embed = new Discord.MessageEmbed()
+				.setColor('RED')
+				.setTimestamp()
+				.setAuthor(lang.messagedeleteevent_deleted)
+				.addField(`🗣 ${lang.messagedeleteevent_author}`, msg.author.tag)
+				.addField(`📲 ${lang.messagedeleteevent_channel}`, `${msg.channel.name} (${msg.channel.id})`)
+				.addField(`📎 ${lang.messagedeleteevent_mid}`, msg.id)
+				.addField(`📜 ${lang.messagedeleteevent_message}`, msg.cleanContent.length >= 1 ? msg.cleanContent.substring(0, 960) : '-');
 
-		messagechannel.send({
-			embed: embed
+			messagechannel.send({
+				embed: embed
+			});
 		});
-	});
+	}
 };
