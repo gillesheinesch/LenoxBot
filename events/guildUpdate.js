f0809e4ca7ebc46f3185185969b5d839a66779b7
BannerBomb <BannerBomb55@gmail.com>
2019-06-23 17:01:39 -0400

    removed the client argument
    removed the client argument

--- events/guildUpdate.js
+++ events/guildUpdate.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 module.exports = {
-	run: (client, oldGuild, newGuild) => {
+	run: (oldGuild, newGuild) => {
 		if (!client.provider.isReady) return;
 		if (!client.provider.getGuild(newGuild.id, 'prefix')) return;
 
