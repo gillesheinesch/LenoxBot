2ca7a7824f735cf07bca3b25633d7d7f6255c3c1
BannerBomb <BannerBomb55@gmail.com>
2019-06-23 16:18:34 -0400

    updated to match new changes
    updated to match new changes

--- events/roleUpdate.js
+++ events/roleUpdate.js
@@ -1,45 +1,47 @@
 const Discord = require('discord.js');
-exports.run = (client, oldRole, newRole) => {
-	if (!client.provider.isReady) return;
-	if (!client.provider.getGuild(oldRole.guild.id, 'prefix')) return;
+module.exports = {
+	run: (client, oldRole, newRole) => {
+		if (!client.provider.isReady) return;
+		if (!client.provider.getGuild(oldRole.guild.id, 'prefix')) return;
 
-	if (client.provider.getGuild(oldRole.guild.id, 'rolecreatelog') === 'false') return;
+		if (client.provider.getGuild(oldRole.guild.id, 'rolecreatelog') === 'false') return;
 
-	const lang = require(`../languages/${client.provider.getGuild(oldRole.guild.id, 'language')}.json`);
+		const lang = require(`../languages/${client.provider.getGuild(oldRole.guild.id, 'language')}.json`);
 
-	const messagechannel = client.channels.get(client.provider.getGuild(oldRole.guild.id, 'rolecreatelogchannel'));
-	if (!messagechannel) return;
+		const messagechannel = client.channels.get(client.provider.getGuild(oldRole.guild.id, 'rolecreatelogchannel'));
+		if (!messagechannel) return;
 
-	if (oldRole.name !== newRole.name) {
-		const embed = new Discord.MessageEmbed()
-			.setColor('ORANGE')
-			.setTimestamp()
-			.setAuthor(lang.roleupdateevent_nameupdated)
-			.addField(`📎 ${lang.rolecreateevent_id}:`, oldRole.id)
-			.addField(`📤 ${lang.roleupdateevent_oldname}:`, oldRole.name)
-			.addField(`📥 ${lang.roleupdateevent_newname}:`, newRole.name);
-		return messagechannel.send({ embed: embed });
-	}
-	if (oldRole.hexColor !== newRole.hexColor) {
-		const embed = new Discord.MessageEmbed()
-			.setColor('ORANGE')
-			.setTimestamp()
-			.setAuthor(lang.roleupdateevent_rolecolorupdated)
-			.addField(`⚙ ${lang.rolecreateevent_rolename}:`, oldRole.name)
-			.addField(`📎 ${lang.rolecreateevent_id}:`, oldRole.id)
-			.addField(`📤${lang.roleupdateevent_oldcolor}:`, oldRole.hexColor)
-			.addField(`📥 ${lang.roleupdateevent_newcolor}:`, newRole.hexColor);
-		return messagechannel.send({ embed: embed });
-	}
-	if (oldRole.rawPosition !== newRole.rawPosition) {
-		const embed = new Discord.MessageEmbed()
-			.setColor('ORANGE')
-			.setTimestamp()
-			.setAuthor(lang.roleupdateevent_positionupdated)
-			.addField(`⚙ ${lang.rolecreateevent_rolename}:`, oldRole.name)
-			.addField(`📎 ${lang.rolecreateevent_id}:`, oldRole.id)
-			.addField(`📤 ${lang.roleupdateevent_old}:`, oldRole.rawPosition)
-			.addField(`📥 ${lang.roleupdateevent_new}:`, newRole.rawPosition);
-		return messagechannel.send({ embed: embed });
+		if (oldRole.name !== newRole.name) {
+			const embed = new Discord.MessageEmbed()
+				.setColor('ORANGE')
+				.setTimestamp()
+				.setAuthor(lang.roleupdateevent_nameupdated)
+				.addField(`📎 ${lang.rolecreateevent_id}:`, oldRole.id)
+				.addField(`📤 ${lang.roleupdateevent_oldname}:`, oldRole.name)
+				.addField(`📥 ${lang.roleupdateevent_newname}:`, newRole.name);
+			return messagechannel.send({ embed: embed });
+		}
+		if (oldRole.hexColor !== newRole.hexColor) {
+			const embed = new Discord.MessageEmbed()
+				.setColor('ORANGE')
+				.setTimestamp()
+				.setAuthor(lang.roleupdateevent_rolecolorupdated)
+				.addField(`⚙ ${lang.rolecreateevent_rolename}:`, oldRole.name)
+				.addField(`📎 ${lang.rolecreateevent_id}:`, oldRole.id)
+				.addField(`📤${lang.roleupdateevent_oldcolor}:`, oldRole.hexColor)
+				.addField(`📥 ${lang.roleupdateevent_newcolor}:`, newRole.hexColor);
+			return messagechannel.send({ embed: embed });
+		}
+		if (oldRole.rawPosition !== newRole.rawPosition) {
+			const embed = new Discord.MessageEmbed()
+				.setColor('ORANGE')
+				.setTimestamp()
+				.setAuthor(lang.roleupdateevent_positionupdated)
+				.addField(`⚙ ${lang.rolecreateevent_rolename}:`, oldRole.name)
+				.addField(`📎 ${lang.rolecreateevent_id}:`, oldRole.id)
+				.addField(`📤 ${lang.roleupdateevent_old}:`, oldRole.rawPosition)
+				.addField(`📥 ${lang.roleupdateevent_new}:`, newRole.rawPosition);
+			return messagechannel.send({ embed: embed });
+		}
 	}
 };
