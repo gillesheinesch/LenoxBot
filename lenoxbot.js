16b059353664b273f03c537a297d6c953a6b187d
BannerBomb <BannerBomb55@gmail.com>
2019-06-23 17:21:08 -0400

    added a disabled property to events
    added a disabled property to events to allow disabling events.

--- lenoxbot.js
+++ lenoxbot.js
@@ -43,6 +43,7 @@ fs.readdir('./events/', (err, files) => {
 	if (err) return console.error(err);
 	files.forEach(file => {
 		const eventFunction = require(`./events/${file}`);
+		if (eventFunction.disabled) return;
 		const event = eventFunction.event || file.split('.')[0];
 		const emitter = (typeof eventFunction.emitter === 'string' ? client[eventFunction.emitter] : eventFunction.emitter) || client;
 		const once = eventFunction.once;
