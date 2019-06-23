86f628331c6c19b9a64de6e5c8cb985ac98ea0e6
BannerBomb <BannerBomb55@gmail.com>
2019-06-23 16:17:56 -0400

    updated to match changes
    updated to match changes

--- events/roleDelete.js
+++ events/roleDelete.js
@@ -1,29 +1,31 @@
 const Discord = require('discord.js');
-exports.run = (client, role) => {
-	if (!client.provider.isReady) return;
-	if (!client.provider.getGuild(role.guild.id, 'prefix')) return;
+module.exports = {
+	run: (client, role) => {
+		if (!client.provider.isReady) return;
+		if (!client.provider.getGuild(role.guild.id, 'prefix')) return;
 
-	for (let i = 0; i < client.provider.getGuild(role.guild.id, 'selfassignableroles').length; i++) {
-		if (role.id === client.provider.getGuild(role.guild.id, 'selfassignableroles')[i]) {
-			const currentSelfassignableroles = client.provider.getGuild(role.guild.id, 'selfassignableroles');
-			currentSelfassignableroles.splice(i, 1);
-			return client.provider.setGuild(role.guild.id, 'selfassignableroles', currentSelfassignableroles);
+		for (let i = 0; i < client.provider.getGuild(role.guild.id, 'selfassignableroles').length; i++) {
+			if (role.id === client.provider.getGuild(role.guild.id, 'selfassignableroles')[i]) {
+				const currentSelfassignableroles = client.provider.getGuild(role.guild.id, 'selfassignableroles');
+				currentSelfassignableroles.splice(i, 1);
+				return client.provider.setGuild(role.guild.id, 'selfassignableroles', currentSelfassignableroles);
+			}
 		}
+		if (role.name === 'LenoxBot') return;
+		if (client.provider.getGuild(role.guild.id, 'roledeletelog') === 'false') return;
+
+		const lang = require(`../languages/${client.provider.getGuild(role.guild.id, 'language')}.json`);
+
+		const messagechannel = client.channels.get(client.provider.getGuild(role.guild.id, 'roledeletelogchannel'));
+		if (!messagechannel) return;
+
+		const embed = new Discord.MessageEmbed()
+			.setColor('RED')
+			.setTimestamp()
+			.setAuthor(lang.roledeleteevent_deleted)
+			.addField(`📎 ${lang.rolecreateevent_id}:`, role.id)
+			.addField(`🔰 ${lang.rolecreateevent_color}:`, role.hexColor)
+			.addField(`📝 ${lang.rolecreateevent_name}:`, role.name);
+		messagechannel.send({ embed: embed });
 	}
-	if (role.name === 'LenoxBot') return;
-	if (client.provider.getGuild(role.guild.id, 'roledeletelog') === 'false') return;
-
-	const lang = require(`../languages/${client.provider.getGuild(role.guild.id, 'language')}.json`);
-
-	const messagechannel = client.channels.get(client.provider.getGuild(role.guild.id, 'roledeletelogchannel'));
-	if (!messagechannel) return;
-
-	const embed = new Discord.MessageEmbed()
-		.setColor('RED')
-		.setTimestamp()
-		.setAuthor(lang.roledeleteevent_deleted)
-		.addField(`📎 ${lang.rolecreateevent_id}:`, role.id)
-		.addField(`🔰 ${lang.rolecreateevent_color}:`, role.hexColor)
-		.addField(`📝 ${lang.rolecreateevent_name}:`, role.name);
-	messagechannel.send({ embed: embed });
 };
