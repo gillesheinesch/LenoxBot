75856f51631235a28efd914a1aab4ea3b06fd8ad
BannerBomb <BannerBomb55@gmail.com>
2019-06-25 17:05:20 -0400

    cleaned out branch for klasa rewrite

--- public/script/newdocumentation.js
+++
@@ -1,8 +0,0 @@
-$("document").ready(function () {
-    $("#myInput").on("keyup", function () {
-		var value = $(this).val().toLowerCase();
-		$(".card").filter(function () {
-			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-		});
-	});
-});
--- server/routes/error.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const errorController = require('../controllers/error');
-
-const router = express.Router();
-
-router.get('/', errorController.index);
-
-module.exports = router;
