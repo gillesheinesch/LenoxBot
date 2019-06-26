83c56cfae9d2f9b906069dad8a1f8dac55b5be67
Monkeyyy11 <gaileshd@gmail.com>
2019-06-26 17:32:37 +0200

    eslint

--- commands/nsfw/hentaihaven.js
+++ commands/nsfw/hentaihaven.js
@@ -1,6 +1,6 @@
 const LenoxCommand = require('../LenoxCommand.js');
 const Discord = require('discord.js');
-const { JSDOM } = require("jsdom");
+const { JSDOM } = require('jsdom');
 
 module.exports = class hentaihavenCommand extends LenoxCommand {
 	constructor(client) {
@@ -26,15 +26,14 @@ module.exports = class hentaihavenCommand extends LenoxCommand {
 
 		try {
 			/* eslint no-undef: 0 */
-			
-			JSDOM.fromURL('https://hentaihaven.org').then((res) => {
-				let elements = res.window.document.querySelectorAll('head > script');
-				let element = Array.from(elements).filter((elem) => elem.text.startsWith('/*  */\nvar Pukka = '))[0];
-				const Pukka = JSON.parse(element.text.replace(/^\/\*  \*\/\nvar Pukka = |\;\n\/\*  \*\/$/g, ''));
+			JSDOM.fromURL('https://hentaihaven.org').then(res => {
+				const elements = res.window.document.querySelectorAll('head > script');
+				const element = Array.from(elements).filter(elem => elem.text.startsWith('/*  */\nvar Pukka = '))[0];
+				const Pukka = JSON.parse(element.text.replace(/^\/\* {2}\*\/\nvar Pukka = |\;\n\/\* {2}\*\/$/g, ''));
 				const random_category = Pukka.category_links[Math.floor(Math.random() * (Pukka.category_links.length - 1))];
-				JSDOM.fromURL(random_category).then((resource) => {
+				JSDOM.fromURL(random_category).then(resource => {
 					const document = resource.window.document;
-					const title = document.getElementsByClassName("archive-title")[0].textContent;
+					const title = document.getElementsByClassName('archive-title')[0].textContent;
 					const videos = document.getElementsByClassName('hidden animate_video');
 					const video_selection = Math.floor(Math.random() * (videos.length - 1)); // select a random video off the page
 					const video_url = videos[video_selection].attributes[1].value;
@@ -44,16 +43,15 @@ module.exports = class hentaihavenCommand extends LenoxCommand {
 						.setURL(video_url)
 						.setDescription(`[Video URL](${video_url})`)
 						.setColor('#ff0000')
-						.setURL(video_url)
 						.setAuthor(title);
 
 					return msg.channel.send({
 						embed: embed
 					});
-				}).catch((err) => {
+				}).catch(err => {
 					throw err;
 				});
-			}).catch((e) => {
+			}).catch(e => {
 				throw e;
 			});
 		} catch (error) {
