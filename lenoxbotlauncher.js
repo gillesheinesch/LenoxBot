8e7f404fe2700935c0f14908b20d4139a7e1254c
Monkeyyy11 <gaileshd@gmail.com>
2019-06-27 18:48:22 +0200

    fixes #515

--- lenoxbotlauncher.js
+++ lenoxbotlauncher.js
@@ -839,9 +839,11 @@ async function run() {
 				botconfs: 'botconfs'
 			});
 			const newcommandlist = [];
+			const lenoxbotLang = require(`./languages/${req.getLocale()}`);
 			// eslint-disable-next-line guard-for-in
 			for (const key in commandlist.settings.commands) {
 				commandlist.settings.commands[key].usage = `?${commandlist.settings.commands[key].usage}`;
+				commandlist.settings.commands[key].description = lenoxbotLang[`${commandlist.settings.commands[key].name}_description`];
 				newcommandlist.push(commandlist.settings.commands[key]);
 			}
 
