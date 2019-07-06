8357fc7b129e59017fd12c6f3319dc472a7c3529
BannerBomb <BannerBomb55@gmail.com>
2019-07-06 19:42:31 -0400

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const path = require('path');
 const shard = false;
 
 module.exports = {
-	Client: require(path.resolve(__dirname, './lib/LenoxClient'))
+	Client: require(path.resolve(__dirname, './lib/LenoxClient')),
+	ModLog: require(path.resolve(__dirname, '../lib/classes/modlogs'))
 };
 
 module.exports = require(`./${shard ? 'shard' : 'bot'}`);
