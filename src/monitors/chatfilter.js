73d168ff1b1776162875569de4a589a465c68990
BannerBomb <BannerBomb55@gmail.com>
2019-07-08 00:25:47 -0400

    condenced script

--- src/monitors/chatfilter.js
+++ src/monitors/chatfilter.js
@@ -13,14 +13,14 @@ module.exports = class extends Monitor {
 	async run(message) {
 		if (!message.guild || !message.content || !message.content.length) return;
 
-		const [chatfilter_enabled, chatfilter_array] = message.guildSettings.pluck('chatfilter.chatfilter_enabled', 'chatfilter.chatfilter_array');
+		const [chatfilter_enabled, chatfilter_array, chatfilterlog_channel] = message.guildSettings.pluck('chatfilter.chatfilter_enabled', 'chatfilter.chatfilter_array', 'chatfilter.chatfilterlog_channel');
 		if (!chatfilter_enabled || !chatfilter_array.length || message.command) return;
 
 		const getFiltered = message.content.split(' ').filter(m => chatfilter_array.includes(m));
 		if (!getFiltered.length) return;
 
-		if (message.guildSettings.get('chatfilter.chatfilterlog_enabled') && message.guildSettings.get('chatfilter.chatfilterlog_channel')) {
-			message.guildSettings.get('chatfilter.chatfilterlog_channel').send(new MessageEmbed()
+		if (chatfilter_enabled && chatfilterlog_channel) {
+			chatfilterlog_channel.send(new MessageEmbed()
 				.setAuthor(`A message from ${message.author.tag} was deleted by the chat filter.`)
 				.setColor('RED')
 				.addField(`🗣 ${message.language.get('MONITOR_CHATFILTER_AUTHOR')}:`, message.author.tag)
