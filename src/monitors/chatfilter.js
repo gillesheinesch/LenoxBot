a8e767c9d7e5dc94cd51910a3e2adb1e6406a331
Mark Fisher <markf0298@gmail.com>
2019-07-08 06:21:13 +0100

    More condencing

--- src/monitors/chatfilter.js
+++ src/monitors/chatfilter.js
@@ -13,14 +13,14 @@ module.exports = class extends Monitor {
 	async run(message) {
 		if (!message.guild || !message.content || !message.content.length) return;
 
-		const [chatfilter_enabled, chatfilter_array, chatfilterlog_channel] = message.guildSettings.pluck('chatfilter.chatfilter_enabled', 'chatfilter.chatfilter_array', 'chatfilter.chatfilterlog_channel');
-		if (!chatfilter_enabled || !chatfilter_array.length || message.command) return;
+		const [enabled, array, logEnabled, logChannel] = message.guildSettings.pluck('chatfilter.chatfilter_enabled', 'chatfilter.chatfilter_array', 'chatfilter.chatfilterlog_enabled', 'chatfilter.chatfilterlog_channel');
+		if (!enabled || !array.length || message.command) return;
 
-		const getFiltered = message.content.split(' ').filter(m => chatfilter_array.includes(m));
+		const getFiltered = message.content.split(' ').filter(m => array.includes(m));
 		if (!getFiltered.length) return;
 
-		if (chatfilter_enabled && chatfilterlog_channel) {
-			message.guild.channels.get(chatfilterlog_channel).send(new MessageEmbed()
+		if (logEnabled && logChannel) {
+			message.guild.channels.get(logChannel).send(new MessageEmbed()
 				.setAuthor(`A message from ${message.author.tag} was deleted by the chat filter.`)
 				.setColor('RED')
 				.addField(`🗣 ${message.language.get('MONITOR_CHATFILTER_AUTHOR')}:`, message.author.tag)
