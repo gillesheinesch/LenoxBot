59b923173060ea0077f733e30a89d85c5567d251
BannerBomb <BannerBomb55@gmail.com>
2019-07-08 22:32:07 -0400

    update

--- src/events/guildBanAdd.js
+++ src/events/guildBanAdd.js
@@ -4,7 +4,7 @@ const ModLog = require('../lib/classes/modlogs');
 module.exports = class extends Event {
 	run(guild, user) {
 		if (this.client.ready) {
-			if (guild.guild.get('moderations.modlogs_enabled')) {
+			if (guild.settings.get('moderations.modlogs_enabled')) {
 				ModLog()
 					.setAction('ban')
 					.setUser(user)
