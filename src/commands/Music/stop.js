2374ea7884307c612fb15219f18c168e45fe4b8e
BannerBomb <BannerBomb55@gmail.com>
2019-07-14 04:56:21 -0400

    update

--- src/commands/Music/stop.js
+++ src/commands/Music/stop.js
@@ -6,22 +6,24 @@ module.exports = class extends Command {
 			runIn: ['text'],
 			description: language => language.get('COMMAND_STOP_DESCRIPTION'),
 			extendedHelp: language => language.get('COMMAND_STOP_EXTENDEDHELP'),
+			aliases: ['disconnect'],
 			requiredPermissions: ['SEND_MESSAGES', 'CONNECT'],
 			userPermissions: ['CONNECT'],
 			permissionLevel: 6
 		});
 	}
 
-	run(message) {
-		const { queue } = message.guildSettings.get('music');
+	async run(message) {
+		const music_settings = message.guildSettings.get('music');
 		const voice_connection = message.guild.voice ? message.guild.voice.connection : null;
 		if (!voice_connection || !queue.length) return message.channel.sendLocale('MUSIC_NOAUDIOBEINGPLAYED');
 		try {
-			if (queue.length) queue.length = 0;
-			if (voice_connection) voice_connection.disconnect();
+			if (music_settings.queue.length) queue.length = 0;
+			music_settings.loop = false;
+			if (voice_connection) await voice_connection.disconnect();
 			message.channel.sendLocale('MUSIC_SUCCESSFULLYDISCONNECTED');
 		} catch (e) {
-			message.channel.sendLocale('MUSIC_FAILEDTODISCONNECT');
+			throw message.language.get('MUSIC_FAILEDTODISCONNECT');
 		}
 	}
 };
