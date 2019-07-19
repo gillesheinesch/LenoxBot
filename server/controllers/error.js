79ef2f5f3a8c95583cd072b596db819772376b33
Monkeyyy11 <gaileshd@gmail.com>
2019-07-19 16:31:35 +0200

    deleted server folder

--- server/controllers/error.js
+++
@@ -1,7 +0,0 @@
-function index(req, res) {
-	res.render('error');
-}
-
-module.exports = {
-	index: index
-};
--- server/routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const homeController = require('../controllers/home');
-
-const router = express.Router();
-
-router.get('/', homeController.index);
-
-module.exports = router;
