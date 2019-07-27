b0d680009364fe50454a888de01d4587b46313e1
Monkeyyy11 <gaileshd@gmail.com>
2019-07-27 06:51:06 +0200

    removed console log

--- commands/nsfw/pornhubgif.js
+++ commands/nsfw/pornhubgif.js
@@ -44,7 +44,6 @@ module.exports = class pornhubgifCommand extends LenoxCommand {
 				embed: embed
 			});
 		} catch (error) {
-			console.log(error)
 			return msg.reply(lang.pornhubgif_couldfindnothing);
 		}
 	}
