2ee5354190d0007eaa3d640d8537abf1257837ac
Monkeyyy11 <gaileshd@gmail.com>
2019-08-25 13:14:45 +0200

    fix

--- commands/utility/serverinfo.js
+++ commands/utility/serverinfo.js
@@ -21,51 +21,50 @@ module.exports = class serverinfoCommand extends LenoxCommand {
   }
 
   run(msg) {
-      const langSet = msg.client.provider.getGuild(msg.guild.id, 'language');
-      const lang = require(`../../languages/${langSet}.json`);
+    const langSet = msg.client.provider.getGuild(msg.guild.id, 'language');
+    const lang = require(`../../languages/${langSet}.json`);
 
-      moment.locale(msg.client.provider.getGuild(msg.guild.id, 'momentLanguage'));
+    moment.locale(msg.client.provider.getGuild(msg.guild.id, 'momentLanguage'));
 
-      const servercreated = moment(msg.guild.createdAt).format('MMMM Do YYYY, h:mm:ss a');
+    const servercreated = moment(msg.guild.createdAt).format('MMMM Do YYYY, h:mm:ss a');
 
-      const emojis = [];
-      if (msg.guild.emojis.size !== 0) {
-        msg.guild.emojis.forEach((r) => {
-          const emoji = msg.client.emojis.resolve(r.id);
-          emojis.push(emoji);
-        });
-      }
-
-      const emojisembed = [];
-      if (emojis.length === 0) {
-        emojisembed.push(lang.serverinfo_emojisnone);
-      }
-      else if (emojis.join(' ').length > 1020) {
-        for (let i = 0; i < emojis.length; i += 1) {
-          if (emojisembed.join(' ').length < 980) {
-            emojisembed.push(emojis[i]);
-          }
-        }
-        emojisembed.push('...');
-      }
-      else {
-        emojisembed.push(emojis.join(' '));
-      }
-
-      const embed = new Discord.MessageEmbed()
-        .setAuthor(`${msg.guild.name} (${msg.guild.id})`, msg.guild.iconURL())
-        .setColor('#0066CC')
-        .setTimestamp()
-        .setThumbnail(msg.guild.iconURL())
-        .addField(`🤵 ${lang.serverinfo_members}`, msg.guild.memberCount)
-        .addField(`🗻 ${lang.serverinfo_region}`, msg.guild.region)
-        .addField(`📲 ${lang.serverinfo_channels}`, msg.guild.channels.size)
-        .addField(`⏳ ${lang.serverinfo_created}`, servercreated)
-        .addField(`☑ ${lang.serverinfo_verification}`, msg.guild.verificationLevel || lang.serverinfo_noverification)
-        .addField(`📤 ${lang.serverinfo_afkchannel}`, msg.guild.afkChannel === null ? lang.serverinfo_noafkchannel : msg.guild.afkChannel.name)
-        .addField(`🎊 ${lang.serverinfo_emojis}`, emojisembed.join(' '));
-
-      msg.channel.send({ embed });
+    const emojis = [];
+    if (msg.guild.emojis.size !== 0) {
+      msg.guild.emojis.forEach((r) => {
+        const emoji = msg.client.emojis.resolve(r.id);
+        emojis.push(emoji);
+      });
     }
+
+    const emojisembed = [];
+    if (emojis.length === 0) {
+      emojisembed.push(lang.serverinfo_emojisnone);
+    }
+    else if (emojis.join(' ').length > 1020) {
+      for (let i = 0; i < emojis.length; i += 1) {
+        if (emojisembed.join(' ').length < 980) {
+          emojisembed.push(emojis[i]);
+        }
+      }
+      emojisembed.push('...');
+    }
+    else {
+      emojisembed.push(emojis.join(' '));
+    }
+
+    const embed = new Discord.MessageEmbed()
+      .setAuthor(`${msg.guild.name} (${msg.guild.id})`, msg.guild.iconURL())
+      .setColor('#0066CC')
+      .setTimestamp()
+      .setThumbnail(msg.guild.iconURL())
+      .addField(`🤵 ${lang.serverinfo_members}`, msg.guild.memberCount)
+      .addField(`🗻 ${lang.serverinfo_region}`, msg.guild.region)
+      .addField(`📲 ${lang.serverinfo_channels}`, msg.guild.channels.size)
+      .addField(`⏳ ${lang.serverinfo_created}`, servercreated)
+      .addField(`☑ ${lang.serverinfo_verification}`, msg.guild.verificationLevel || lang.serverinfo_noverification)
+      .addField(`📤 ${lang.serverinfo_afkchannel}`, msg.guild.afkChannel === null ? lang.serverinfo_noafkchannel : msg.guild.afkChannel.name)
+      .addField(`🎊 ${lang.serverinfo_emojis}`, emojisembed.join(' '));
+
+    msg.channel.send({ embed });
   }
 };
