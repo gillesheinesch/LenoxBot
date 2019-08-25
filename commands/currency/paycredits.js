b9ba2758f9fe7d9b30e94b9efcb04b608a6673f3
Monkeyyy11 <gaileshd@gmail.com>
2019-08-25 16:34:48 +0200

    closes #642

--- commands/currency/paycredits.js
+++ commands/currency/paycredits.js
@@ -7,7 +7,7 @@ module.exports = class paycreditsCommand extends LenoxCommand {
       group: 'currency',
       memberName: 'paycredits',
       description: 'Allows a user to give their credits to someone',
-      format: 'paycredits {@User} {amount}',
+      format: 'paycredits {@User/UserID} {amount}',
       aliases: [],
       examples: ['paycredits @Monkeyyy11#7584 100'],
       clientpermissions: ['SEND_MESSAGES'],
@@ -22,18 +22,18 @@ module.exports = class paycreditsCommand extends LenoxCommand {
     const lang = require(`../../languages/${langSet}.json`);
     const args = msg.content.split(' ').slice(1);
 
-    const mention = msg.mentions.users.first();
+    const mention = msg.mentions.users.first() || msg.client.users.get(args[0]);
 
-    if (!mention) return msg.channel.send(lang.paycredits_nomention);
-    if (mention.id === msg.author.id) return msg.channel.send(lang.paycredits_yourself);
-    if (args.slice(1).length === 0) return msg.channel.send(lang.paycredits_noinput);
-    if (isNaN(args.slice(1))) return msg.channel.send(lang.paycredits_number);
-    if (parseInt(args.slice(1).join(' '), 10) === 0) return msg.channel.send(lang.paycredits_not0);
-    if (parseInt(args.slice(1).join(' '), 10) < 2) return msg.channel.send(lang.paycredits_one);
+    if (!mention) return msg.reply(lang.paycredits_nomention);
+    if (mention.id === msg.author.id) return msg.reply(lang.paycredits_yourself);
+    if (args.slice(1).length === 0) return msg.reply(lang.paycredits_noinput);
+    if (isNaN(args.slice(1))) return msg.reply(lang.paycredits_number);
+    if (parseInt(args.slice(1).join(' '), 10) === 0) return msg.reply(lang.paycredits_not0);
+    if (parseInt(args.slice(1).join(' '), 10) < 2) return msg.reply(lang.paycredits_one);
 
     const msgauthortable = msg.client.provider.getUser(msg.author.id, 'credits');
 
-    if (msgauthortable < parseInt(args.slice(1).join(' '), 10)) return msg.channel.send(lang.paycredits_notenough);
+    if (msgauthortable < parseInt(args.slice(1).join(' '), 10)) return msg.reply(lang.paycredits_notenough);
 
     let currentCredits = msg.client.provider.getUser(msg.author.id, 'credits');
     currentCredits -= parseInt(args.slice(1).join(' '), 10);
@@ -43,7 +43,7 @@ module.exports = class paycreditsCommand extends LenoxCommand {
     currentCreditsMention += parseInt(args.slice(1).join(' '), 10);
     await msg.client.provider.setUser(mention.id, 'credits', currentCreditsMention);
 
-    const medalsgiven = lang.paycredits_creditsgiven.replace('%author', msg.author).replace('%creditscount', args.slice(1).join(' ')).replace('%mentiontag', mention.tag);
-    return msg.channel.send(medalsgiven);
+    const creditsgiven = lang.paycredits_creditsgiven.replace('%author', msg.author).replace('%creditscount', args.slice(1).join(' ')).replace('%mentiontag', mention.tag);
+    return msg.reply(creditsgiven);
   }
 };
