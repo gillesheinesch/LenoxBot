3eb36e0d2679762d9f81e11091da9474671e9c64
Monkeyyy11 <gaileshd@gmail.com>
2019-08-29 13:41:50 +0200

    closes #458 + reason now needed to use the clear command

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -8,9 +8,9 @@ module.exports = class clearCommand extends LenoxCommand {
       group: 'moderation',
       memberName: 'clear',
       description: 'Deletes for you the last X messages that were sent in the current channel',
-      format: 'clear {amount of messages between 2 and 100}',
+      format: 'clear {amount of messages between 2 and 100} {reason}',
       aliases: ['purge'],
-      examples: ['clear 50'],
+      examples: ['clear 50 test'],
       clientpermissions: ['SEND_MESSAGES', 'MANAGE_MESSAGES'],
       userpermissions: ['MANAGE_MESSAGES'],
       shortDescription: 'General',
@@ -24,18 +24,32 @@ module.exports = class clearCommand extends LenoxCommand {
     const args = msg.content.split(' ').slice(1);
 
     if (args.slice().length === 0) return msg.reply(lang.clear_error);
-    if (isNaN(args.slice().join(' '))) return msg.reply(lang.clear_number);
+    if (isNaN(args.slice(0, 1).join(' '))) return msg.reply(lang.clear_number);
 
-    const messagecount = parseInt(args.join(' '), 10);
-
+    const messagecount = parseInt(args.slice(0, 1).join(' '), 10);
     if (messagecount > 100) return msg.reply(lang.clear_max100);
     if (messagecount < 2) return msg.reply(lang.clear_min2);
+
+    const reason = args.slice(1, 2);
+    if (!reason || !reason.length) return msg.reply(lang.clear_noreason);
 
     if (msg.client.provider.getGuild(msg.guild.id, 'commanddel') === 'false') {
       await msg.delete();
     }
 
     await msg.channel.messages.fetch({ limit: messagecount }).then((messages) => msg.channel.bulkDelete(messages));
+
+    const embedtitle = lang.clear_embedtitle.replace('%amount', messagecount).replace('%user', `${msg.author.username}${msg.author.discriminator}`);
+    const modLogEmbed = new Discord.MessageEmbed()
+      .setAuthor(embedtitle, msg.author.displayAvatarURL())
+      .setColor('ORANGE')
+      .setTimestamp()
+      .addField(lang.clear_embedreason, reason);
+
+    if (msg.client.provider.getGuild(msg.guild.id, 'modlog') === 'true') {
+      const modlogchannel = msg.client.channels.get(msg.client.provider.getGuild(msg.guild.id, 'modlogchannel'));
+      modlogchannel.send({ embed: modLogEmbed });
+    }
 
     const messagesdeleted = lang.clear_messagesdeleted.replace('%messagecount', messagecount);
     const messageclearembed = new Discord.MessageEmbed()
