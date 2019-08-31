ba24818b7133e2069db8e445ce00f7d3d497df0f
BannerBomb <BannerBomb55@gmail.com>
2019-08-30 20:57:46 -0400

    Update klasaReady.js

--- src/events/klasaReady.js
+++ src/events/klasaReady.js
@@ -1,6 +1,4 @@
 const { Event } = require("klasa");
-const { MessageEmbed } = require('discord.js');
-const TABLES = ['members', 'guilds', 'users', 'clientStorage'];
 
 module.exports = class extends Event {
 	constructor(...args) {
@@ -11,34 +9,6 @@ module.exports = class extends Event {
 	}
 
 	async run() {
-		if (process.argv.includes("--migrate")) {
-			for (const table of TABLES) {
-				const docs = await this.client.providers.default.getAll(table);
-				for (const doc of docs) {
-					const newID = doc.id.split("-").join(".");
-					await this.client.providers.default.delete(table, doc.id).catch(() => null);
-					doc.id = newID;
-					await this.client.providers.default.create(table, newID, doc);
-				}
-			}
-			this.client.console.warn("Migration Done. Process will now exit. Remove the `--migrate` flag");
-			process.exit(0);
-		}
-		if (process.argv.includes("--migrate-rethink")) {
-			const jsonProvider = this.client.providers.get("json");
-			for (const table of TABLES) {
-				const docs = await jsonProvider.getAll(table);
-				for (const doc of docs) {
-					const newID = doc.id.split("-").join(".");
-					await jsonProvider.delete(table, doc.id).catch(() => null);
-					doc.id = newID;
-					await this.client.providers.default.create(table, newID, doc);
-				}
-			}
-			this.client.console.warn("Migration to RethinkDB Done. Process will now exit. Remove the `--migrate` flag");
-			process.exit(0);
-		}
-
 		this.client.setMaxListeners(Infinity); // change the clients event listener max amount
 
 		this.client.user.setPresence({
