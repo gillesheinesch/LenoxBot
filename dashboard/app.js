60384967bc774c4f344c228cb77413301c3d9ef8
Monkeyyy11 <gaileshd@gmail.com>
2019-08-31 16:20:50 +0200

    lint

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,52 +1,56 @@
-var createError = require('http-errors');
-var express = require('express');
-var exphbs = require('express-handlebars');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 const i18n = require('i18n');
 
 i18n.configure({
-  directory: `${__dirname}/public/lang`,
-  defaultLocale: 'en-US',
-  cookie: 'ulang'
-})
+	directory: `${__dirname}/public/lang`,
+	defaultLocale: 'en-US',
+	cookie: 'ulang'
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 
-app.engine('hbs', exphbs({extname: '.hbs'}))
+app.engine('hbs', exphbs({
+	extname: '.hbs'
+}));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(i18n.init)
+app.use(i18n.init);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
--- dashboard/routes/index.js
+++ dashboard/routes/index.js
@@ -1,39 +1,40 @@
 const express = require('express');
+// eslint-disable-next-line new-cap
 const router = express.Router();
 const axios = require('axios');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  const bot = await axios({
-    method: 'get',
-    url: 'http://127.0.0.1:4000/api/application'
-  })
-  .then(res => res.data)
-  .catch(e => console.log(e));
+	const bot = await axios({
+		method: 'get',
+		url: 'http://127.0.0.1:4000/api/application'
+	})
+		.then(res2 => res2.data)
+		.catch(e => console.log(e));
 
-  const lang = require(`../public/lang/${req.getLocale()}`);
+	const lang = require(`../public/lang/${req.getLocale()}`);
 
-  res.render('index', { 
-    title: 'Express', 
-    bot,
-    lang
-   });
+	res.render('index', {
+		title: 'Express',
+		bot,
+		lang
+	});
 });
 
 router.get('/commands', async (req, res, next) => {
-  const commands = await axios({
-    method: 'get',
-    url: 'http://127.0.0.1:4000/api/commands'
-  })
-  .then(res => res.data)
-  .catch(e => console.log(e));
+	const commands = await axios({
+		method: 'get',
+		url: 'http://127.0.0.1:4000/api/commands'
+	})
+		.then(res2 => res2.data)
+		.catch(e => console.log(e));
 
-  const lang = require(`../public/lang/${req.getLocale()}`);
+	const lang = require(`../public/lang/${req.getLocale()}`);
 
-  res.render('commands', {
-    commands,
-    lang
-  })
+	res.render('commands', {
+		commands,
+		lang
+	});
 });
 
 module.exports = router;
