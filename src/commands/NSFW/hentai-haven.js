e7b94d3c8896a286fadd45d4182e4bfef0e59d1f
BannerBomb <BannerBomb55@gmail.com>
2019-08-30 21:05:43 -0400

    Update hentai-haven.js

--- src/commands/NSFW/hentai-haven.js
+++ src/commands/NSFW/hentai-haven.js
@@ -14,12 +14,12 @@ module.exports = class extends Command {
 	async run(message) {
 		try {
 			/* eslint no-undef: 0 */
-			JSDOM.fromURL('https://hentaihaven.org').then(res => {
+			await JSDOM.fromURL('https://hentaihaven.org').then(async(res) => {
 				const elements = res.window.document.querySelectorAll('head > script');
 				const element = Array.from(elements).filter(elem => elem.text.startsWith('/*  */\nvar Pukka = '))[0];
 				const Pukka = JSON.parse(element.text.replace(/^\/\* {2}\*\/\nvar Pukka = |\;\n\/\* {2}\*\/$/g, ''));
 				const random_category = Pukka.category_links[Math.floor(Math.random() * (Pukka.category_links.length - 1))];
-				JSDOM.fromURL(random_category).then(resource => {
+				await JSDOM.fromURL(random_category).then(resource => {
 					const document = resource.window.document;
 					const title = document.getElementsByClassName('archive-title')[0].textContent;
 					const videos = document.getElementsByClassName('hidden animate_video');
