0c3979e08582db257a38e21c0d8d5dc1b5f40b22
Monkeyyy11 <gaileshd@gmail.com>
2019-10-05 10:02:50 +0200

    website updates

---
+++ middleware/languages.js
@@ -0,0 +1,40 @@
+module.exports = (req) => {
+  const languagesList = [{
+    fileName: 'en-US',
+    name: 'English',
+    icon: 'us',
+    status: false
+  },
+  {
+    fileName: 'de-DE',
+    name: 'German',
+    icon: 'de',
+    status: false
+  },
+  {
+    fileName: 'fr-FR',
+    name: 'French',
+    icon: 'fr',
+    status: false
+  },
+  {
+    fileName: 'es-ES',
+    name: 'Spanish',
+    icon: 'es',
+    status: false
+  },
+  {
+    fileName: 'de-CH',
+    name: 'Swiss',
+    icon: 'ch',
+    status: false
+  }];
+
+  const currentLanguage = req.getLocale();
+  for (let i = 0; i < languagesList.length; i += 1) {
+    if (languagesList[i].fileName === currentLanguage) {
+      languagesList[i].status = true;
+    }
+  }
+  return languagesList;
+};
