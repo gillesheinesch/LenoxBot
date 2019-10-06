de4246f0909a0663000d17ec60748aab73701c54
Monkeyyy11 <gaileshd@gmail.com>
2019-10-06 17:58:29 +0200

    fixes

--- lenoxbotlauncher.js
+++ lenoxbotlauncher.js
@@ -22,7 +22,7 @@ const shardingManager = new Discord.ShardingManager('./lenoxbot.js', {
   token: settings.token
 });
 
-shardingManager.spawn(1).then(() => {
+shardingManager.spawn().then(() => {
   console.log(chalk.green(`[ShardManager] Started ${shardingManager.totalShards} shards`));
 }).catch((error) => {
   console.log(error);
