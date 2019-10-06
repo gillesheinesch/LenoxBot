7ea92b3b0dfbb4e52587e9ab1cb0d06f6026dd33
Monkeyyy11 <gaileshd@gmail.com>
2019-10-06 15:49:09 +0200

    New error message when a bug occurs

--- commands/LenoxCommand.js
+++ commands/LenoxCommand.js
@@ -1,4 +1,5 @@
 const DiscordCommando = require('discord.js-commando');
+const Discord = require('discord.js');
 
 module.exports = class LenoxCommand extends DiscordCommando.Command {
   constructor(client, info) {
@@ -8,5 +9,16 @@ module.exports = class LenoxCommand extends DiscordCommando.Command {
     this.dashboardsettings = info.dashboardsettings || null;
     this.clientpermissions = info.clientpermissions || [];
     this.userpermissions = info.userpermissions || [];
+  }
+
+  onError(err, message, args, fromPattern, result) { // eslint-disable-line no-unused-vars
+    const embed = new Discord.MessageEmbed()
+      .setColor('RED')
+      .setTimestamp()
+      .setTitle('Please report this on our Discord server https://lenoxbot.com/discord')
+      .setDescription(`StackTrace: \n\`\`\`${err.stack}\`\`\``)
+      .addField('Command:', `${message.content.split(' ').join(' ')}`);
+
+    return message.reply({ embed });
   }
 };
