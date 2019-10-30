61235107f72302fe5b4f4cef74f27fec57fcbf0f
Monkeyyy11 <gaileshd@gmail.com>
2019-10-30 17:22:03 +0100

    fix

--- events/error.js
+++ events/error.js
@@ -1,17 +1,21 @@
 module.exports = {
-	run: (error) => {
-		if (!client.ready || !client.settings || !client.settings.log_channels || !client.settings.log_channels.errors || !error) return;
-		try {
-			if (client.channels.has(client.settings.log_channels.errors)) client.channels.get(client.settings.log_channels.errors).send(null, {
-				embed: {
-					color: 15684432,
-					timestamp: new Date(),
-					title: 'Error',
-					description: error.stack ? `\`\`\`x86asm\n${error.stack}\n\`\`\`` : `\`${error.toString()}\``
-				}
-			});
-		} catch (error) {
-			return console.error(error.stack ? error.stack : error.toString());
-		}
-	}
-}
+  run: (error) => {
+    if (!client.ready || !client.settings || !client.settings.log_channels || !client.settings.log_channels.errors || !error) return;
+    try {
+      console.error(error.stack);
+      if (client.channels.has(client.settings.log_channels.errors)) {
+        client.channels.get(client.settings.log_channels.errors).send(null, {
+          embed: {
+            color: 15684432,
+            timestamp: new Date(),
+            title: 'Error',
+            description: error.stack ? `\`\`\`x86asm\n${error.stack}\n\`\`\`` : `\`${error.toString()}\``
+          }
+        });
+      }
+    }
+    catch (error) {
+      return console.error(error.stack ? error.stack : error.toString());
+    }
+  }
+};
